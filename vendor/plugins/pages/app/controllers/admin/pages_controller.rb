4a139e6daf9713b2894ee6955d151263d6bdb321
Philip Arndt <parndt@gmail.com>
2009-06-16 11:19:45 +1200

    The last fix to page parts was flawed and page parts did not save correctly when creating a new page with default page parts.  The issue has now been resolved.

--- vendor/plugins/pages/app/controllers/admin/pages_controller.rb
+++ vendor/plugins/pages/app/controllers/admin/pages_controller.rb
@@ -2,18 +2,6 @@ class Admin::PagesController < Admin::BaseController
 
   crudify :page, :conditions => "parent_id IS NULL", :order => "position ASC"
   
-  def create
-    @page = Page.create(params[:page])
-    if @page.valid?
-      params[:page_part][:title] = "Body" # default part name
-      @page.parts.create(params[:page_part])
-      flash[:notice] = "'#{@page.title}' was successfully created."
-      redirect_to :action => 'index'
-    else 
-      render :action => 'new'
-    end
-  end
-  
   def index
     @pages = Page.paginate :page => params[:page],
                   :order => "position ASC",
