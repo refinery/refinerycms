20d17ed40e534ee409a984e7ab96190a8d55e56a
Philip Arndt <parndt@gmail.com>
2009-08-23 19:16:41 +1200

    404 if the page is not live?

--- vendor/plugins/pages/app/controllers/pages_controller.rb
+++ vendor/plugins/pages/app/controllers/pages_controller.rb
@@ -8,6 +8,8 @@ class PagesController < ApplicationController
 	def show
 	  @page = Page.find(params[:id], :include => [:parts, :slugs])
 		
+		error_404 unless @page.live?
+		
 		# if the admin wants this to be a "placeholder" page which goes to its first child, go to that instead.
 		if @page.skip_to_first_child
 			first_live_child = @page.children.find_by_draft(false, :order => "position ASC")
