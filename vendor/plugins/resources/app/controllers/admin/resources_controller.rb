8df1834b2b620c4980d2ed67133e7f1b47fad6e5
Philip Arndt <parndt@gmail.com>
2009-09-09 10:53:16 +1200

    ability added for resources upload to be used inside of a dialogue and ability applied to dashboard 'upload a file' link

--- vendor/plugins/resources/app/controllers/admin/resources_controller.rb
+++ vendor/plugins/resources/app/controllers/admin/resources_controller.rb
@@ -2,4 +2,24 @@ class Admin::ResourcesController < Admin::BaseController
   
   crudify :resource, :order => "updated_at DESC"
   
+	def new
+		@resource = Resource.new
+		
+    @url_override = admin_resources_url(:dialog => from_dialog?)
+	end
+	
+	def create
+    @resource = Resource.create params[:resource]
+		if @resource.valid?
+			flash[:notice] = "'#{@resource.title}' was successfully created."
+			unless from_dialog?
+				redirect_to :action => 'index'
+			else
+				render :text => "<script type='text/javascript'>parent.window.location = '#{admin_resources_url}';</script>"
+			end
+		else
+			render :action => 'new'
+		end
+  end
+
 end
