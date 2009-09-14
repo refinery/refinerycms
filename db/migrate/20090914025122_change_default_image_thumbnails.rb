25822f720a584c70a587cef045dd7226b46f791b
Philip Arndt <parndt@gmail.com>
2009-09-14 15:09:01 +1200

    improved the migration and default seed to be strongly typed instead of a string and added grid thumbnail to default seed.

--- db/migrate/20090914025122_change_default_image_thumbnails.rb
+++ db/migrate/20090914025122_change_default_image_thumbnails.rb
@@ -1,24 +1,9 @@
 class ChangeDefaultImageThumbnails < ActiveRecord::Migration
   def self.up
-		RefinerySetting[:image_thumbnails] = "{
-	:lightbox => '500x500>',
-	:dialog_thumb => 'c106x106',
-	:thumb => '50x50',
-	:medium => '225x255',
-	:side_body => '300x500',
-	:preview => 'c96x96',
-	:grid => 'c135x135'
-}"
+		RefinerySetting[:image_thumbnails] = RefinerySetting[:image_thumbnails].merge({:grid => 'c135x135'})
   end
 
   def self.down
-		RefinerySetting[:image_thumbnails] = "{
-	:lightbox => '500x500>',
-	:dialog_thumb => 'c106x106',
-	:thumb => '50x50',
-	:medium => '225x255',
-	:side_body => '300x500',
-	:preview => 'c96x96'
-}"
+		RefinerySetting[:image_thumbnails] = RefinerySetting[:image_thumbnails].delete_if {|key, value| key == :grid }
   end
 end
