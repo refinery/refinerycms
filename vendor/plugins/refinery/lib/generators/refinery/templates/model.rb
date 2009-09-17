a53dd1728fa7b2ca1a0e3697b1cc55a704613861
Philip Arndt <parndt@gmail.com>
2009-09-17 19:10:36 +1200

    Updated plugin/engine generator to use the new style of doing things. There may still be a couple of inconsistencies so testing is advised. Generator is run with script/generate refinery singular_plugin_name field1:field_type field2:field_type etc.

--- vendor/plugins/refinery/lib/generators/refinery/templates/model.rb
+++ vendor/plugins/refinery/lib/generators/refinery/templates/model.rb
@@ -1,5 +1,7 @@
 class <%= class_name %> < ActiveRecord::Base
   
+	acts_as_indexed :fields => [:<%= attributes.collect{ |attribute| attribute.name }.join(", :") %>]
+
   validates_presence_of :<%= attributes.first.name %>
   validates_uniqueness_of :<%= attributes.first.name %>
   
--- vendor/plugins/refinery/lib/generators/refinery/templates/public_controller.rb
+++ vendor/plugins/refinery/lib/generators/refinery/templates/public_controller.rb
@@ -1,7 +1,19 @@
 class <%= class_name.pluralize %>Controller < ApplicationController
 
+	before_filter :find_page
+
+	def index
+		@<%= plural_name %> = <%= class_name %>.find(:all, :order => "position ASC")
+	end
+
   def show
     @<%= singular_name %> = <%= class_name %>.find(params[:id])
   end
   
+protected
+
+	def find_page
+		@page = Page.find_by_link_url("/<%= plural_name %>")
+	end
+
 end
