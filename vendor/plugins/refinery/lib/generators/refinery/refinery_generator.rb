688df5a3bbb082c161d1e83a8956e1b1382e68b8
Philip Arndt <parndt@gmail.com>
2009-09-17 19:10:36 +1200

    Updated plugin/engine generator to use the new style of doing things. There may still be a couple of inconsistencies so testing is advised. Generator is run with script/generate refinery singular_plugin_name field1:field_type field2:field_type etc.

--- vendor/plugins/refinery/lib/generators/refinery/refinery_generator.rb
+++ vendor/plugins/refinery/lib/generators/refinery/refinery_generator.rb
@@ -12,7 +12,8 @@ class RefineryGenerator < Rails::Generator::NamedBase
         "vendor/plugins/#{plural_name}/app/controllers/admin", "vendor/plugins/#{plural_name}/app/models", "vendor/plugins/#{plural_name}/app/views", 
         "vendor/plugins/#{plural_name}/app/helpers", "vendor/plugins/#{plural_name}/app/views", "vendor/plugins/#{plural_name}/app/views/admin",
         "vendor/plugins/#{plural_name}/config"]
-      for dir in directories
+
+      directories.each do |dir|
         m.directory dir
       end
       
@@ -21,21 +22,27 @@ class RefineryGenerator < Rails::Generator::NamedBase
       m.template "config/routes.rb", "vendor/plugins/#{plural_name}/config/routes.rb"
       
       # Create view directory
-      view_dir = File.join("vendor/plugins/#{plural_name}/app/views/admin", plural_name)
-      m.directory view_dir
+      admin_view_dir = File.join("vendor/plugins/#{plural_name}/app/views/admin", plural_name)
+      m.directory admin_view_dir
       
       # Copy in all views
-      view_files = ['index.html.erb', '_form.html.erb', '_list.html.erb', '_make_sortable.html.erb', 'edit.html.erb', 'new.html.erb', 'reorder.html.erb']
-                    
-      for file in view_files
-        m.template "views/#{file}", "#{view_dir}/#{file}"
-      end
+      admin_view_files = ['_form.html.erb', '_sortable_list.html.erb', 'edit.html.erb', 'index.html.erb', 'new.html.erb']
       
-      # Now for the public view and controller
+			admin_view_files.each do |view_file|
+        m.template "views/admin/#{view_file}", "#{admin_view_dir}/#{view_file}"
+      end
+
+			m.template "views/admin/_singular_name.html.erb", "#{admin_view_dir}/_#{singular_name}.html.erb"
+
+      # Now for the public views and controller
       public_dir = File.join("vendor/plugins/#{plural_name}/app/views/", plural_name)
       m.directory public_dir
       
-      m.template "views/show.html.erb", "#{public_dir}/show.html.erb"
+			view_files = ['index.html.erb', 'show.html.erb']   
+      view_files.each do |view_file|
+        m.template "views/#{view_file}", "#{public_dir}/#{view_file}"
+      end
+
       m.template "public_controller.rb", "vendor/plugins/#{plural_name}/app/controllers/#{plural_name}_controller.rb"
 
       # Add in the init file that ties the plugin to the app.
