57a3b8f632c80dbff5c46b02dc49707aade84cc3
Philip Arndt <parndt@gmail.com>
2009-09-30 15:42:27 +1300

    Move from plugins to gems to both reduce refinery size and optimise refinery's footprint when running in an environment that can cache memory across apps that share the same gems, e.g. passenger. This will also make upgrading versions easier as you just have to install a later version of the gem.

--- vendor/plugins/acts_as_state_machine/init.rb
+++
@@ -1,5 +0,0 @@
-require 'acts_as_state_machine'
-
-ActiveRecord::Base.class_eval do
-  include ScottBarron::Acts::StateMachine
-end
--- vendor/plugins/friendly_id/test/models/post.rb
+++
@@ -1,3 +0,0 @@
-class Post < ActiveRecord::Base
-  has_friendly_id :title, :use_slug => true
-end
