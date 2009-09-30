a95ba766501c0cd9088c6ccd763f54abdaa5af11
Philip Arndt <parndt@gmail.com>
2009-09-30 16:55:08 +1300

    Fix case where rake gems:install wouldn't run thus rendering refinery useless unless a specific gem was installed manually. Now, just clone Refinery and run (sudo) rake gems:install to install gem dependencies

--- vendor/plugins/authentication/app/models/user.rb
+++ vendor/plugins/authentication/app/models/user.rb
@@ -1,7 +1,40 @@
 require 'digest/sha1'
-require 'aasm'
-class User < ActiveRecord::Base
-	include AASM # include the library which will give us state machine functionality.
+class User < ActiveRecord::Base	
+	# Hack: Allow "rake gems:install" to run when this class is missing its gem dependency.
+	# For further clarification on why, refer to:
+	# https://rails.lighthouseapp.com/projects/8994/tickets/780-rake-gems-install-doesn-t-work-if-plugins-are-missing-gem-dependencies
+	if defined? AASM
+		include AASM # include the library which will give us state machine functionality.
+		aasm_column :state
+	  aasm_initial_state :pending
+	  aasm_state :passive
+	  aasm_state :pending, :enter => :make_activation_code
+	  aasm_state :active,  :enter => :do_activate
+	  aasm_state :suspended
+	  aasm_state :deleted, :enter => :do_delete
+
+	  aasm_event :register do
+	    transitions :from => :passive, :to => :pending, :guard => Proc.new {|u| !(u.crypted_password.blank? && u.password.blank?) }
+	  end
+  
+	  aasm_event :activate do
+	    transitions :from => :pending, :to => :active 
+	  end
+  
+	  aasm_event :suspend do
+	    transitions :from => [:passive, :pending, :active], :to => :suspended
+	  end
+  
+	  aasm_event :delete do
+	    transitions :from => [:passive, :pending, :active, :suspended], :to => :deleted
+	  end
+
+	  aasm_event :unsuspend do
+	    transitions :from => :suspended, :to => :active,  :guard => Proc.new {|u| !u.activated_at.blank? }
+	    transitions :from => :suspended, :to => :pending, :guard => Proc.new {|u| !u.activation_code.blank? }
+	    transitions :from => :suspended, :to => :passive
+	  end
+	end	
 	
   # Virtual attribute for the unencrypted password
   attr_accessor :password
@@ -23,36 +56,6 @@ class User < ActiveRecord::Base
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
   attr_accessible :login, :email, :password, :password_confirmation, :plugins
-
-	aasm_column :state
-  aasm_initial_state :pending
-  aasm_state :passive
-  aasm_state :pending, :enter => :make_activation_code
-  aasm_state :active,  :enter => :do_activate
-  aasm_state :suspended
-  aasm_state :deleted, :enter => :do_delete
-
-  aasm_event :register do
-    transitions :from => :passive, :to => :pending, :guard => Proc.new {|u| !(u.crypted_password.blank? && u.password.blank?) }
-  end
-  
-  aasm_event :activate do
-    transitions :from => :pending, :to => :active 
-  end
-  
-  aasm_event :suspend do
-    transitions :from => [:passive, :pending, :active], :to => :suspended
-  end
-  
-  aasm_event :delete do
-    transitions :from => [:passive, :pending, :active, :suspended], :to => :deleted
-  end
-
-  aasm_event :unsuspend do
-    transitions :from => :suspended, :to => :active,  :guard => Proc.new {|u| !u.activated_at.blank? }
-    transitions :from => :suspended, :to => :pending, :guard => Proc.new {|u| !u.activation_code.blank? }
-    transitions :from => :suspended, :to => :passive
-  end
 
   # Authenticates a user by their login name and unencrypted password.  Returns the user or nil.
   def self.authenticate(login, password)
