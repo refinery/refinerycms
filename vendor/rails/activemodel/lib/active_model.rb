92807d5ef6070f0bedb394eafc2a7afea4faf724
Philip Arndt <parndt@gmail.com>
2009-09-30 14:36:06 +1300

    We don't really need to have rails in the vendor directory. Just install the gem and choose your own version. This will save a lot of time cloning and a lot of bother every time we update the version of Refinery CMS.

--- vendor/rails/activemodel/lib/active_model.rb
+++
@@ -1,5 +0,0 @@
-require 'active_model/observing'
-# disabled until they're tested
-# require 'active_model/callbacks'
-# require 'active_model/validations'
-require 'active_model/base'
--- vendor/rails/railties/test/fixtures/metal/multiplemetals/app/metal/metal_a.rb
+++
@@ -1,5 +0,0 @@
-class MetalA < Rails::Rack::Metal
-  def self.call(env)
-    [200, { "Content-Type" => "text/html"}, ["Hi"]]
-  end
-end
