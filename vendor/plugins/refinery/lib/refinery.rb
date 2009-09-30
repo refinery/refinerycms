7fd507b9510004e3b296657043042fd1d6acb3a0
Philip Arndt <parndt@gmail.com>
2009-09-30 16:09:39 +1300

    added gemspec and modified require statements

--- vendor/plugins/refinery/lib/refinery.rb
+++ vendor/plugins/refinery/lib/refinery.rb
@@ -3,6 +3,7 @@ module Refinery
 		begin
 			require 'friendly_id'
 			require 'will_paginate'
+			require 'aasm'
 		rescue LoadError => load_error
 			# this will stop us running rake gems:install which we don't really want so just trap this error.
 		end
