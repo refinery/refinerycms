a6833e08ca94e592c3f897e9179035e5a157bd85
Philip Arndt <parndt@gmail.com>
2009-10-06 11:19:41 +1300

    remove pagination by overriding index method

--- vendor/plugins/pages/app/controllers/admin/pages_controller.rb
+++ vendor/plugins/pages/app/controllers/admin/pages_controller.rb
@@ -2,6 +2,14 @@ class Admin::PagesController < Admin::BaseController
 
   crudify :page, :conditions => "parent_id IS NULL", :order => "position ASC", :include => [:parts, :slugs, :children, :images]
   
+  def index
+    if searching?
+      @pages = Page.find_with_index params[:search]
+    else
+      @pages = Page.find_all_by_parent_id(nil, :order => "position ASC")
+    end
+  end
+  
   def new
     @page = Page.new
     RefinerySetting.find_or_set(:default_page_parts, ["body", "side_body"]).each do |page_part|
