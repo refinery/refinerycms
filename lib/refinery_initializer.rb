d5ae9f927dbae037722527e0f87f63abfb9432ad
Philip Arndt <parndt@gmail.com>
2009-10-06 15:39:14 +1300

    Remove any dir/.. in REFINERY_ROOT which are ugly and useless.

--- lib/refinery_initializer.rb
+++ lib/refinery_initializer.rb
@@ -1,4 +1,7 @@
-REFINERY_ROOT = File.join File.dirname(__FILE__), '..'
+# Get library path without '..' directories.
+dir_parts = (File.join File.dirname(__FILE__), '..').split(File::SEPARATOR)
+dir_parts = dir_parts[0..(dir_parts.length-3)] until dir_parts[dir_parts.length-1] != ".."
+REFINERY_ROOT = File.join dir_parts
 
 unless REFINERY_ROOT == RAILS_ROOT # e.g. only if we're in a gem.
   $LOAD_PATH.unshift "#{REFINERY_ROOT}/vendor/plugins"
