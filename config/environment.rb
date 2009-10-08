e7dac379ca58606781455b3b93df29ccd9527477
Philip Arndt <parndt@gmail.com>
2009-10-09 09:06:44 +1300

    update rails gem requirement to 2.3.4 and add in a stub for has_friendly_id to prevent it whinging when the gem is not installed when it should instead be worrying about telling the developer to install the gem with rake gems:install

--- config/environment.rb
+++ config/environment.rb
@@ -61,7 +61,7 @@ eval("#{(defined? Refinery::Initializer) ? Refinery::Initializer : Rails::Initia
   config.gem "rake", :version => ">= 0.8.3", :lib => "rake"
   config.gem "friendly_id", :version => ">= 2.1.4", :lib => "friendly_id"
   config.gem "mislav-will_paginate", :version => ">= 2.3.11", :lib => "will_paginate", :source => "http://gems.github.com"
-  config.gem "rails", :version => ">= 2.3.2", :lib => "rails"
+  config.gem "rails", :version => ">= 2.3.4", :lib => "rails"
   config.gem "rubyist-aasm", :version => ">= 2.1.1", :lib => "aasm", :source => "http://gems.github.com"
   config.gem "unicode", :version => ">= 0.1", :lib => "unicode"
 end
