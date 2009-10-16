33d54cbf2df5ab9383686708379cc899b8e80430
Philip Arndt <parndt@gmail.com>
2009-10-16 14:01:50 +1300

    Wrap the site name in quotes so that a multiple word name still works just fine on actionmailer. If mail still doesn't work for you check out this about rails 2.3.4 https://rails.lighthouseapp.com/projects/8994-ruby-on-rails/tickets/2340-action-mailer-cant-deliver-mail-via-smtp-on-ruby-191

--- refinerycms.gemspec
+++ refinerycms.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
 	
   s.name = %q{refinerycms}
-  s.version = "0.9.4"
+  s.version = "0.9.4.1"
 
   s.authors = ["Resolve Digital", "David Jones", "Philip Arndt"]
   s.date = %q{2009-10-16}
--- vendor/plugins/inquiries/app/models/inquiry_mailer.rb
+++ vendor/plugins/inquiries/app/models/inquiry_mailer.rb
@@ -3,7 +3,7 @@ class InquiryMailer < ActionMailer::Base
   def confirmation(inquiry, request)
     @subject = "Thank you for your inquiry"
     @recipients = inquiry.email
-    @from = "#{RefinerySetting[:site_name]} <no-reply@#{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}>"
+    @from = "\"#{RefinerySetting[:site_name]}\" <no-reply@#{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}>"
     @sent_on = Time.now
     @body[:inquiry] = inquiry
   end
@@ -11,7 +11,7 @@ class InquiryMailer < ActionMailer::Base
   def notification(inquiry, request)
     @subject = "New inquiry from your website"
     @recipients = InquirySetting.notification_recipients.value
-    @from = "#{RefinerySetting[:site_name]} <no-reply@#{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}>"
+    @from = "\"#{RefinerySetting[:site_name]}\" <no-reply@#{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}>"
     @sent_on = Time.now
     @body[:inquiry] = inquiry
   end
