7814f8a75f7840f892d85c94ffd2ca4e4fc3bf33
Philip Arndt <parndt@gmail.com>
2009-11-04 15:52:26 +1300

    update logic on refinery-update-core command to include rake tasks (anywhere) and database migrations (from db/migrate directory). Removed some now unncessary migrations. Bumped version to 0.9.5.1 to illustrate this change.

--- db/migrate/20090903030652_remove_title_from_resources.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTitleFromResources < ActiveRecord::Migration
-  def self.up
-    remove_column :resources, :title
-  end
-
-  def self.down
-    add_column :resources, :title, :string
-  end
-end
--- db/migrate/20090916055243_add_position_to_user_plugins.rb
+++
@@ -1,9 +0,0 @@
-class AddPositionToUserPlugins < ActiveRecord::Migration
-  def self.up
-    add_column :user_plugins, :position, :integer
-  end
-
-  def self.down
-    remove_column :user_plugins, :position
-  end
-end
