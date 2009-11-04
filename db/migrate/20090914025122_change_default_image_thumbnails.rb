70ba3ce18eb40b7f495c9561431b4684aa140f39
Philip Arndt <parndt@gmail.com>
2009-11-04 15:52:26 +1300

    update logic on refinery-update-core command to include rake tasks (anywhere) and database migrations (from db/migrate directory). Removed some now unncessary migrations. Bumped version to 0.9.5.1 to illustrate this change.

--- db/migrate/20090914025122_change_default_image_thumbnails.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDefaultImageThumbnails < ActiveRecord::Migration
-  def self.up
-		RefinerySetting[:image_thumbnails] = RefinerySetting[:image_thumbnails].merge({:grid => 'c135x135'})
-  end
-
-  def self.down
-		RefinerySetting[:image_thumbnails] = RefinerySetting[:image_thumbnails].delete_if {|key, value| key == :grid }
-  end
-end
