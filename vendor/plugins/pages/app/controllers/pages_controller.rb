1c72bf13724bd5be133c5ca75758b7995ff4376d
Philip Arndt <parndt@gmail.com>
2009-11-16 17:42:28 +1300

    whitespace

--- vendor/plugins/pages/app/controllers/pages_controller.rb
+++ vendor/plugins/pages/app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   def home
     @page = Page.find_by_link_url("/", :include => [:parts, :slugs])
     error_404 if @page.nil?
+    
     respond_to do |wants|
       wants.html
     end
@@ -18,6 +19,7 @@ class PagesController < ApplicationController
       first_live_child = @page.children.find_by_draft(false, :order => "position ASC")
       redirect_to first_live_child.url unless first_live_child.nil?
     end
+    
     respond_to do |wants|
       wants.html
     end
