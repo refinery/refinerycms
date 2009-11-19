a278bee87bfc68f1a910d19f0cee540677d50be1
Philip Arndt <parndt@gmail.com>
2009-11-20 10:52:55 +1300

    Improved the appearance of the images form in/out of dialogs and the recent activity list for long time_ago_in_words results

--- vendor/plugins/dashboard/app/helpers/admin/dashboard_helper.rb
+++ vendor/plugins/dashboard/app/helpers/admin/dashboard_helper.rb
@@ -1,19 +1,19 @@
 module Admin::DashboardHelper
-
+  
   def activity_message_for(record)
     unless (activity = Refinery::Plugins.active.find_activity_by_model(record.class)).nil? or activity.title.blank?
       title = eval "record.#{activity.title}"
-      link = link_to truncate(title, :length => 60),
-        eval("#{activity.url_prefix}admin_#{record.class.name.underscore.downcase}_url(record)"),
+      link = link_to truncate(title, :length => 60), 
+        eval("#{activity.url_prefix}admin_#{record.class.name.underscore.downcase}_url(record)"), 
         :title => "See '#{title}'"
-
+        
       # next work out which action occured and how long ago it happened
       action = record.updated_at.eql?(record.created_at) ? "created" : "updated"
-
+    
       message = "<td>#{refinery_icon_tag("#{eval("activity.#{action}_image")}")}</td>"
       message << "<td>#{link} #{record.class.name.titleize.downcase} was #{action}</td>"
-      message << "<td align='right'>#{time_ago_in_words(eval("record.#{action}_at"))} ago</td>"
+      message << "<td class='time_ago'>#{time_ago_in_words(eval("record.#{action}_at")).gsub("about ", "")} ago</td>"
     end
   end
-
+  
 end
