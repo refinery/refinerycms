f148aa96671dd3fc8fa9418e8c8a1e317c2fcda4
Philip Arndt <parndt@gmail.com>
2009-11-24 14:36:32 +1300

    Fix a case where the wymeditor fields were not getting updated properly when using save and continue. Moved continue_editing javascript to admin.js and we are using jQuery for many of the functions inside admin.js now.

--- public/javascripts/refinery/admin.js
+++ public/javascripts/refinery/admin.js
@@ -10,9 +10,9 @@ FastInit.addOnLoad(function()
 {
   init_tooltips();
   // focus first field in an admin form.
-  try{$(document.forms[0]).getInputs('text').first().focus();}catch(err){}
+  jQuery('form input[type=text]:first').focus();
 
-  $$('a([href*="dialog=true"])').each(function(anchor)
+  jQuery('a[href*="dialog=true"]').each(function(i, anchor)
   {
     ['modal=true', 'width=928', 'height=473', 'titlebar=true', 'draggable=true'].each(function(feature)
     {
@@ -21,7 +21,52 @@ FastInit.addOnLoad(function()
         anchor.href += "&" + feature;
       }
     });
-    
+
     tb_init(anchor);
+  });
+
+  jQuery('#submit_continue_button').bind('click', function(e) {
+    // ensure wymeditors are up to date.
+    if (jQuery(this).hasClass('wymupdate')) {
+      WYMeditor.INSTANCES.each(function(wym)
+      {
+        wym.update();
+      });
+    }
+
+    jQuery('#continue_editing').val(true);
+
+    if ((flash=$('flash')) != null) {
+      flash.hide();
+    }
+
+    jQuery.post(this.form.action, this.form.serialize(), function(data) {
+      if ((flash_container = $('flash_container')) != null) {
+        flash_container.update(data);
+
+        if ((flash = $('flash')) != null) {
+          flash.style.width = 'auto';
+          flash.appear();
+        }
+
+        jQuery('.errorExplanation').each(function(i, node) {
+          if (node.parentNode.id != 'flash_container') {
+            node.remove();
+          }
+        });
+
+        jQuery('.fieldWithErrors').each(function(i, field) {
+          field.removeClassName('fieldWithErrors').addClassName('field');
+        });
+
+        $('flash_container').scrollTo();
+
+        jQuery('#continue_editing').val(false);
+
+        jQuery('.fieldWithErrors:first :input:first').focus();
+      }
+    });
+
+    e.preventDefault();
   });
 });
