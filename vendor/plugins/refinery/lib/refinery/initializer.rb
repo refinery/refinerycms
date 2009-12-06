1f013963011f7e6ff0154ada8ffc88e00545d1ae
Philip Arndt <parndt@gmail.com>
2009-12-07 12:30:28 +1300

    provide the ability for lib/tasks to run within vendor/plugins when the project is installed from a gem and remove the require statement from the indexer rake task as this is now redundant.

--- vendor/plugins/refinery/lib/refinery/initializer.rb
+++ vendor/plugins/refinery/lib/refinery/initializer.rb
@@ -1,6 +1,6 @@
 begin
   # Try to include the rails initializer. If this isn't in a gem, this will fail.
-  require 'initializer'
+  require 'initializer' unless REFINERY_ROOT == RAILS_ROOT # A Refinery gem install's RAILS_ROOT is not the REFINERY_ROOT.
 rescue LoadError => load_error
 end
 
@@ -12,11 +12,25 @@ module Refinery
       end
     end
   end
+  if defined? Rails::Plugin::Loader
+    class GemedPluginLoader < Rails::Plugin::Loader
+      def add_plugin_load_paths
+        super
+        # add plugin lib paths to the $LOAD_PATH so that rake tasks etc. can be run when using a gem.
+        plugins.each do |plugin|
+          plugin.load_paths.reject { |path| path.scan(/refinerycms.+?vendor\/plugins\/.+?\/lib/).empty? }.each do |path|
+            $LOAD_PATH.unshift path
+          end
+        end
+      end
+    end
+  end
   if defined? Rails::Initializer
     class Initializer < Rails::Initializer
       def self.run(command = :process, configuration = Configuration.new)
         Rails.configuration = configuration
         configuration.reload_plugins = true if RAILS_ENV =~ /development/ and REFINERY_ROOT == RAILS_ROOT # seems to work, don't in gem.
+        configuration.plugin_loader = Refinery::GemedPluginLoader unless REFINERY_ROOT == RAILS_ROOT # only do this in a gem.
         super
       end
 
