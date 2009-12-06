19489a7f1685319dac7880707e59bc17d85ca3e1
Philip Arndt <parndt@gmail.com>
2009-12-07 12:27:44 +1300

    provide some protection where the home page is nil

--- vendor/plugins/pages/app/controllers/pages_controller.rb
+++ vendor/plugins/pages/app/controllers/pages_controller.rb
@@ -2,26 +2,30 @@ class PagesController < ApplicationController
 
   def home
     @page = Page.find_by_link_url("/", :include => [:parts, :slugs])
-    error_404 if @page.nil?
-
-    respond_to do |wants|
-      wants.html
+    if @page.nil?
+      error_404
+    else
+      respond_to do |wants|
+        wants.html
+      end
     end
   end
 
   def show
     @page = Page.find(params[:id], :include => [:parts, :slugs])
 
-    error_404 unless @page.live? or (logged_in? and current_user.authorized_plugins.include?("Pages"))
-
-    # if the admin wants this to be a "placeholder" page which goes to its first child, go to that instead.
-    if @page.skip_to_first_child
-      first_live_child = @page.children.find_by_draft(false, :order => "position ASC")
-      redirect_to first_live_child.url unless first_live_child.nil?
-    else
-      respond_to do |wants|
-        wants.html
+    if @page.live? or (logged_in? and current_user.authorized_plugins.include?("Pages"))
+      # if the admin wants this to be a "placeholder" page which goes to its first child, go to that instead.
+      if @page.skip_to_first_child
+        first_live_child = @page.children.find_by_draft(false, :order => "position ASC")
+        redirect_to first_live_child.url unless first_live_child.nil?
+      else
+        respond_to do |wants|
+          wants.html
+        end
       end
+    else
+      error_404
     end
   end
 
