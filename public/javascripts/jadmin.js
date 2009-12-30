04bfb73df996121df32910213c4e1ddb92c7b239
Hernan Fernandez <hernan@netflux.com.ar>
2009-12-30 14:28:59 -0300

    migrate image dialog

--- public/javascripts/jadmin.js
+++ public/javascripts/jadmin.js
@@ -202,109 +202,166 @@ var link_dialog = {
 }
 
 
-  var page_options = {
-    init: function(enable_parts, new_part_url, del_part_url){
-      this.enable_parts = enable_parts;
-      this.new_part_url = new_part_url;
-      this.del_part_url = del_part_url;
-      this.show_options();
-      this.title_type();
+var page_options = {
+  init: function(enable_parts, new_part_url, del_part_url){
+    this.enable_parts = enable_parts;
+    this.new_part_url = new_part_url;
+    this.del_part_url = del_part_url;
+    this.show_options();
+    this.title_type();
 
-      // Hook into the loaded function. This will be called when WYMeditor has done its thing.
-      WYMeditor.loaded = function(){
-        page_options.tabs = $j('#page-tabs').tabs();
-      }
-
-      if(this.enable_parts){
-        this.page_part_dialog();
-      }
-    },
-
-    show_options: function(){
-      $j('#toggle_advanced_options').click(function(ev){
-        ev.preventDefault();
-        $j('#more_options').toggle();
-      });
-    },
-
-    title_type: function(){
-      $j("input[name=page\[custom_title_type\]]").change(function(){
-        $j('#custom_title_text, #custom_title_image').hide();
-        $j('#custom_title_' + this.value).show();
-      });
-    },
-
-    page_part_dialog: function(){
-      $j('#new_page_part_dialog').dialog({
-        title: 'Create Content Section',
-        modal: true,
-        resizable: false,
-        autoOpen: false
-      });
-
-      $j('#add_page_part').click(function(ev){
-        ev.preventDefault();
-        $j('#new_page_part_dialog').dialog('open');
-      });
-
-      $j('#new_page_part_save').click(function(ev){
-        ev.preventDefault();
-
-        var part_title = $j('#new_page_part_title').val();
-
-        if(part_title.length > 0){
-          var tab_title = part_title.toLowerCase().gsub(" ", "_");
-
-          if ($j('#part_' + tab_title).size() == 0){
-            $j.get(page_options.new_part_url,
-                    {title: tab_title, part_index: $j('#new_page_part_index').val(), body: ''},
-                    function(data, status){
-                      // Add a new tab for the new content section.
-                      $j('#page_part_editors').append(data);
-                      page_options.tabs.tabs('add', '#part_' + tab_title, part_title);
-                      page_options.tabs.tabs('select', '#part_' + tab_title);
-                      // turn the new textarea into a wymeditor.
-                      $j('#page_parts_attributes_' + $j('#new_page_part_index').val() + "_body").wymeditor(wymeditor_boot_options);
-                      // Wipe the title and increment the index counter by one.
-                      $j('#new_page_part_index').val(parseInt($j('#new_page_part_index').val()) + 1);
-                      $j('#new_page_part_title').val('');
-                    }
-            );
-          }else{
-            alert("A content section with that title already exists, please choose another.");
-          }
-        }else{
-          alert("You have not entered a title for the content section, please enter one.");
-        }
-
-
-        $j('#new_page_part_dialog').dialog('close');
-      });
-
-      $j('#new_page_part_cancel').click(function(ev){
-        ev.preventDefault();
-        $j('#new_page_part_dialog').dialog('close');
-        $j('#new_page_part_title').val('');
-      });
-
-      $j('#delete_page_part').click(function(ev){
-        ev.preventDefault();
-        var stab_id = page_options.tabs.tabs('option', 'selected');
-        var part_id = $j('#page_parts_attributes_' + stab_id + '_id').val();
-        //console.log('stab_id: ' + stab_id + ' part_id: ' + part_id);
-
-        var result = confirm("This will remove the content section '" + $j('#page_parts .ui-tabs-selected a').html() + "' when the page is saved and erase all content that has been entered into it, Are you sure?");
-        if(part_id && result){
-          $j.ajax({
-            url: page_options.del_part_url + '/' + part_id,
-            type: 'DELETE'
-          });
-          page_options.tabs.tabs('remove', stab_id);
-          //WYMeditor.loaded();
-        }
-
-      });
-
+    // Hook into the loaded function. This will be called when WYMeditor has done its thing.
+    WYMeditor.loaded = function(){
+      page_options.tabs = $j('#page-tabs').tabs();
     }
 
+    if(this.enable_parts){
+      this.page_part_dialog();
+    }
+  },
+
+  show_options: function(){
+    $j('#toggle_advanced_options').click(function(ev){
+      ev.preventDefault();
+      $j('#more_options').toggle();
+    });
+  },
+
+  title_type: function(){
+    $j("input[name=page\[custom_title_type\]]").change(function(){
+      $j('#custom_title_text, #custom_title_image').hide();
+      $j('#custom_title_' + this.value).show();
+    });
+  },
+
+  page_part_dialog: function(){
+    $j('#new_page_part_dialog').dialog({
+      title: 'Create Content Section',
+      modal: true,
+      resizable: false,
+      autoOpen: false
+    });
+
+    $j('#add_page_part').click(function(ev){
+      ev.preventDefault();
+      $j('#new_page_part_dialog').dialog('open');
+    });
+
+    $j('#new_page_part_save').click(function(ev){
+      ev.preventDefault();
+
+      var part_title = $j('#new_page_part_title').val();
+
+      if(part_title.length > 0){
+        var tab_title = part_title.toLowerCase().gsub(" ", "_");
+
+        if ($j('#part_' + tab_title).size() == 0){
+          $j.get(page_options.new_part_url,
+                  {title: tab_title, part_index: $j('#new_page_part_index').val(), body: ''},
+                  function(data, status){
+                    // Add a new tab for the new content section.
+                    $j('#page_part_editors').append(data);
+                    page_options.tabs.tabs('add', '#part_' + tab_title, part_title);
+                    page_options.tabs.tabs('select', '#part_' + tab_title);
+                    // turn the new textarea into a wymeditor.
+                    $j('#page_parts_attributes_' + $j('#new_page_part_index').val() + "_body").wymeditor(wymeditor_boot_options);
+                    // Wipe the title and increment the index counter by one.
+                    $j('#new_page_part_index').val(parseInt($j('#new_page_part_index').val()) + 1);
+                    $j('#new_page_part_title').val('');
+                  }
+          );
+        }else{
+          alert("A content section with that title already exists, please choose another.");
+        }
+      }else{
+        alert("You have not entered a title for the content section, please enter one.");
+      }
+
+
+      $j('#new_page_part_dialog').dialog('close');
+    });
+
+    $j('#new_page_part_cancel').click(function(ev){
+      ev.preventDefault();
+      $j('#new_page_part_dialog').dialog('close');
+      $j('#new_page_part_title').val('');
+    });
+
+    $j('#delete_page_part').click(function(ev){
+      ev.preventDefault();
+      var stab_id = page_options.tabs.tabs('option', 'selected');
+      var part_id = $j('#page_parts_attributes_' + stab_id + '_id').val();
+      //console.log('stab_id: ' + stab_id + ' part_id: ' + part_id);
+
+      var result = confirm("This will remove the content section '" + $j('#page_parts .ui-tabs-selected a').html() + "' when the page is saved and erase all content that has been entered into it, Are you sure?");
+      if(part_id && result){
+        $j.ajax({
+          url: page_options.del_part_url + '/' + part_id,
+          type: 'DELETE'
+        });
+        page_options.tabs.tabs('remove', stab_id);
+        //WYMeditor.loaded();
+      }
+
+    });
+
   }
+
+}
+
+var image_dialog = {
+  init: function(){
+    this.init_tabs();
+    this.init_select();
+  },
+
+  init_tabs: function(){
+    var radios = $j('#dialog_menu_left input:radio');
+    var selected = radios.filter('.selected_radio')[0] || radios[0];
+
+    radios.each(function(){
+      $j(this).click(function(){
+        image_dialog.switch_area(this);
+      });
+    });
+
+    selected.checked = true;
+    image_dialog.switch_area(selected);
+  },
+
+  switch_area: function(area){
+    $j('#dialog_menu_left .selected_radio').removeClass('selected_radio');
+    $j(area).parent().addClass('selected_radio');
+    $j('#dialog_main .dialog_area').hide();
+    $j('#' + area.value + '_area').show();
+  },
+
+  init_select: function(){
+    $j('#existing_image_area_content ul li img').click(function(){
+        image_dialog.set_image(this);
+    });
+    //Select any currently selected, just uploaded...
+    $j('#existing_image_area_content ul li.selected img').each(function(){
+      image_dialog.set_image(this);
+    });
+  },
+
+  set_image: function(img){
+    $j('#existing_image_area_content ul li.selected').removeClass('selected');
+
+    $j(img).parent().addClass('selected');
+    var imageUrl = parseURL(img.src);
+    var relevant_src = imageUrl.pathname.replace('_dialog_thumb', '');
+
+    if(imageUrl.hostname.match(/s3.amazonaws.com/)){
+      relevant_src = imageUrl.protocol + '//' + imageUrl.host + relevant_src;
+    }
+
+    try {
+      parent.document.getElementById('wym_src').value = relevant_src;
+      parent.document.getElementById('wym_title').value = img.title;
+      parent.document.getElementById('wym_alt').value = img.alt;
+    }
+    catch($e){}
+  }
+}
