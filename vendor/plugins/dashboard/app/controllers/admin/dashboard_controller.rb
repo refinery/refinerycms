7b8ea9213209ed3d0e56430101d18f8a31857a81
Philip Arndt <parndt@gmail.com>
2010-01-11 17:27:40 +1300

    added better tracing of errors on dashboard and do more to try to prevent these errors

--- vendor/plugins/dashboard/app/controllers/admin/dashboard_controller.rb
+++ vendor/plugins/dashboard/app/controllers/admin/dashboard_controller.rb
@@ -3,14 +3,25 @@ class Admin::DashboardController < Admin::BaseController
   def index
     @recent_activity = []
     Refinery::Plugins.active.each do |plugin|
-      plugin.activity.each do |activity|
-        include_associations = []
-        include_associations.push(:slugs) if activity.class.methods.include? "find_one_with_friendly" # wee performance gain if slugs are used.
-        @recent_activity += activity.class.find(:all, :order => activity.order, :limit => activity.limit, :conditions => activity.conditions, :include => include_associations)
-      end rescue nil
+      begin
+        plugin.activity.each do |activity|
+          include_associations = []
+          include_associations.push(:slugs) if activity.class.methods.include? "find_one_with_friendly" # wee performance gain if slugs are used.
+
+          @recent_activity += activity.class.find(:all,
+            :conditions => activity.conditions,
+            :order => activity.order,
+            :limit => activity.limit,
+            :include => include_associations
+          )
+        end
+      rescue
+        logger.info "#{$!.class.name} raised while getting recent activity for dashboard."
+        logger.warn $!.backtrace.collect { |b| " > #{b}" }.join("\n")
+      end
     end
 
-    @recent_activity = @recent_activity.sort{|x,y| y.updated_at <=> x.updated_at}[0..((RefinerySetting.activity_show_limit ||= 15)-1)]
+    @recent_activity = @recent_activity.compact.uniq.sort{|x,y| y.updated_at <=> x.updated_at}[0..(RefinerySetting.find_or_set(:activity_show_limit, 15)-1)]
   end
 
 end
