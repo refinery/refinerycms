a0a1f4debdc3a362adfd16401213541f5e8c06c8
Philip Arndt <parndt@gmail.com>
2010-01-12 13:47:39 +1300

    use send rather than eval

--- vendor/plugins/dashboard/app/helpers/admin/dashboard_helper.rb
+++ vendor/plugins/dashboard/app/helpers/admin/dashboard_helper.rb
@@ -2,7 +2,7 @@ module Admin::DashboardHelper
 
   def activity_message_for(record)
     unless (activity = Refinery::Plugins.active.find_activity_by_model(record.class)).nil? or activity.title.blank?
-      title = eval "record.#{activity.title}"
+      title = record.send activity.title
       link = link_to truncate(title, :length => 45),
         eval("#{activity.url_prefix}admin_#{record.class.name.underscore.downcase}_url(record)"),
         :title => "See '#{title}'"
@@ -10,9 +10,9 @@ module Admin::DashboardHelper
       # next work out which action occured and how long ago it happened
       action = record.updated_at.eql?(record.created_at) ? "created" : "updated"
 
-      message = "<td>#{refinery_icon_tag("#{eval("activity.#{action}_image")}")}</td>"
+      message = "<td>#{refinery_icon_tag("#{activity.send "#{action}_image"}")}</td>"
       message << "<td>#{link} #{record.class.name.titleize.downcase} was #{action}</td>"
-      message << "<td class='time_ago'>#{time_ago_in_words(eval("record.#{action}_at")).gsub("about ", "")} ago</td>"
+      message << "<td class='time_ago'>#{time_ago_in_words(record.send "#{action}_at").gsub("about ", "")} ago</td>"
     end
   end
 
