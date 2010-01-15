1ed1d137e9dc656f27c87ff1db7cfe13a24911b3
Philip Arndt <parndt@gmail.com>
2010-01-15 13:53:43 +1300

    Fix truncate for Hpricot 0.8.2 and allow this to be used (or 0.8.1)

--- config/environment.rb
+++ config/environment.rb
@@ -69,6 +69,6 @@ eval("#{(defined? Refinery::Initializer) ? Refinery : Rails}::Initializer").run 
   config.gem "rails", :version => ">= 2.3.5", :lib => "rails"
   config.gem "aasm", :version => ">= 2.1.3", :lib => "aasm", :source => "http://gemcutter.org"
   config.gem "slim_scrooge", :version => ">= 1.0.3", :lib => "slim_scrooge", :source => "http://gemcutter.org" #unless RUBY_PLATFORM =~ /mswin|mingw/ # kill gem when windows is running.
-  config.gem "hpricot", :version => "= 0.8.1", :lib => "hpricot", :source => "http://gemcutter.org"
+  config.gem "hpricot", :version => ">= 0.8.1", :lib => "hpricot", :source => "http://gemcutter.org"
   #===REFINERY END OF REQUIRED GEMS===
 end
