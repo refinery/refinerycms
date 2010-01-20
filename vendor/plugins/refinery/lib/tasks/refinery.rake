81eb013ff50b60d796d5f86026847d330749ee42
djones <dgjones@gmail.com>
2010-01-20 14:38:54 +1300

    added documentation for themes

--- vendor/plugins/refinery/lib/tasks/refinery.rake
+++ vendor/plugins/refinery/lib/tasks/refinery.rake
@@ -32,4 +32,3 @@ namespace :refinery do
   end
 
 end
-
--- vendor/plugins/themes/config/routes.rb
+++ vendor/plugins/themes/config/routes.rb
@@ -4,6 +4,7 @@ ActionController::Routing::Routes.draw do |map|
     admin.resources :themes, :member => {:preview_image => :get, :activate => :get}
   end
 
+	# allows theme files that are not in the Rails public directory to be served back to the client
  	map.connect 'stylesheets/theme/:filename*extension', :controller => 'themes', :action => 'stylesheets'
   map.connect 'javascripts/theme/:filename*extension', :controller => 'themes', :action => 'javascripts'
   map.connect 'images/theme/:filename*extension', :controller => 'themes', :action => 'images'
