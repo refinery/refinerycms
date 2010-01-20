4fc8b5672292a5d42734878debb6f38ba9ac056e
Philip Arndt <parndt@gmail.com>
2010-01-20 16:20:30 +1300

    Make themes support play nice with rake and the boot process and change theme_directory_is_writable to a class method as it doesn't really require an instance to work.

--- vendor/plugins/themes/app/models/theme.rb
+++ vendor/plugins/themes/app/models/theme.rb
@@ -1,5 +1,10 @@
-require 'zip/zip'
-require 'zip/zipfilesystem'
+begin
+  require 'zip/zip'
+  require 'zip/zipfilesystem'
+rescue LoadError => load_error
+  # Otherwise, this will stop rake tasks running
+  "*** rubyzip gem needs to be installed to use themes. ***"
+end
 
 class Theme < ActiveRecord::Base
 
@@ -12,10 +17,10 @@ class Theme < ActiveRecord::Base
 
 	# Once a zip is uploaded it unzips it into the themes directory if writable
 	def after_attachment_saved
-		if theme_directory_is_writable?
+		if Theme::directory_is_writable?
 			# make the folder for the them
 			FileUtils.mkdir(self.theme_path) unless File.exists? self.theme_path
-		
+
 			# extracts the contents of the zip file into the theme directory
 		 	Zip::ZipFile.foreach(self.filename) do |entry|
 				FileUtils.mkdir_p(File.dirname("#{theme_path}/#{entry}"))
@@ -25,33 +30,33 @@ class Theme < ActiveRecord::Base
 			raise "Theme directory not writable"
 		end
 	end
-	
+
 	def theme_folder_title
 		File.basename(self.filename).split(".").first
 	end
-	
+
 	def theme_path
 		File.join(RAILS_ROOT, "themes", theme_folder_title)
 	end
-	
+
 	def preview_image
 		File.join(theme_path, "preview.png")
 	end
-	
+
 	def read_theme
 		self.title = File.basename(self.filename).split(".").first.titleize
-		
+
 		if File.exists? File.join(theme_path, "LICENSE")
 			self.license =  File.open(File.join(theme_path, "LICENSE")).read
 		end
-		
+
 		if File.exists? File.join(theme_path, "README")
 			self.description =  File.open(File.join(theme_path, "README")).read
 		end
 	end
 
-	def theme_directory_is_writable?
+	def self.directory_is_writable?
 		File.writable? File.join(RAILS_ROOT, "themes") # Heroku users will receive false here
 	end
-	
+
 end
