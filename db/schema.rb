6d4dc00b543e40ef35ef55a31fb647c167511e04
Philip Arndt <parndt@gmail.com>
2010-01-25 16:42:13 +1300

    make it easier for people to upgrade their plugin security for the new name scheme

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100125003944) do
+ActiveRecord::Schema.define(:version => 20100125033146) do
 
   create_table "images", :force => true do |t|
     t.integer  "parent_id"
