a4d71bf8303f991c775e6dcc9d67454ce9ba8c3a
djones <dgjones@gmail.com>
2010-02-01 14:12:46 +1300

    added page unit tests and basic fixtures to test deletability, and page paths

--- test/unit/page_test.rb
+++ test/unit/page_test.rb
@@ -1,10 +1,33 @@
 require 'test_helper'
 
 class PageTest < ActiveSupport::TestCase
-
+	
+	fixtures :pages
+	
+	def setup
+		@new_page = Page.new
+	end
+	
   def test_should_not_save_page_without_title
-    page = Page.new
-		assert !page.save, "Saved the page without a title"
+		assert !@new_page.save, "Saved the page without a title"
   end
+
+	def test_should_stop_system_pages_from_being_deleted
+		assert_equal false, pages(:home_page).deletable?
+		assert_equal false, pages(:page_not_found).destroy
+	end
+	
+	def test_should_allow_normal_pages_to_be_deleted
+		assert_equal true, pages(:services_page).deletable?
+	end
+	
+	def test_should_stop_custom_link_pages_from_being_deleted
+		assert_equal false, pages(:contact_us).deletable?
+	end
+	
+	def test_should_have_custom_seo_settings
+		assert_equal "Services", pages(:services_page).path
+		assert_equal "Secret Product - Secret information about secret product", pages(:draft_page_child).path
+	end
 
 end
