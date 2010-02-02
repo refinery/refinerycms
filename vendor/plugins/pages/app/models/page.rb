f12449df8a425fcfd516020bf6a0551fc77d4dac
djones <dgjones@gmail.com>
2010-02-02 16:56:38 +1300

    remove the relationship between pages and images. Now we have page parts you use that mechanism to display custom content like images. We don't need to rely on pages having a direct link with images anymore.

--- vendor/plugins/pages/app/models/page.rb
+++ vendor/plugins/pages/app/models/page.rb
@@ -6,9 +6,6 @@ class Page < ActiveRecord::Base
 
   has_friendly_id :title, :use_slug => true, :strip_diacritics => true
 
-  belongs_to :image
-  belongs_to :custom_title_image, :class_name => "Image"
-
   has_many :parts, :class_name => "PagePart", :order => "position ASC"
   accepts_nested_attributes_for :parts, :allow_destroy => true
 	
