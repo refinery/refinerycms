88c423b20c0f75147e7bf3aaf638c687e62b54b0
Philip Arndt <parndt@gmail.com>
2010-02-05 10:41:57 +1300

    this may as well say the new version

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 # Freeze to a specific version of refinerycms when running as a gem
-# REFINERY_GEM_VERSION = '0.9.5.29' unless defined? REFINERY_GEM_VERSION
+# REFINERY_GEM_VERSION = '0.9.6' unless defined? REFINERY_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
