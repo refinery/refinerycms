dbe1c81d62fd2d542830f74039fbde0c249284dd
Philip Arndt <parndt@gmail.com>
2010-02-09 16:38:52 +1300

    Added the ability to use nested routes into the Dashboard Activity. Removed the use of eval from dashboard_helper and cleaned up the init files so that they are not all scrunched up.

--- vendor/plugins/news/rails/init.rb
+++ vendor/plugins/news/rails/init.rb
@@ -3,5 +3,9 @@ Refinery::Plugin.register do |plugin|
   plugin.description = "Provides a blog-like news section"
   plugin.version = 1.0
   plugin.menu_match = /admin\/((news)|(news_items))$/
-  plugin.activity = {:class => NewsItem, :title => 'title', :url_prefix => 'edit'}
+  plugin.activity = {
+    :class => NewsItem,
+    :title => 'title',
+    :url_prefix => 'edit'
+  }
 end
