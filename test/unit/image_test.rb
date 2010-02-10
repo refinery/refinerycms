266a8449c649b2c02110bb9eebf0cba2b3b8f7cd
djones <dgjones@gmail.com>
2010-02-10 14:52:14 +1300

    get better titles from filenames. also added tests

--- test/unit/image_test.rb
+++ test/unit/image_test.rb
@@ -5,10 +5,18 @@ class ImageTest < ActiveSupport::TestCase
 	fixtures :images
 
 	def test_titles
+		assert_equal "The World", images(:the_world).title
+		assert_equal "Car Wallpapers19", images(:our_car).title
 	end
 	
 	def test_per_page
-		
+		assert_equal 18, Image.per_page(dialog = true)
+		assert_equal 20, Image.per_page # dialog = false
+	end
+	
+	def test_attachment_fu_options
+		assert_equal 50.megabytes, Image.attachment_options[:max_size]
+		assert_equal 'Rmagick', Image.attachment_options[:processor]
 	end
 
 end
