3b960dc4ff0b33cdb0fa0ec1d21bfe0e5ca7a7e5
Philip Arndt <parndt@gmail.com>
2010-02-16 09:52:59 +1300

    Fix logic errors on content_page template and fix the refinery and refinery-update-core commands to more correctly use config/environment.rb and config/application.rb

--- vendor/plugins/themes/lib/theme_server.rb
+++ vendor/plugins/themes/lib/theme_server.rb
@@ -12,7 +12,7 @@ class ThemeServer
     if env["PATH_INFO"] =~ /^\/theme/
       relative_path = env["PATH_INFO"].gsub(/^\/theme\//, '')
 
-      if (file_path = Rails.root.join "themes", RefinerySetting[:theme], relative_path).exist?
+      if (file_path = Rails.root.join("themes", RefinerySetting[:theme], relative_path)).exist?
         # generate an etag for client-side caching.
         etag = Digest::MD5.hexdigest("#{file_path.to_s}#{file_path.mtime}")
         unless env["HTTP_IF_NONE_MATCH"] == etag
