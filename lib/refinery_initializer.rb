a5a431a14930723741c17cd80c29b6d2ad0219ec
Philip Arndt <parndt@gmail.com>
2010-02-16 10:11:11 +1300

    fix updating the core and put the attachment fu patch in a more logical place and include it from the initializer not environment.rb

--- lib/refinery_initializer.rb
+++ lib/refinery_initializer.rb
@@ -7,3 +7,6 @@ unless REFINERY_ROOT == RAILS_ROOT # e.g. only if we're in a gem.
   require 'refinery'
   require 'refinery/initializer'
 end
+
+# Pull in attachment_fu patch for windows
+require 'refinery/attachment_fu_patch' if RUBY_PLATFORM =~ /mswin/
