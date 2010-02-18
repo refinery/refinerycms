7ea0b6304506ad08d3ab543fea02bdaeca77f9e5
Philip Arndt <parndt@gmail.com>
2010-02-18 17:15:45 +1300

    get rid of some code smells and complexities

--- lib/refinery_initializer.rb
+++ lib/refinery_initializer.rb
@@ -1,11 +1,10 @@
 require 'pathname'
-begin
-  require 'vendor/plugins/refinery/lib/refinery'
-rescue LoadError => load_error
-  # We won't be able to load this if we're in the gem as there is no such thing.
+root_directory = File.expand_path(File.dirname(__FILE__) << "/..")
+if File.exists?(File.join(root_directory, %w(vendor plugins refinery lib refinery)))
+  require 'vendor/plugins/refinery/lib/refinery' # this won't exist when using a gem.
 end
 
-refinery_root = Pathname.new((defined?(Refinery.root) ? Refinery.root.to_s : File.expand_path(File.dirname(__FILE__) << "/.."))) 
+refinery_root = Pathname.new((defined?(Refinery.root) ? Refinery.root.to_s : root_directory))
 unless refinery_root == (defined?(Rails.root) ? Rails.root : Pathname.new(RAILS_ROOT)) # e.g. only if we're in a gem.
   $LOAD_PATH.unshift refinery_root.join("vendor", "plugins").to_s
   $LOAD_PATH.unshift refinery_root.join("vendor", "plugins", "refinery", "lib").to_s
