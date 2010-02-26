490c0f0d9833c0a2efa375924c60e9caf05556a3
djones <dgjones@gmail.com>
2010-02-26 17:04:02 +1300

    auto login with tests on setup but still allow logout for testing security.

--- test/functional/images_controller_test.rb
+++ test/functional/images_controller_test.rb
@@ -11,26 +11,58 @@ class ImagesControllerTest < ActionController::TestCase
     @controller = Admin::ImagesController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
+    
+    login_as(:quentin)
   end
 
   def test_should_get_index
-    login_as(:quentin)
-
     get :index
     assert_response :success
     assert_not_nil assigns(:images)
     assert assigns(:images).size, Image.count
   end
+
+  def test_new_image_form
+    get :new
+    assert_not_nil assigns(:image)
+    assert_not_nil assigns(:url_override)
+  end
+  
+  def test_search
+    get :index, :search => "Car"
+    
+    assert 1, assigns(:images).size
+    assert images(:the_world), assigns(:images).first
+    assert_not_nil assigns(:images)
+  end
   
   def test_should_require_login_and_redirect
-
+    logout
+    
     get :index
     assert_response :redirect
     assert_nil assigns(:images)
   end
   
-  # we need to somehow make it so that recent activity comes
-  # into tests so we can check the order.
-  # not sure why the recent activity assign has no activity
+  def test_edit
+    get :edit, :id => images(:the_world).id
+    
+    assert_response :success
+    
+    assert_not_nil assigns(:image)
+    assert_equal images(:the_world), assigns(:image)
+  end
   
+  def test_insert
+    
+  end
+  
+  def test_create
+    
+  end
+  
+  def test_destroy
+    
+  end
+
 end
