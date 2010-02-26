f915739371a84fb82282ab5741d8efc519be844d
djones <dgjones@gmail.com>
2010-02-26 15:42:11 +1300

    working towards more functional tests

--- test/functional/images_controller_test.rb
+++ test/functional/images_controller_test.rb
@@ -1,35 +1,32 @@
 require File.dirname(__FILE__) + '/../test_helper'
-require 'admin/dashboard_controller'
+require 'admin/images_controller'
 
-class Admin::DashboardController; def rescue_action(e) raise e end; end
+class Admin::ImagesController; def rescue_action(e) raise e end; end
 
-class DashboardControllerTest < ActionController::TestCase
+class ImagesControllerTest < ActionController::TestCase
   
-  fixtures :users, :pages
+  fixtures :users, :images
   
   def setup
-    @controller = Admin::DashboardController.new
+    @controller = Admin::ImagesController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
   end
   
   def test_should_get_index
     login_as(:quentin)
-    
-    # update a page to generate recent activity
-    pages(:home_page).update_attribute(:title, "Welcome")
-    
+
     get :index
     assert_response :success
-    assert_not_nil assigns(:recent_activity)
-    
-    raise assigns(:recent_activity).inspect
+    assert_not_nil assigns(:images)
+    assert assigns(:images).size, Image.count
   end
   
   def test_should_require_login_and_redirect
+
     get :index
     assert_response :redirect
-    assert_nil assigns(:recent_activity)
+    assert_nil assigns(:images)
   end
   
   # we need to somehow make it so that recent activity comes
