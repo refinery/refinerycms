ad70409f23e4883651545301afc6868c48eb462f
suweller <me@suweller.com>
2010-02-28 23:55:26 +0800

    Automatically translate all activerecord labels

--- vendor/plugins/refinery/lib/refinery/form_helpers.rb
+++ vendor/plugins/refinery/lib/refinery/form_helpers.rb
@@ -2,8 +2,18 @@ ActionView::Helpers::FormHelper.module_eval do
 
   def required_label(object_name, method, text = nil, options = {})
     options = {:class => "required"}.merge!(options)
-    text ||= "#{method.to_s.humanize} *"
-    label(object_name, method, text, options)
+
+    label(object_name, method, "#{label_humanize_text(object_name, method, text, options)} *", options)
+  end
+
+
+  def label_humanize_text object_name, method, text = nil, options = {}
+    if text.blank?
+      text = object_name.classify.constantize.respond_to?(:human_attribute_name) ? object_name.classify.constantize.human_attribute_name(method) : method.to_s
+    else
+      text = text.to_s
+    end
+    text.humanize
   end
 
 end
@@ -25,4 +35,29 @@ ActionView::Helpers::FormTagHelper.module_eval do
     label_tag(name, text, options)
   end
 
+end
+
+
+# I18n labels automatically
+module ActionView
+  module Helpers
+    class InstanceTag
+      def to_label_tag(text = nil, options = {})
+        options = options.stringify_keys
+        name_and_id = options.dup
+        add_default_name_and_id(name_and_id)
+        options.delete("index")
+        options["for"] ||= name_and_id["id"]
+        if text.blank?
+          content = method_name.humanize
+          if object.class.respond_to?(:human_attribute_name)
+            content = object.class.human_attribute_name(method_name)
+          end
+        else
+          content = text.to_s
+        end
+        label_tag(name_and_id["id"], content, options)
+      end
+    end
+  end
 end
