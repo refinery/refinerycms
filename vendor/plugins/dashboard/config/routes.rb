8290ec347f2857e159d317440754ae7df4a57cb9
Alex Coles <alex@alexcolesportfolio.com>
2010-03-03 18:40:33 +0100

    Revert a Rails 3 Router API change because of failing tests
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- vendor/plugins/dashboard/config/routes.rb
+++ vendor/plugins/dashboard/config/routes.rb
@@ -1,5 +1,8 @@
-Rails::Application.routes.draw do
-  namespace(:admin) do
-    resources :dashboard
+Rails::Application.routes.draw do |map|
+  map.namespace(:admin) do |admin|
+    admin.resources :dashboard
   end
+  #namespace(:admin) do
+  #  resources :dashboard
+  #end
 end
