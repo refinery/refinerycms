f5570f8a07cb9988d91e2aa007e6e0d7427f182a
Philip Arndt <parndt@gmail.com>
2010-03-04 11:27:51 +1300

    fix whitespace (again?) and reduce friendly_id gem requirement to 2.2.2 because this works too and provides greater compatibility with previous releases of Refinery

--- vendor/plugins/refinery/lib/generators/refinery/templates/model.rb
+++ vendor/plugins/refinery/lib/generators/refinery/templates/model.rb
@@ -5,7 +5,7 @@ class <%= class_name %> < ActiveRecord::Base
 
   validates_presence_of :<%= attributes.first.name %>
   validates_uniqueness_of :<%= attributes.first.name %>
-  
+
 <% attributes.collect{|a| a if a.type.to_s == 'image'}.compact.uniq.each do |a| -%>
   belongs_to :<%= a.name.gsub("_id", "") %><%= ", :class_name => 'Image'" unless a.name =~ /^image(_id)?$/ %>
 <% end -%>
