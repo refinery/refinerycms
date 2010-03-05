58030a135d3cac81bbf45605dd41d5f14088426f
Tyler Rick <github.com@tylerrick.com>
2010-03-04 17:15:32 -0800

    Added config.log_level = :debug to config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
-config.log_level = :info
+config.log_level = :debug
 
 # Uncomment the following lines if you're getting
 # "A copy of XX has been removed from the module tree but is still active!"
