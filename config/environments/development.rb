9b06589c8caed89db321e3221edb6160c2ea43f6
Tyler Rick <github.com@tylerrick.com>
2010-03-07 16:10:28 -0800

    Set config.reload_plugins = true because that would make things less tedious so we don't have to restart the server every time we change a controller in a plugin.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 # Settings specified here will take precedence over those in config/environment.rb
 
+config.reload_plugins = true
+
 # In the development environment your application's code is reloaded on
 # every request.  This slows down response time but is perfect for development
 # since you don't have to restart the webserver when you make code changes.
