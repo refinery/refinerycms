21ea40b7993fc59d10da2935653260fdf96fba02
Alex Coles <alex@alexcolesportfolio.com>
2010-03-10 19:43:29 +0100

    Rails 3 + Engines: Fix an incorrect controller path
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- vendor/engines/refinery_settings/lib/refinery_settings.rb
+++ vendor/engines/refinery_settings/lib/refinery_settings.rb
@@ -3,6 +3,7 @@ Refinery::Plugin.register do |plugin|
   plugin.description = "Manage Refinery settings"
   plugin.version = 1.0
   plugin.menu_match = /admin\/(refinery_)?settings$/
+  plugin.url = {:controller => "admin/refinery_settings"}
   plugin.activity = {
     :class => 'RefinerySetting',
     :title => 'title',
