4a4b965396e9cfe19faf4da7cff5bf256ff9117c
Philip Arndt <parndt@gmail.com>
2010-03-18 12:43:39 +1300

    update to latest version of parndt/slim_scrooge

--- vendor/plugins/slim_scrooge/rails/init.rb
+++ vendor/plugins/slim_scrooge/rails/init.rb
@@ -1,6 +1,1 @@
-unless File.exists?(File.join(File.dirname(__FILE__), "../", "ext", "Makefile"))
-  Dir.chdir(File.join(File.dirname(__FILE__), "../", "ext"))
-  `rake`
-end
-
 require 'slim_scrooge.rb'
