8c4fba277a83fe69910b9777bb57a115e1e74c08
Philip Arndt <parndt@gmail.com>
2010-03-27 18:34:51 +1300

    updated yardopts and remove this plugin until it can be included cleanly

--- vendor/plugins/slim_scrooge/test/schema/schema.rb
+++
@@ -1,5 +0,0 @@
-ActiveRecord::Schema.define do
-  create_table :courses, :force => true do |t|
-    t.column :name, :string, :null => false
-  end
-end
--- vendor/plugins/slim_scrooge/test/setup.rb
+++
@@ -1,5 +0,0 @@
-require 'rubygems'
-require 'active_record'
-require 'active_record/connection_adapters/mysql_adapter'
-$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'slim_scrooge'
