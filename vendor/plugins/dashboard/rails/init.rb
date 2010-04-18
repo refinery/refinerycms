a7814e30d68cb5624d0232622bde4e2f7f1d2fd3
Philip Arndt <parndt@gmail.com>
2010-04-19 11:10:40 +1200

    fix typo and remove comment and menu match -- seems unnecessary

--- vendor/plugins/dashboard/rails/init.rb
+++ vendor/plugins/dashboard/rails/init.rb
@@ -3,8 +3,6 @@ Refinery::Plugin.register do |plugin|
   plugin.title = "Dashboard"
   plugin.description = "Gives an overview of activity in Refinery"
   plugin.directory = "dashboard"
-  #TODO Find out why the menu_match is necessary; does the magic magic in Refinery::Plugin fail?
-  plugin.menu_match = /admin\/dashboard/
   plugin.version = 1.0
   plugin.always_allow_access = true
 end
