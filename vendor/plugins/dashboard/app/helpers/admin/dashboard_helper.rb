1ef68371b1a97bcf10c3d7d612b0540c04a602f9
unixcharles <unixcharles@gmail.com>
2010-04-27 15:54:39 -0400

    french locales
    french locales

--- vendor/plugins/dashboard/app/helpers/admin/dashboard_helper.rb
+++ vendor/plugins/dashboard/app/helpers/admin/dashboard_helper.rb
@@ -13,7 +13,7 @@ module Admin::DashboardHelper
 
       message = "<td>#{refinery_icon_tag("#{activity.send "#{action}_image"}")}</td>"
       message << "<td>#{link} #{record.class.human_name.titleize.downcase} #{t(action)}</td>"
-      message << "<td class='time_ago'>#{time_ago_in_words(record.send "#{action}_at")} #{t(:ago)}</td>"
+      message << "<td class='time_ago'>#{t(:ago, :time => time_ago_in_words(record.send "#{action}_at"))}</td>"
     end
   end
 
