4ebbd027689ca28b56a99d89df627cbd0e8a1ade
Philip Arndt <parndt@gmail.com>
2010-05-20 16:19:43 +1200

    fix up the blanket find and replace that went a bit far.

--- vendor/plugins/refinery/lib/generators/refinery/templates/config/routes.rb
+++ vendor/plugins/refinery/lib/generators/refinery/templates/config/routes.rb
@@ -2,6 +2,6 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :<%= class_name.pluralize.underscore.downcase %>
 
   map.namespace(:admin, :path_prefix => 'refinery') do |admin|
-    admin.resources :<%= class_name.pluralize.underscore.downcase %>, :collection => {:update_positions => :post}, :collection => {:update_positions => :post}
+    admin.resources :<%= class_name.pluralize.underscore.downcase %>, :collection => {:update_positions => :post}
   end
 end
