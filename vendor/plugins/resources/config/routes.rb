e83425039985dc615cfabb14347f62ada3075163
Philip Arndt <parndt@gmail.com>
2010-05-20 16:21:08 +1200

    And these resources will never sort.

--- vendor/plugins/resources/config/routes.rb
+++ vendor/plugins/resources/config/routes.rb
@@ -3,7 +3,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :resources
 
   map.namespace(:admin, :path_prefix => 'refinery') do |admin|
-    admin.resources :resources, :collection => {:insert => :get, :update_positions => :post}
+    admin.resources :resources, :collection => {:insert => :get}
   end
 
 end
