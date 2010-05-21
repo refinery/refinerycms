0290052d2f3713c807bd9f19e013ef2e8b1bfca1
Philip Arndt <parndt@gmail.com>
2010-05-21 17:17:33 +1200

    Update wymeditor to rc1 including some other fixes from jonatan lundis to fix cursor position in firefox.

--- public/javascripts/refinery/boot_wym.js
+++ public/javascripts/refinery/boot_wym.js
@@ -15,6 +15,10 @@ var wymeditor_boot_options = $.extend({
     ,{'name': 'Italic', 'title': 'Emphasis', 'css': 'wym_tools_emphasis'}
     ,{'name': 'InsertOrderedList', 'title': 'Ordered_List', 'css': 'wym_tools_ordered_list'}
     ,{'name': 'InsertUnorderedList', 'title': 'Unordered_List', 'css': 'wym_tools_unordered_list'}
+    /*,{'name': 'Indent', 'title': 'Indent', 'css': 'wym_tools_indent'}
+    ,{'name': 'Outdent', 'title': 'Outdent', 'css': 'wym_tools_outdent'}
+    ,{'name': 'Undo', 'title': 'Undo', 'css': 'wym_tools_undo'}
+    ,{'name': 'Redo', 'title': 'Redo', 'css': 'wym_tools_redo'}*/
     ,{'name': 'CreateLink', 'title': 'Link', 'css': 'wym_tools_link'}
     ,{'name': 'Unlink', 'title': 'Unlink', 'css': 'wym_tools_unlink'}
     ,{'name': 'InsertImage', 'title': 'Image', 'css': 'wym_tools_image'}
