f1d92f9886fcf7eb6151dcb9a03eba9187a0cf19
djones <dgjones@gmail.com>
2010-05-22 14:04:39 +1200

    and here's the migration for that change

---
+++ db/migrate/20100522020225_rename_permissions_for_resources_plugin_to_files.rb
@@ -0,0 +1,15 @@
+class RenamePermissionsForResourcesPluginToFiles < ActiveRecord::Migration
+  
+  def self.up
+    UserPlugin.find_all_by_title("Resources").each do |up|
+      up.update_attribute(:title, "Files")
+    end
+  end
+
+  def self.down
+    UserPlugin.find_by_title("Files").each do |up|
+      up.update_attribute(:title, "Resources")
+    end
+  end
+  
+end
