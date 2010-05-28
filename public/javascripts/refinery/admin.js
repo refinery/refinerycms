f16cfcbaba92135e09dfacf95d80817abfb95d94
Philip Arndt <parndt@gmail.com>
2010-05-28 21:22:34 +1200

    it's just little

--- public/javascripts/refinery/admin.js
+++ public/javascripts/refinery/admin.js
@@ -427,10 +427,7 @@ var page_options = {
   show_options: function(){
     $('#toggle_advanced_options').click(function(e){
       e.preventDefault();
-      $('#more_options').animate({
-        opacity: 'toggle'
-        , height: 'toggle'
-      }, 250);
+      $('#more_options').animate({opacity: 'toggle', height: 'toggle'}, 250);
 
       $('html,body').animate({
         scrollTop: $('#toggle_advanced_options').parent().offset().top
