c9d0489c42e7250161c05d2cde3dc8af1d0222ab
Philip Arndt <parndt@gmail.com>
2010-05-28 16:31:28 +1200

    remove tooltips from recent activity.

--- vendor/plugins/dashboard/app/helpers/admin/dashboard_helper.rb
+++ vendor/plugins/dashboard/app/helpers/admin/dashboard_helper.rb
@@ -2,15 +2,12 @@ module Admin::DashboardHelper
 
   def activity_message_for(record)
     if (activity = Refinery::Plugins.active.find_activity_by_model(record.class)).present? and activity.title.present?
-      title = h(record.send(activity.title))
-
-      # next work out which action occured
+      # work out which action occured
       action = record.updated_at.eql?(record.created_at) ? "created" : "updated"
 
       # now create a link to the notification's corresponding record.
-      link_to("#{truncate(title.to_s, :length => 45)} #{record.class.name.titleize.downcase} was #{action}",
-              eval("#{activity.url}(#{activity.nesting("record")}record)"),
-              :title => "See '#{title}'")
+      link_to "#{h(record.send(activity.title))} #{record.class.name.titleize.downcase} was #{action}",
+              eval("#{activity.url}(#{activity.nesting("record")}record)")
     end
   end
 
