592142570115944f5c6d2427abcf77a9e9c260ba
Philip Arndt <parndt@gmail.com>
2010-05-31 12:21:06 +1200

    explicitly state the url for refinery_settings as it is generated differently than the others.

--- vendor/plugins/refinery_settings/rails/init.rb
+++ vendor/plugins/refinery_settings/rails/init.rb
@@ -1,5 +1,6 @@
 Refinery::Plugin.register do |plugin|
   plugin.title = "Settings"
+  plugin.url = {:controller => "/refinery/settings"}
   plugin.description = "Manage Refinery settings"
   plugin.version = 1.0
   plugin.menu_match = /admin\/(refinery_)?settings$/
