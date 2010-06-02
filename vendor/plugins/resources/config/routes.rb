dbf77f803bc46f3c2ac24c879fea232a435cf2cd
Philip Arndt <parndt@gmail.com>
2010-06-02 14:21:28 +1200

    There is no frontend for resources.

--- vendor/plugins/resources/config/routes.rb
+++ vendor/plugins/resources/config/routes.rb
@@ -1,7 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
 
-  map.resources :resources
-
   map.namespace(:admin, :path_prefix => 'refinery') do |admin|
     admin.resources :resources, :collection => {:insert => :get}
   end
