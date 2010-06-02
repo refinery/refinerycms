1d1fa696d452bc284238a75bef36b09d499bce78
Philip Arndt <parndt@gmail.com>
2010-06-03 10:14:11 +1200

    Fix to_param on page parts and store_location only when it makes more sense.

--- vendor/plugins/pages/app/models/page_part.rb
+++ vendor/plugins/pages/app/models/page_part.rb
@@ -5,8 +5,10 @@ class PagePart < ActiveRecord::Base
   validates_presence_of :title
   alias_attribute :content, :body
 
+  has_friendly_id :title, :use_slug => true
+
   def to_param
-    "page_part_#{self.title.downcase.gsub(" ", "_")}"
+    "page_part_#{super}"
   end
 
   before_save :normalise_text_fields
