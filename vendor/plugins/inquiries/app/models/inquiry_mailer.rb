93b632968d5782a21543fc6f13ffe4f5f07af9cf
Nick Hammond <nick@nickhammond.com>
2010-06-08 12:41:55 +0800

    add in the same accessor methods available to regular settings as inquiry settings, update default subject line in outgoing emails

--- vendor/plugins/inquiries/app/models/inquiry_mailer.rb
+++ vendor/plugins/inquiries/app/models/inquiry_mailer.rb
@@ -1,7 +1,7 @@
 class InquiryMailer < ActionMailer::Base
 
   def confirmation(inquiry, request)
-    subject     "Thank you for your inquiry"
+    subject     "#{RefinerySetting[:site_name]} - Thank you for your inquiry"
     recipients  inquiry.email
     from        "\"#{RefinerySetting[:site_name]}\" <no-reply@#{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}>"
     reply_to    InquirySetting.notification_recipients.value.split(',').first
@@ -10,7 +10,7 @@ class InquiryMailer < ActionMailer::Base
   end
 
   def notification(inquiry, request)
-    subject     "New inquiry from your website"
+    subject     "New inquiry from #{RefinerySetting[:site_name]}"
     recipients  InquirySetting.notification_recipients.value
     from        "\"#{RefinerySetting[:site_name]}\" <no-reply@#{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}>"
     sent_on     Time.now
