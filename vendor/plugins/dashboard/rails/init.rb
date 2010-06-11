0acc9921599a4f7d8b298c42c389bfb332bbed99
Philip Arndt <parndt@gmail.com>
2010-06-11 18:12:41 +1200

    improve the controller restriction and error_404 processes and tie controller restriction to error_404 which is a more convincing 'maybe that doesn't exist?' for the user

--- vendor/plugins/dashboard/rails/init.rb
+++ vendor/plugins/dashboard/rails/init.rb
@@ -3,6 +3,7 @@ Refinery::Plugin.register do |plugin|
   plugin.description = "Gives an overview of activity in Refinery"
   plugin.version = 1.0
   plugin.always_allow_access = true
+  plugin.dashboard = true
   # this tells refinery where this plugin is located on the filesystem and helps with urls.
   plugin.directory = directory
 end
