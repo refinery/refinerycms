a9c068e2736d8a6d16b2dc97506bf9a54ede17c9
Alex Coles <alex@alexcolesportfolio.com>
2010-06-15 20:56:56 +0800

    Dragonfly: Fix tests for Dragonfly
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- test/unit/image_test.rb
+++ test/unit/image_test.rb
@@ -5,23 +5,24 @@ class ImageTest < ActiveSupport::TestCase
   fixtures :images
 
   def test_titles
-    assert_equal "The%20world!.gif", images(:the_world).filename
+    assert_equal "The%20world!.gif", images(:the_world).image_name
     assert_equal "The World!", images(:the_world).title
 
-    assert_equal "car-wallpapers19.jpg", images(:our_car).filename
+    assert_equal "car-wallpapers19.jpg", images(:our_car).image_name
     assert_equal "Car Wallpapers19", images(:our_car).title
+  end
+
+  def test_delegate_methods
+    assert_equal images(:the_world).image_size, images(:the_world).size
+
+    [:size, :mime_type, :url, :width, :height].each do |meth|
+      assert_equal images(:the_world).image.send(meth), images(:the_world).send(meth)
+    end
   end
 
   def test_per_page
     assert_equal 18, Image.per_page(dialog = true)
     assert_equal 20, Image.per_page # dialog = false
-  end
-
-  def test_attachment_fu_options
-    assert_equal 20.megabytes, Image.attachment_options[:max_size]
-
-    # want to change this to ImageScience at some point. Rmagick sucks.
-    assert_equal 'Rmagick', Image.attachment_options[:processor]
   end
 
 end
--- test/unit/resource_test.rb
+++ test/unit/resource_test.rb
@@ -5,25 +5,21 @@ class ResourceTest < ActiveSupport::TestCase
   fixtures :resources
 
   def test_titles
-    assert_equal "teng.pdf", resources(:pdf_document).filename
+    assert_equal "teng.pdf", resources(:pdf_document).file_name
     assert_equal "Teng", resources(:pdf_document).title
+  end
+
+  def test_delegate_methods
+    assert_equal resources(:pdf_document).file_size, resources(:pdf_document).size
+
+    [:size, :mime_type, :url].each do |meth|
+      assert_equal resources(:pdf_document).file.send(meth), resources(:pdf_document).send(meth)
+    end
   end
 
   def test_per_page
     assert_equal 12, Resource.per_page(dialog = true)
     assert_equal 20, Resource.per_page # dialog = false
-  end
-
-  def test_attachment_fu_options
-    assert_equal 50.megabytes, Resource.attachment_options[:max_size]
-
-    if Refinery.s3_backend
-      assert_equal :s3, Resource.attachment_options[:storage]
-      assert_nil Resource.attachment_options[:path_prefix]
-    else
-      assert_equal :file_system, Resource.attachment_options[:storage]
-      assert_equal 'public/system/resources', Resource.attachment_options[:path_prefix]
-    end
   end
 
   def test_type_of_content
