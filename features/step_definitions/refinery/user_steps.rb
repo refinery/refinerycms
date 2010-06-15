ff7e46b93b34aee2f6ba456a059d9cc47b66d357
Alex Coles <alex@alexcolesportfolio.com>
2010-06-15 15:08:09 +0200

    Clean whitespace
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

---
+++ features/step_definitions/refinery/user_steps.rb
@@ -0,0 +1,21 @@
+def create_user
+  params = {
+    "login" => "cucumber",
+    "email" => "cucumber@isavegetable.com",
+    "password" => "greenandjuicy",
+    "password_confirmation" => "greenandjuicy"
+  }
+  @user ||= User.create(params)
+end
+
+def login
+  create_user
+  visit '/refinery'
+  fill_in("session_login", :with => @user.email)
+  fill_in("session_password", :with => @user.password)
+  click_button("submit_button")
+end
+
+Given /^I am a logged in user$/ do
+  login
+end
