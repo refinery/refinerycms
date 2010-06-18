2a8ef30e9bb9289915dfcec4e9120f75b5ade7d3
stevenheidel <steven@livingskyweb.ca>
2010-06-19 03:42:51 +0800

    Fixes 'can't modify a frozen hash' error which appears when running rake db:migrate.

--- db/migrate/20100525110237_add_small_and_large_thumbnail_sizes.rb
+++ db/migrate/20100525110237_add_small_and_large_thumbnail_sizes.rb
@@ -1,12 +1,11 @@
 class AddSmallAndLargeThumbnailSizes < ActiveRecord::Migration
   def self.up
     image_thumbnails = RefinerySetting.find_or_set(:image_thumbnails, {})
-    image_thumbnails = image_thumbnails.update({
+    RefinerySetting.image_thumbnails = image_thumbnails.merge({
       :small => '110x110>',
       :medium => '225x255>',
       :large => '450x450>'
     })
-    RefinerySetting[:image_thumbnails] = image_thumbnails
   end
 
   def self.down
