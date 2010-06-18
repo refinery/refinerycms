ed1b70f54926ecb66d6363c3794bfe71262645e6
Philip Arndt <parndt@gmail.com>
2010-06-19 01:15:06 +1200

    Add cucumber scenarios for creating valid and invalid pages.

--- features/step_definitions/refinery/page_steps.rb
+++ features/step_definitions/refinery/page_steps.rb
@@ -3,3 +3,11 @@ Given /^I have pages titled (.+)$/ do |titles|
     Page.create(:title => title)
   end
 end
+
+Given /^I have no pages$/ do
+  Page.delete_all  
+end
+
+Then /^I should have ([0-9]+) pages?$/ do |count|
+  Page.count.should == count.to_i
+end
