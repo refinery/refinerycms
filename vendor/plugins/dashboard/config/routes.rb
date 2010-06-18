bdc4417d8fa5b0dca1f06ac2a1e0a64786c70082
Philip Arndt <parndt@gmail.com>
2010-06-18 23:22:33 +1200

    Remove features from Internet Explorer users that don't work quite right. Tell the user their browser isn't great.

--- vendor/plugins/dashboard/config/routes.rb
+++ vendor/plugins/dashboard/config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.namespace(:admin, :path_prefix => 'refinery') do |admin|
     admin.resources :dashboard
+    admin.disable_upgrade_message 'disable_upgrade_message', :controller => 'dashboard', :action => 'disable_upgrade_message'
   end
 end
