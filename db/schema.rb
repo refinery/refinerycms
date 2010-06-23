2aaf2426644709f23545dd0aaf40ca1f5b56f160
stevenheidel <steven@livingskyweb.ca>
2010-06-24 06:05:56 +0800

    Add a restricted column to refinery settings.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file,
+# This file is auto-generated from the current state of the database. Instead of editing this file, 
 # please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
 #
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100608062447) do
+ActiveRecord::Schema.define(:version => 20100623220402) do
 
   create_table "images", :force => true do |t|
     t.integer  "parent_id"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(:version => 20100608062447) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "scoping"
+    t.boolean  "restricted",  :default => false
   end
 
   add_index "refinery_settings", ["name"], :name => "index_refinery_settings_on_name"
