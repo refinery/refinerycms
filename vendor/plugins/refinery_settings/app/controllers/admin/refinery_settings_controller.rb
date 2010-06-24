8388fa6d73d1f7b412747fcfe52b7f19bf88e07d
stevenheidel <steven@livingskyweb.ca>
2010-06-24 10:15:29 +0800

    Implemented the restricted value of Refinery Settings.
    Only the superuser can change the value of restricted, and if restricted is set to true then regular users cannot edit/delete that particular setting. However, this could still have bugs.

--- vendor/plugins/refinery_settings/app/controllers/admin/refinery_settings_controller.rb
+++ vendor/plugins/refinery_settings/app/controllers/admin/refinery_settings_controller.rb
@@ -1,11 +1,24 @@
 class Admin::RefinerySettingsController < Admin::BaseController
 
   crudify :refinery_setting, :title_attribute => :title, :order => "name ASC", :searchable => false
+          #:conditions => current_user.superuser? ? '' : 'restricted IS false'
 
   def edit
     @refinery_setting = RefinerySetting.find(params[:id])
 
     render :layout => false if request.xhr?
+  end
+
+  def find_all_refinery_settings
+    @refinery_settings = RefinerySetting.find :all,
+                                              :order => "name ASC",
+                                              :conditions => current_user.superuser? ? "" : "restricted IS false"
+  end
+
+  def paginate_all_refinery_settings
+    @refinery_settings = RefinerySetting.paginate :page => params[:page],
+                                                  :order => "name ASC",
+                                                  :conditions => current_user.superuser? ? "" : "restricted IS false"
   end
 
 end
