363113d168cf179dffc27a8df900c302405d8c71
Philip Arndt <parndt@gmail.com>
2010-06-29 22:27:37 +1200

    Fix menu matches on some plugins and fix reordering of backend menu.

--- vendor/plugins/refinery/app/controllers/admin/refinery_core_controller.rb
+++ vendor/plugins/refinery/app/controllers/admin/refinery_core_controller.rb
@@ -1,9 +1,9 @@
 class Admin::RefineryCoreController < Admin::BaseController
 
   def update_plugin_positions
-    params[:menu].each do |plugin_name|
+    params[:menu].each_with_index do |plugin_name, index|
       if (plugin = current_user.plugins.find_by_name(plugin_name))
-        plugin.update_attribute(:position, params[:menu].index(plugin_name))
+        plugin.update_attribute(:position, index)
       end
     end
     render :nothing => true
