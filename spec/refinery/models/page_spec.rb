cbb51d2f13f95241f9191249484d96135e750223
stevenheidel <steven@livingskyweb.ca>
2010-06-29 20:53:48 -0600

    The very first refinery spec works as advertised

---
+++ spec/refinery/models/page_spec.rb
@@ -0,0 +1,14 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+ 
+describe Page do
+  before(:each) do
+    @valid_attributes = {
+      :title => "RSpec is great for testing too"
+    }
+  end
+ 
+  it "should create a new instance given valid attributes" do
+    Page.create!(@valid_attributes)
+  end
+end
+
