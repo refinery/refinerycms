457348de4a33a46205b95e46aa2c94c9d38d85d0
Philip Arndt <parndt@gmail.com>
2010-07-01 16:41:27 +1200

    fix broken logout functionality

--- vendor/plugins/authentication/app/controllers/sessions_controller.rb
+++ vendor/plugins/authentication/app/controllers/sessions_controller.rb
@@ -19,8 +19,6 @@ class SessionsController < ApplicationController
 
   def destroy
     current_user_session.destroy if logged_in?
-    flash[:notice] = t('sessions.logged_out')
-    redirect_back_or_default(root_url)
 
     redirect_to(root_url)
   end
