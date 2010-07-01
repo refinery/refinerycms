b75c6cd2d9489130bff5140e59b9444101f2202f
Amanda Nicholas <melime@gmail.com>
2010-07-01 17:30:47 +1200

    Added methods to user to add and remove roles to simplify code; added [] method to Role to access roles

--- test/functional/dashboard_controller_test.rb
+++ test/functional/dashboard_controller_test.rb
@@ -9,7 +9,7 @@ class DashboardControllerTest < ActionController::TestCase
 
   def setup
     @controller = Admin::DashboardController.new
-    users(:quentin).roles << Role.find_or_create_by_title('Refinery')
+    users(:quentin).add_role(:refinery)
     login_as(:quentin)
   end
 
--- vendor/plugins/authentication/app/models/role.rb
+++ vendor/plugins/authentication/app/models/role.rb
@@ -7,5 +7,9 @@ class Role < ActiveRecord::Base
   def camelize_title
     self.title = title.camelize
   end
+  
+  def self.[](title)
+    find_or_create_by_title(title.to_s)
+  end
 
 end
