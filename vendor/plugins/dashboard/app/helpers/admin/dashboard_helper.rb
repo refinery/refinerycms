f106f9303f48eee30bd7c50a13c6cdd72309fd62
Halan Pinheiro <halan.pinheiro@gmail.com>
2010-07-06 00:58:33 -0300

    Translate the dashboard messages with correct grammar and capitalization.

--- vendor/plugins/dashboard/app/helpers/admin/dashboard_helper.rb
+++ vendor/plugins/dashboard/app/helpers/admin/dashboard_helper.rb
@@ -6,8 +6,14 @@ module Admin::DashboardHelper
       # work out which action occured
       action = record.updated_at.eql?(record.created_at) ? "created" : "updated"
 
+      #get article to define gender of model name, some lang have it
+      article = t("plugins.#{record.class.name.pluralize.downcase}.article", :default => 'the')
+
       # now create a link to the notification's corresponding record.
-      link_to "#{h(record.send(activity.title))} #{record.class.name.titleize.downcase} was #{action}",
+      link_to t(".last_activity_mensage", :what => record.send(activity.title),
+
+                                  :kind => record.class.human_name,
+                                  :action => t("#{action}.with_article \"#{article}\"")).downcase.capitalize,
               eval("#{activity.url}(#{activity.nesting("record")}record)")
     end
   end
