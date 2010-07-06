d4b6801db7da901c2a9ba00766a3454fe332b5fa
Philip Arndt <parndt@gmail.com>
2010-07-06 16:41:48 +1200

    add human name for inquiry_setting for english.

--- vendor/plugins/dashboard/app/helpers/admin/dashboard_helper.rb
+++ vendor/plugins/dashboard/app/helpers/admin/dashboard_helper.rb
@@ -6,15 +6,15 @@ module Admin::DashboardHelper
       # work out which action occured
       action = record.updated_at.eql?(record.created_at) ? "created" : "updated"
 
-      #get article to define gender of model name, some lang have it
+      # get article to define gender of model name, some languages require this for proper grammar
       article = t("plugins.#{record.class.name.pluralize.downcase}.article", :default => 'the')
 
       # now create a link to the notification's corresponding record.
-      link_to t(".latest_activity_message", :what => record.send(activity.title),
-
-                                  :kind => record.class.human_name,
-                                  :action => t("#{action}.with_article \"#{article}\"")).downcase.capitalize,
-              eval("#{activity.url}(#{activity.nesting("record")}record)")
+      link_to t(".latest_activity_message",
+                :what => record.send(activity.title),
+                :kind => record.class.human_name,
+                :action => t("#{action}.with_article \"#{article}\"")
+               ).downcase.capitalize, eval("#{activity.url}(#{activity.nesting("record")}record)")
     end
   end
 
