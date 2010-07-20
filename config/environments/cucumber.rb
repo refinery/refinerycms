a0743e3a0a3ac7ca872ae80eea87ed576be8199d
Philip Arndt <parndt@gmail.com>
2010-07-21 11:03:06 +1200

    Use a version of cucumber that supports spork and use spork as the default on OS that isn't Windows. Remove hardcoded config.gem specified by cucumber as we use Bundler and don't need them.

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -21,7 +21,3 @@ config.action_controller.allow_forgery_protection    = false
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
 
-config.gem 'cucumber-rails',   :lib => false, :version => '>=0.3.2' unless File.directory?(File.join(Rails.root, 'vendor/plugins/cucumber-rails'))
-config.gem 'database_cleaner', :lib => false, :version => '>=0.5.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/database_cleaner'))
-config.gem 'capybara',         :lib => false, :version => '>=0.3.5' unless File.directory?(File.join(Rails.root, 'vendor/plugins/capybara'))
-
