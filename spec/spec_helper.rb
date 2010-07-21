3cbe582aa591821ae6f804dcd6ae1c83677166b5
Philip Arndt <parndt@gmail.com>
2010-07-22 09:24:35 +1200

    Seems I have a new spec_helper.rb modification

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,30 @@
+unless RUBY_PLATFORM =~ /mswin|mingw/
+  require 'spork'
+
+  Spork.prefork do
+    # Loading more in this block will cause your tests to run faster. However,
+    # if you change any configuration or code from libraries loaded here, you'll
+    # need to restart spork for it take effect.
+
+  end
+
+  Spork.each_run do
+    # This code will be run each time you run your specs.
+
+  end
+
+  # --- Instructions ---
+  # - Sort through your spec_helper file. Place as much environment loading
+  #   code that you don't normally modify during development in the
+  #   Spork.prefork block.
+  # - Place the rest under Spork.each_run block
+  # - Any code that is left outside of the blocks will be ran during preforking
+  #   and during each_run!
+  # - These instructions should self-destruct in 10 seconds.  If they don't,
+  #   feel free to delete them.
+  #
+end
+
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
 ENV["RAILS_ENV"] ||= 'test'
