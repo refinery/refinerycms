dd303dceaf14ff652fe625373af0b7be5d0734d7
James Fiderlick <fiderlick.james@gmail.com>
2010-07-26 16:58:53 -0500

    Revert "We need more Cucumbers."
    This reverts commit 4a7140fb81a9142d49157748bb149cd61d68ef7e.

--- features/step_definitions/refinery/file_steps.rb
+++ features/step_definitions/refinery/file_steps.rb
@@ -10,13 +10,7 @@ Then /^the file "([^"]*)" should have uploaded successfully$/ do |file_name|
   Resource.find_by_filename(file_name).nil?.should == false
 end
 
-And /^I should have ([0-9]+) files?$/ do |number|
+Then /^I should have (\d+) file$/ do |number|
   Resource.count.should == number.to_i
-end
-
-When /^I upload the file at "([^"]*)"$/ do |file_path|
-  visit new_admin_resource_path
-  attach_file('resource[uploaded_data]', File.join(Rails.root, file_path))
-  click_button 'Save'
 end
 
