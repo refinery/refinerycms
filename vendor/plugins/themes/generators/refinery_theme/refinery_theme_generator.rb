8cb7dca25e8185873d2623df1ef192fa1f7a327f
djones <dgjones@gmail.com>
2010-07-28 15:09:12 +1200

    ensure a theme name is supplied before creating a theme

--- vendor/plugins/themes/generators/refinery_theme/refinery_theme_generator.rb
+++ vendor/plugins/themes/generators/refinery_theme/refinery_theme_generator.rb
@@ -1,26 +1,31 @@
 class RefineryThemeGenerator < Rails::Generator::Base
 
   def manifest
-    record do |m|
-      m.directory "themes"
-      m.directory "themes/#{theme_name}"
-      m.directory "themes/#{theme_name}/javascripts"
+    unless @args[0].nil?
+      record do |m|
+        m.directory "themes"
+        m.directory "themes/#{theme_name}"
+        m.directory "themes/#{theme_name}/javascripts"
       
-      m.directory "themes/#{theme_name}/stylesheets"
-      m.file "stylesheets/application.css", "themes/#{theme_name}/stylesheets/application.css"
-      m.file "stylesheets/formatting.css", "themes/#{theme_name}/stylesheets/formatting.css"
-      m.file "stylesheets/home.css", "themes/#{theme_name}/stylesheets/home.css"
+        m.directory "themes/#{theme_name}/stylesheets"
+        m.file "stylesheets/application.css", "themes/#{theme_name}/stylesheets/application.css"
+        m.file "stylesheets/formatting.css", "themes/#{theme_name}/stylesheets/formatting.css"
+        m.file "stylesheets/home.css", "themes/#{theme_name}/stylesheets/home.css"
 
-      m.directory "themes/#{theme_name}/views"
-      m.directory "themes/#{theme_name}/views/layouts"
-      m.file "views/layouts/application.html.erb", "themes/#{theme_name}/views/layouts/application.html.erb"
+        m.directory "themes/#{theme_name}/views"
+        m.directory "themes/#{theme_name}/views/layouts"
+        m.file "views/layouts/application.html.erb", "themes/#{theme_name}/views/layouts/application.html.erb"
       
-      m.directory "themes/#{theme_name}/views/pages"
-      m.file "views/pages/show.html.erb", "themes/#{theme_name}/views/pages/show.html.erb"
-      m.file "views/pages/home.html.erb", "themes/#{theme_name}/views/pages/home.html.erb"
+        m.directory "themes/#{theme_name}/views/pages"
+        m.file "views/pages/show.html.erb", "themes/#{theme_name}/views/pages/show.html.erb"
+        m.file "views/pages/home.html.erb", "themes/#{theme_name}/views/pages/home.html.erb"
       
-      puts 'NOTE: If you want this new theme to be the current theme used, set the "theme"
-            setting in the Refinery backend to the name of this theme.'
+        puts 'NOTE: If you want this new theme to be the current theme used, set the "theme"
+              setting in the Refinery backend to the name of this theme.'
+      end
+    else
+      puts "You must specify a theme name."
+      puts "Eg: ruby script/generate refinery_theme modern"
     end
   end
 
