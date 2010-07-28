cfaa0b3ee4770ed0ecfb8e1b5c9196d929b2965d
djones <dgjones@gmail.com>
2010-07-28 16:52:49 +1200

    add deprecation instructions to the existing refinery generator

---
+++ vendor/plugins/refinery/lib/generators/refinery/refinery_generator.rb
@@ -0,0 +1,13 @@
+class RefineryGenerator < Rails::Generator::NamedBase
+  
+  def banner
+    puts "This generator has been renamed. Please this instead:"
+    puts "ruby script/generate refinery_plugin"
+    exit
+  end
+    
+  def manifest
+    banner
+  end
+
+end
