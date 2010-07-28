0a83d475ec4f9e3a7db1986b478e09b91f8e1f81
djones <dgjones@gmail.com>
2010-07-28 13:32:51 +1200

    fix left over merge tag

--- features/support/env.rb
+++ features/support/env.rb
@@ -30,7 +30,6 @@ def setup_environment
   Capybara.default_selector = :css
 end
 
-<<<<<<< HEAD
 def each_run
   # If you set this to false, any error raised from within your app will bubble
   # up to your step definition and out to cucumber unless you catch it somewhere
