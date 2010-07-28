85324164dca58fb537d417cf1dd8618fc090d225
Philip Arndt <parndt@gmail.com>
2010-07-28 13:04:10 +1200

    Default url prefix to edit

--- vendor/plugins/refinery/lib/generators/refinery/templates/rails/init.rb
+++ vendor/plugins/refinery/lib/generators/refinery/templates/rails/init.rb
@@ -5,7 +5,6 @@ Refinery::Plugin.register do |plugin|
   plugin.version = 1.0
   plugin.activity = {
     :class => <%= class_name %>,
-    :url_prefix => "edit",
     :title => '<%= attributes.first.name %>'
   }
   # this tells refinery where this plugin is located on the filesystem and helps with urls.
