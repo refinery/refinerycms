516ff484d1dacbd4d55a0017f08e3554bbfbd26a
Philip Arndt <parndt@gmail.com>
2010-08-08 14:54:28 +1200

    We'll do this automatically.

--- core/lib/generators/refinery_plugin/templates/init.rb
+++ core/lib/generators/refinery_plugin/templates/init.rb
@@ -1,11 +1,3 @@
-module RefineryStaticAssetsEngine
-  class Engine < Rails::Engine
-    initializer "static assets" do |app|
-      app.middleware.insert_after ::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public"
-    end
-  end
-end
-
 Refinery::Plugin.register do |plugin|
   plugin.name = "<%= class_name.pluralize.underscore.downcase %>"
   plugin.activity = {
