b9157f0f4c1474ad6905be66b66ee905be460153
Philip Arndt <parndt@gmail.com>
2010-08-09 10:19:33 +1200

    we can get rid of lots of this code, but link_url is still defective around_generate

--- vendor/plugins/i18n/lib/i18n_filter.rb
+++ vendor/plugins/i18n/lib/i18n_filter.rb
@@ -4,12 +4,11 @@ module RoutingFilter
     def around_recognize(path, env, &block)
       if ::Refinery::I18n.enabled?
         locale = nil
-        if path =~ %r{^/(#{::Refinery::I18n.locales.keys.join('|')})/}
+        if path =~ %r{^/(#{::Refinery::I18n.locales.keys.join('|')})/?}
           if path !~ %r{^/(sessions?|admin|refinery|wymiframe)}
             path.sub! %r(^/(([a-zA-Z\-_])*)(?=/|$)) do
-              locale = $1
-              ::I18n.locale = locale
-              ''
+              ::I18n.locale = $1
+              '/'
             end
           end
         else
@@ -17,8 +16,8 @@ module RoutingFilter
         end
 
         yield.tap do |params|
-          unless path =~ %r{^/(admin|refinery|wymiframe)} or ::I18n.locale == ::Refinery::I18n.default_frontend_locale
-            params[:locale] = (locale.presence || ::Refinery::I18n.current_frontend_locale)
+          unless path =~ %r{^/(admin|refinery|wymiframe)}
+            params[:locale] = ::I18n.locale
           end
         end
       else
@@ -32,7 +31,7 @@ module RoutingFilter
       if ::Refinery::I18n.enabled?
         locale = params.delete(:locale) || ::I18n.locale
         yield.tap do |result|
-          if (locale != ::Refinery::I18n.default_frontend_locale and result !~ %r{^/(refinery|wymiframe)})
+          if (locale != ::Refinery::I18n.default_frontend_locale.to_s and result !~ %r{^/(refinery|wymiframe)})
             result.sub!(%r(^(http.?://[^/]*)?(.*))){ "#{$1}/#{locale}#{$2}" }
           end
           result
