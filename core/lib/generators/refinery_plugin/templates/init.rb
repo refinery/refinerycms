8a79e65d76f961a7621b7e2530ef01332b338244
Philip Arndt <parndt@gmail.com>
2010-08-08 14:52:34 +1200

    directory no workie

--- core/lib/generators/refinery_plugin/templates/init.rb
+++ core/lib/generators/refinery_plugin/templates/init.rb
@@ -12,6 +12,4 @@ Refinery::Plugin.register do |plugin|
     :class => <%= class_name %><% if attributes.first.name != "title" %>,
     :title => '<%= attributes.select { |a| a.type.to_s == "string" }.first.name %>'
   <% end %>}
-
-  # plugin.directory = directory # tell refinery where this plugin is located
 end
