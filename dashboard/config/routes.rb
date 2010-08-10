5a6dec756fadf073598bcf0a6a06e5f89742c361
Philip Arndt <parndt@gmail.com>
2010-08-10 17:03:27 +1200

    Route better, dashboard doesn't have resources.

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -1,6 +1,11 @@
 Refinery::Application.routes.draw do
   scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
-    resource :dashboard, :controller => 'dashboard'
-    match 'disable_upgrade_message', :to => 'dashboard#disable_upgrade_message', :as => :disable_upgrade_message
+    match 'dashboard',
+          :to => 'dashboard#index',
+          :as => :dashboard
+          
+    match 'disable_upgrade_message', 
+          :to => 'dashboard#disable_upgrade_message', 
+          :as => :disable_upgrade_message
   end
 end
