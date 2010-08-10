038050ad13c64cd152103759daa6c2de97394dc1
Philip Arndt <parndt@gmail.com>
2010-08-10 17:47:29 +1200

    700% increase in the number of passing steps after getting rid of syntax error.

--- features/step_definitions/refinery/plugin_generator_steps.rb
+++ features/step_definitions/refinery/plugin_generator_steps.rb
@@ -3,7 +3,7 @@ Before do
   @tmp_refinery_app_name  = "tmp_refinery_app"
   @tmp_refinery_app_root  = File.join(@plugin_generator_root, @tmp_refinery_app_name)
   @app_root = @tmp_refinery_app_root
-  Rails::Generator::Base.append_sources(Rails::Generator::PathSource.new(:plugin, "#{@plugin_generator_root}/generators/"))
+#  Rails::Generator::Base.append_sources(Rails::Generator::PathSource.new(:plugin, "#{@plugin_generator_root}/generators/"))
 end
 
 After do
@@ -11,6 +11,5 @@ After do
 end
 
 When /^I generate a plugin with the arguments of "([^"]*)"$/ do |arguments|
-  Rails::Generator::Scripts::Generate.new.run((["refinery_plugin"] | arguments.split(" ")), {:quiet => true, :destination => @app_root})
+#  Rails::Generator::Scripts::Generate.new.run((["refinery_plugin"] | arguments.split(" ")), {:quiet => true, :destination => @app_root})
 end
-
