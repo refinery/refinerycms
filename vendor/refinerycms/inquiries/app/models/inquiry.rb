b7bb74d4cff8146299b2b50b34dbef4ecfc98ca6
Uģis Ozols <ugis.ozolss@gmail.com>
2010-08-17 16:58:52 +0300

    Oops... Removed needed validation.

--- vendor/refinerycms/inquiries/app/models/inquiry.rb
+++ vendor/refinerycms/inquiries/app/models/inquiry.rb
@@ -1,6 +1,6 @@
 class Inquiry < ActiveRecord::Base
 
-  validates_presence_of :name
+  validates_presence_of :name, :message
   validates_format_of :email,
                       :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
 
