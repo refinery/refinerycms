dc5220c62cb3e9f53c82135320c9c0acb619e538
Philip Arndt <parndt@gmail.com>
2010-08-21 14:42:12 +1200

    Use the new rails3 coolness (generated a new rails3 app and moved Refinery on top of it).

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,15 @@ Refinery::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Specifies the header that your server uses for sending files
+  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+
+  # For nginx:
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+
+  # If you have no front-end server that supports something like X-Sendfile,
+  # just comment this out and Rails will serve the files
+
   # See everything in the log (default is :info)
   # config.log_level = :debug
 
@@ -18,11 +27,12 @@ Refinery::Application.configure do
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
 
-  # Disable Rails's static asset server
+  # Disable Rails's static asset server (Rails default)
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = true
-  # Warning: disabling this will means files in your public directly
+  # Warning: Refinery CMS has enabled this setting.
+  # Disabling this will means files in your public directly
   # won't override core assets that are served from the plugin public directories.
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -33,7 +43,12 @@ Refinery::Application.configure do
   # Enable threaded mode
   # config.threadsafe!
 
-  config.active_support.deprecation = :log
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation can not be found)
+  config.i18n.fallbacks = true
+
+  # Send deprecation notices to registered listeners
+  config.active_support.deprecation = :notify
 
   config.after_initialize do
     # override translate, but only in production
