28788909641a2ea62b7b1842328747bf5cc13975
Maarten Hoogendoorn <maarten@moretea.nl>
2010-08-23 14:39:05 +0200

    Updated validations to rail3 format

--- vendor/refinerycms/resources/app/models/resource.rb
+++ vendor/refinerycms/resources/app/models/resource.rb
@@ -5,10 +5,9 @@ class Resource < ActiveRecord::Base
 
   resource_accessor :file
 
-  validates_presence_of :file, :message => I18n.t('file_specify_for_upload')
-  validates_size_of     :file, :maximum => MAX_SIZE_IN_MB.megabytes,
-                        :message => I18n.t('file_should_be_smaller_than_max_file_size',
-                                    :max_file_size => ActionController::Base.helpers.number_to_human_size(MAX_SIZE_IN_MB.megabytes) )
+  validates :file, :presence => { :message => I18n.t('file_specify_for_upload') },
+                   :length   => { :maximum => MAX_SIZE_IN_MB.megabytes,
+                                  :message => I18n.t('file_should_be_smaller_than_max_file_size', :max_file_size => ActionController::Base.helpers.number_to_human_size(MAX_SIZE_IN_MB.megabytes)) }
 
    # Docs for acts_as_indexed http://github.com/dougal/acts_as_indexed
    acts_as_indexed :fields => [:file_name, :title, :type_of_content]
