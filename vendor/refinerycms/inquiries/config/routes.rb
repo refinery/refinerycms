fd1f357d1373bde8fd6cfc1c33bb1e3cb51c9fbd
Philip Arndt <parndt@gmail.com>
2010-08-23 19:54:51 +1200

    Use the routes better, no more /inquiries when you submit the form (ht: gidogeek)

--- vendor/refinerycms/inquiries/config/routes.rb
+++ vendor/refinerycms/inquiries/config/routes.rb
@@ -1,7 +1,6 @@
 Refinery::Application.routes.draw do
-  match '/contact', :to => 'inquiries#new', :as => 'new_inquiry'
-  match '/contact/thank_you', :to => 'inquiries#thank_you', :as => 'thank_you_inquiries'
-  resources :inquiries do
+  get '/contact', :to => 'inquiries#new', :as => 'new_inquiry'
+  resources :contact, :as => :inquiries, :controller => 'inquiries' do
     collection do
       get :thank_you
     end
