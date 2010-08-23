531e0fa2e101a628e4b8f48bf784039e7258def2
Steven Heidel <steven@livingskyweb.ca>
2010-08-22 23:15:41 -0600

    Recomment out optional libraries, and beautify the test group of the Gemfile.

--- autotest/autotest.rb
+++ autotest/autotest.rb
@@ -1,7 +1,9 @@
-require "autotest/restart"
-require "test_notifier/runner/autotest"
-require "redgreen/autotest"
-require "autotest/timestamp"
+###  uncomment any of the following requires applicable for your system
+###  and then copy this to .autotest if you are using the ZenTest autotest.
+# require "autotest/restart"
+# require "test_notifier/runner/autotest"
+# require "redgreen/autotest"
+# require "autotest/timestamp"
 
 # adds exceptions from .gitignore file, please modify exceptions there!
 imported_exceptions =  IO.readlines('.gitignore').inject([]) do |acc, line|
