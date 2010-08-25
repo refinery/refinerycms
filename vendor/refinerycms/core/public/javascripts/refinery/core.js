5f45de852bdae0691b2a9349aa8fbf9d92b84b42
Philip Arndt <parndt@gmail.com>
2010-08-25 14:08:35 +1200

    split assets into core.js and admin.js so that we can load them separately as required.

---
+++ vendor/refinerycms/core/public/javascripts/refinery/core.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  init_flash_messages = function(){
+    $('#flash').fadeIn(550);
+    $('#flash_close').click(function(e) {
+       $('#flash').fadeOut({duration: 330});
+       e.preventDefault();
+    });
+    $('#flash.flash_message').prependTo('#records');
+  }
+});
