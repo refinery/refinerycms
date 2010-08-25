e103406b0b5ea6975abc5a2213b84f0bb2c94be9
Steven Heidel <steven@livingskyweb.ca>
2010-08-24 20:13:27 -0600

    Update the gemspecs.
    Eventually the bottom 8 dependencies should be moved back to the Gemfile as we don't want them installing on Successfully installed refinerycms-0.9.7.13

--- lib/gemspec.rb
+++ lib/gemspec.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-require File.expand_path('../../vendor/refinerycms/core/lib/refinery.rb', __FILE__)
+require File.expand_path('../../vendor/refinerycms/refinerycms.rb', __FILE__)
 files = %w( .gitignore .yardopts Gemfile ).map { |file| Dir[file] }.flatten
 %w(app bin config db features lib public script test themes vendor).sort.each do |dir|
   files += Dir.glob("#{dir}/**/*")
@@ -10,7 +10,7 @@ rejection_patterns = [
   "^config\/environments",
   "^config\/initializers\/(backtrace_silencers|inflections|mime_types|secret_token|session_store).rb$",
   "^config\/(cucumber|database|i18n\-js).yml$",
-  "^public\/",
+  #"^public\/",
   "^lib\/gemspec\.rb",
   "^lib\/tasks",
   ".*\/cache\/",
@@ -29,24 +29,28 @@ end
 gemspec = <<EOF
 Gem::Specification.new do |s|
   s.name              = %q{refinerycms}
-  s.version           = %q{#{Refinery.version}}
+  s.version           = %q{0.9.8.0.beta2.pre}
   s.description       = %q{A beautiful open source Ruby on Rails content manager for small business. Easy to extend, easy to use, lightweight and all wrapped up in a super slick UI.}
-  s.date              = %q{#{Time.now.strftime('%Y-%m-%d')}}
+  s.date              = %q{2010-08-24}
   s.summary           = %q{A beautiful open source Ruby on Rails content manager for small business.}
   s.email             = %q{info@refinerycms.com}
   s.homepage          = %q{http://refinerycms.com}
   s.authors           = ['Resolve Digital', 'David Jones', 'Philip Arndt']
-  s.require_paths     = %w(lib)
-  s.executables       = %w(#{Dir.glob('bin/*').map{|d| d.gsub('bin/','')}.join(' ')})
+  s.require_paths     = %w(vendor/refinerycms)
+  s.executables       = %w(refinerycms)
 
-  s.add_dependency    'rails',      '~> 3.0.0.rc2'
-  s.add_dependency    'bundler',    '~> 1.0.0'
+  s.add_dependency    'rails',            '~> 3.0.0.rc2'
+  s.add_dependency    'bundler',          '~> 1.0.0'
   s.add_dependency    'childlabor'
 
-  s.files             = [
-    '#{files.join("',\n    '")}'
-  ]
-end
+  s.add_dependency    'acts_as_indexed',  '= 0.6.5'
+  s.add_dependency    'friendly_id',      '~> 3.1.3'
+  s.add_dependency    'truncate_html',    '= 0.4'
+  s.add_dependency    'will_paginate',    '>= 3.0.pre2'
+  s.add_dependency    'authlogic',        '~> 2.1.6'
+  s.add_dependency    'rmagick'
+  s.add_dependency    'rack-cache'
+  s.add_dependency    'dragonfly'
 EOF
 
 File.open(File.expand_path("../../refinerycms.gemspec", __FILE__), 'w').puts(gemspec)
