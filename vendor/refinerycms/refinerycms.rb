1c4e75fc0ab76e9165c10266abe48770ba9490fe
Philip Arndt <parndt@gmail.com>
2010-08-27 15:41:53 +1200

    Load in the engines that we're using dynamically rather than writing out each one.

--- vendor/refinerycms/refinerycms.rb
+++ vendor/refinerycms/refinerycms.rb
@@ -1,9 +1,12 @@
 require 'refinery'
 
-require 'core/lib/core'
-require 'dashboard/lib/dashboard'
-require 'images/lib/images'
-require 'pages/lib/pages'
-require 'resources/lib/resources'
-require 'settings/lib/settings'
-require 'authentication/lib/authentication'
+# Load all dependencies generically using the same name as the folder they're in.
+# This is the naming convention we'll have to stick to and it also happens
+# to be the standard naming convention.
+Dir[File.expand_path("../**", __FILE__).to_s].each do |dir|
+  if (dir = Pathname.new(dir)).directory?
+    if (require_file = dir.join('lib', "#{dir.split.last}.rb")).file?
+      require require_file
+    end
+  end
+end
