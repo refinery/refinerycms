915babfe66db7ad51034114bcaf7af799970ae28
Philip Arndt <parndt@gmail.com>
2010-08-30 18:22:02 +1200

    Only once the database is setup.

--- vendor/refinerycms/images/lib/images.rb
+++ vendor/refinerycms/images/lib/images.rb
@@ -11,8 +11,10 @@ module Refinery
         app_images.configure_with(:rails) do |c|
           c.datastore.root_path = Rails.root.join('public', 'system', 'images').to_s
           c.url_path_prefix = '/system/images'
-          c.secret = RefinerySetting.find_or_set(:dragonfly_secret,
-                                                 Array.new(24) { rand(256) }.pack('C*').unpack('H*').first)
+          if RefinerySetting.table_exists?
+            c.secret = RefinerySetting.find_or_set(:dragonfly_secret,
+                                                   Array.new(24) { rand(256) }.pack('C*').unpack('H*').first)
+          end
         end
         app_images.configure_with(:heroku, ENV['S3_BUCKET']) if Refinery.s3_backend
 
