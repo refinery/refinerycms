4304cedf1a64f4b331e104f832a368e7a2f51449
Frederico Macedo <xanthobr@gmail.com>
2010-08-30 20:11:34 -0300

    when i generate an plugin without attributes with string type this init file is gettiing fail. the change is check if atributes is an empty array.

--- vendor/plugins/refinery/lib/generators/refinery_plugin/templates/rails/init.rb
+++ vendor/plugins/refinery/lib/generators/refinery_plugin/templates/rails/init.rb
@@ -1,9 +1,9 @@
-Refinery::Plugin.register do |plugin|
-  plugin.name = "<%= class_name.pluralize.underscore.downcase %>"
-  plugin.activity = {
-    :class => <%= class_name %><% if attributes.first.name != "title" %>,
-    :title => '<%= attributes.select { |a| a.type.to_s == "string" }.first.name %>'
-  <% end %>}
-
-  plugin.directory = directory # tell refinery where this plugin is located
+Refinery::Plugin.register do |plugin| 
+  plugin.name = "<%= class_name.pluralize.underscore.downcase %>" 
+  plugin.activity = { 
+    :class => <%= class_name %><% if attributes.first.name != "title" %>,<% attrs = attributes.select { |a| a.type.to_s == "string" }%> 
+    :title => '<%= (attrs.empty? ? "" : attrs.first.name) %>' 
+  <% end %>} 
+ 
+  plugin.directory = directory # tell refinery where this plugin is located 
 end
