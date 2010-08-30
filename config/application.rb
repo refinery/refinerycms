8ba333022911e17a437bef35487d008c9b9f28e3
Philip Arndt <parndt@gmail.com>
2010-08-30 14:59:34 +1200

    We can't use the ActionController::Base method in our models. Closes GH-161.

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Refinery
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Refinery.root.join('vendor', 'refinerycms', '*/config/locales/*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
--- config/initializers/locale.rb
+++
@@ -1,2 +0,0 @@
-
-I18n.load_path += Dir[Rails.root.join('vendor', 'refinerycms', '*/config/locales/*.{rb,yml}').to_s]
