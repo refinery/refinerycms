7b88635768d454bdaa9cf2eb7579eea315cfc12d
Philip Arndt <parndt@gmail.com>
2010-09-02 22:16:46 +1200

    Make better use of nested sets.

--- vendor/refinerycms/pages/app/controllers/admin/pages_controller.rb
+++ vendor/refinerycms/pages/app/controllers/admin/pages_controller.rb
@@ -6,7 +6,6 @@ class Admin::PagesController < Admin::BaseController
           :include => [:parts, :slugs, :children, :parent],
           :paging => false
 
-  before_filter :find_pages_for_parents_list, :only => [:new, :create, :edit, :update]
   after_filter :expire_caching, :only => [:create, :update, :destroy, :update_positions]
 
   rescue_from FriendlyId::ReservedError, :with => :show_errors_for_reserved_slug
@@ -31,30 +30,6 @@ protected
 
   def expire_action_caching
     expire_fragment %r{.*/pages/.*}
-  end
-
-  # This finds all of the pages that could possibly be assigned as the current page's parent.
-  def find_pages_for_parents_list
-    @pages_for_parents_list = []
-    Page.find_all_by_parent_id(nil, :order => "position ASC").each do |page|
-      @pages_for_parents_list << page
-      @pages_for_parents_list += add_pages_branch_to_parents_list(page)
-    end
-    @pages_for_parents_list.flatten.compact!
-
-    # We need to remove all references to the current page or any of its decendants or we get a nightmare.
-    @pages_for_parents_list.reject! do |page|
-      page.id == @page.id or @page.descendants.include?(page)
-    end unless @page.nil? or @page.new_record?
-  end
-
-  def add_pages_branch_to_parents_list(page)
-    list = []
-    page.children.each do |child|
-      list << child
-      list += add_pages_branch_to_parents_list(child) if child.children.any?
-    end
-    list
   end
 
   def show_errors_for_reserved_slug(exception)
