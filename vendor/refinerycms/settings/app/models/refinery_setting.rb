91960eda3af6ea2da29b99c8101b929d43efc575
Philip Arndt <parndt@gmail.com>
2010-09-03 23:38:20 +1200

    Refactored refinery settings to finally have some sanity.

--- vendor/refinerycms/settings/app/models/refinery_setting.rb
+++ vendor/refinerycms/settings/app/models/refinery_setting.rb
@@ -5,7 +5,9 @@ class RefinerySetting < ActiveRecord::Base
   serialize :value # stores into YAML format
   serialize :callback_proc_as_string
 
-  before_save :check_restriction
+  before_save do |setting|
+    setting.restricted = false if setting.restricted.nil?
+  end
 
   after_save do |setting|
     setting.class.rewrite_cache
@@ -17,10 +19,17 @@ class RefinerySetting < ActiveRecord::Base
       12
     end
 
-    def cache_read(name = nil, scoping = nil)
+    def ensure_cache_exists!
       if (result = Rails.cache.read(self.cache_key)).nil?
         result = rewrite_cache
       end
+
+      result
+    end
+    protected :ensure_cache_exists!
+
+    def cache_read(name = nil, scoping = nil)
+      result = ensure_cache_exists!
 
       if name.present?
         result = result.detect do |rs|
@@ -44,8 +53,16 @@ class RefinerySetting < ActiveRecord::Base
     end
 
     def rewrite_cache
+      # delete cache
       Rails.cache.delete(self.cache_key)
-      Rails.cache.write(self.cache_key, (result = self.to_cache(RefinerySetting.all)))
+
+      # generate new cache
+      result = self.to_cache(RefinerySetting.all)
+
+      # write cache
+      Rails.cache.write(self.cache_key, result)
+
+      # return cache
       result
     end
 
@@ -53,51 +70,37 @@ class RefinerySetting < ActiveRecord::Base
       "#{Refinery.base_cache_key}_refinery_settings_cache"
     end
 
-    # Access method that allows dot notation to work.
-    # Say you had a setting called "site_name". You could access that by going RefinerySetting[:site_name]
-    # but with this you can also access that by going RefinerySettting.site_name
-    def method_missing(method, *args)
-      method_name = method.to_s
-      super(method, *args)
-
-    rescue NoMethodError
-      if method_name =~ /=$/
-        self[method_name.gsub('=', '')] = args.first
-      else
-        self[method_name]
-      end
-    end
-
+    # find_or_set offers a convenient way to
     def find_or_set(name, the_value, options={})
+      options = {:scoping => nil, :restricted => false, :callback_proc_as_string => nil}.merge(options)
       # Try to get the value from cache first.
       scoping = options[:scoping]
       restricted = options[:restricted]
       callback_proc_as_string = options[:callback_proc_as_string]
-      if (value = cache_read(name, scoping)).nil?
-        setting = find_or_create_by_name(:name => name.to_s, :value => (value = the_value))
 
-        # if the database is not up to date yet then it won't know about certain fields.
-        setting.scoping = scoping
-        setting.restricted = restricted
-        if callback_proc_as_string.is_a?(String)
-          setting.callback_proc_as_string = callback_proc_as_string
-        end
+      # try to find the setting first
+      value = cache_read(name, scoping)
 
-        setting.save
-      end
+      # if the setting's value is nil, store a new one using the existing functionality.
+      value = set(name, options.merge({:value => the_value})) if value.nil?
 
       # Return what we found.
       value
     end
 
-    def [](name)
+    alias :get_or_set :find_or_set
+
+    # Retrieve the current value for the setting whose name is supplied.
+    def get(name)
       cache_read(name)
     end
 
-    def []=(name, value)
-      setting = find_or_initialize_by_name(name.to_s)
+    alias :[] :get
 
-      scoping = nil
+    def set(name, value)
+      scoping = (value.is_a?(Hash) and value.has_key?(:scoping)) ? value[:scoping] : nil
+      setting = find_or_initialize_by_name_and_scoping(:name => name.to_s, :scoping => scoping)
+
       # you could also pass in {:value => 'something', :scoping => 'somewhere'}
       unless value.is_a?(Hash) and value.has_key?(:value)
         setting.value = value
@@ -108,8 +111,23 @@ class RefinerySetting < ActiveRecord::Base
         setting.destroyable = value[:destroyable] if value.has_key?(:destroyable)
       end
 
+      # Save because we're in a setter method.
       setting.save
+
+      # Return the value
       setting.value
+    end
+
+    # DEPRECATED for removal at >= 0.9.9
+    def []=(name, value)
+      warning = ["\n*** DEPRECATION WARNING ***"]
+      warning << "You should not use this anymore: RefinerySetting[#{name.inspect}] = #{value.inspect}."
+      warning << "\nInstead, you should use RefinerySetting.set(#{name.inspect}, #{value.inspect})"
+      warning << ""
+      warning << "Called from: #{caller.first.inspect}\n\n"
+      $stdout.puts warning.join("\n")
+
+      set(name, value)
     end
   end
 
@@ -119,6 +137,7 @@ class RefinerySetting < ActiveRecord::Base
     self.name.titleize
   end
 
+  # form_value is so that on the web interface we can display a sane value.
   def form_value
     unless self[:value].blank? or self[:value].is_a?(String)
       YAML::dump(self[:value])
@@ -127,49 +146,48 @@ class RefinerySetting < ActiveRecord::Base
     end
   end
 
-  # Below is not very nice, but seems to be required. The problem is when Rails
-  # serialises a fields like booleans it doesn't retrieve it back out as a boolean
-  # it just returns a string. This code maps the two boolean values
-  # correctly so that a boolean is returned instead of a string.
-  REPLACEMENTS = {"true" => true, "false" => false}
-
   def value
-    unless (current_value = self[:value]).nil?
-      # This bit handles true and false so that true and false are actually returned
-      # not "0" and "1"
-      REPLACEMENTS.each do |current, new_value|
-        current_value = new_value if current_value == current
-      end
-
-      # converts the serialised value back to an integer
-      # if the value is an integer
-      begin
-        if current_value.to_i.to_s == current_value
-          current_value = current_value.to_i
-        end
-      rescue
-        current_value
-      end
-    end
-
-    current_value
+    replacements!(self[:value])
   end
 
   def value=(new_value)
     # must convert to string if true or false supplied otherwise it becomes 0 or 1, unfortunately.
-    new_value = new_value.to_s if %w(trueclass falseclass).include?(new_value.class.to_s.downcase)
-
-    self[:value] = new_value
+    if %w(trueclass falseclass).include?(new_value.class.to_s.downcase)
+      new_value = new_value.to_s
+    else
+      super
+    end
   end
 
   def callback_proc
     eval("Proc.new{#{self.callback_proc_as_string} }") if self.callback_proc_as_string.present?
   end
 
-  private
+private
+  # Below is not very nice, but seems to be required. The problem is when Rails
+  # serialises a fields like booleans it doesn't retrieve it back out as a boolean
+  # it just returns a string. This code maps the two boolean values
+  # correctly so that a boolean is returned instead of a string.
+  REPLACEMENTS = {"true" => true, "false" => false}
 
-  def check_restriction
-    self.restricted = false if restricted.nil?
+  def replacements!(current_value)
+    # This bit handles true and false so that true and false are actually returned
+    # not "0" and "1"
+    REPLACEMENTS.each do |current, new_value|
+      current_value = new_value if current_value == current
+    end
+
+    # converts the serialised value back to an integer
+    # if the value is an integer
+    begin
+      if current_value.to_i.to_s == current_value
+        current_value = current_value.to_i
+      end
+    rescue
+      current_value
+    end
+
+    current_value
   end
 
 end
