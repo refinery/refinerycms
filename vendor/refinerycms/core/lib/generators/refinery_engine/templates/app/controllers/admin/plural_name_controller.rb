a78fe65ca73f8c50970aad318b9891fe24145f02
Philip Arndt <parndt@gmail.com>
2010-09-06 16:21:06 +1200

    Ensure that generates can generate and destroy (and remove any changes introduced in destroy). Better handle the first string attribute as the first attribute may not be usable for many features.

--- vendor/refinerycms/core/lib/generators/refinery_engine/templates/app/controllers/admin/plural_name_controller.rb
+++ vendor/refinerycms/core/lib/generators/refinery_engine/templates/app/controllers/admin/plural_name_controller.rb
@@ -1,5 +1,7 @@
 class Admin::<%= class_name.pluralize %>Controller < Admin::BaseController
 
-  crudify :<%= singular_name %>, :title_attribute => :<%= attributes.first.name %>
+  crudify :<%= singular_name %><% if (title = attributes.detect { |a| a.type.to_s == "string" }).present? and title.name != 'title' %>
+  , :title_attribute => '<%= title.name %>'
+<% end %>
 
 end
