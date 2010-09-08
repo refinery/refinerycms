bd2c286afd121fef365956cfdcb696c88a1b894d
Philip Arndt <parndt@gmail.com>
2010-09-08 17:15:32 +1200

    RSpec now knows how to load specs from all engines using rake spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ def setup_environment
 
   # Requires supporting files with custom matchers and macros, etc,
   # in ./support/ and its subdirectories.
-  Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+  Dir[File.expand_path('../support/**/*.rb', __FILE__)].each {|f| require f}
 
   RSpec.configure do |config|
     # == Mock Framework
