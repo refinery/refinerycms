d4d7ca54c0dba3efee35f34c8dd0d0f875a197e9
Maarten Hoogendoorn <maarten@moretea.nl>
2010-09-13 15:50:54 +0200

    Updated menu rendering to use the nested set efficiently
    Instead of doing a query for each recursion, it shares the common set of pages,
    and does the selection of which pages should be displayed in which branch in ruby.

--- vendor/refinerycms/core/lib/refinery/application_controller.rb
+++ vendor/refinerycms/core/lib/refinery/application_controller.rb
@@ -79,7 +79,7 @@ module Refinery::ApplicationController
 
     # get all the pages to be displayed in the site menu.
     def find_pages_for_menu
-      @menu_pages = Page.top_level
+      @menu_pages = Page.order('lft ASC')
     end
 
     # use a different model for the meta information.
