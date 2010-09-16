805e902b626da2b9f87486f12ecd978d1c507c75
Philip Arndt <parndt@gmail.com>
2010-09-17 10:34:14 +1200

    Fix pagination's use of per_page.
    Added search feature to test the new search functionality of Refinery Settings (thanks Matt!) and used crudify more now that it works properly.

--- vendor/refinerycms/settings/app/controllers/admin/refinery_settings_controller.rb
+++ vendor/refinerycms/settings/app/controllers/admin/refinery_settings_controller.rb
@@ -3,17 +3,12 @@ class Admin::RefinerySettingsController < Admin::BaseController
   crudify :refinery_setting,
           :title_attribute => :title,
           :order => "name ASC",
-          :searchable => false,
+          :searchable => true,
+          :paging => true,
           :redirect_to_url => :redirect_to_where?
 
   before_filter :sanitise_params, :only => [:create, :update]
   after_filter :fire_setting_callback, :only => [:update]
-
-  def index
-    search_all_refinery_settings if searching?
-
-    paginate_all_refinery_settings
-  end
 
   def edit
     @refinery_setting = RefinerySetting.find(params[:id])
@@ -21,6 +16,7 @@ class Admin::RefinerySettingsController < Admin::BaseController
     render :layout => false if request.xhr?
   end
 
+protected
   def find_all_refinery_settings
     @refinery_settings = RefinerySetting.order('name ASC')
 
@@ -35,17 +31,8 @@ class Admin::RefinerySettingsController < Admin::BaseController
     # search for settings that begin with keyword
     term = "^" + params[:search].to_s.downcase.gsub(' ', '_')
 
-    # First find normal results.
-    find_all_refinery_settings
-
-    # Now get weighted results by running the query against the results already found.
-    @refinery_settings = @refinery_settings.with_query(term)
-  end
-
-  def paginate_all_refinery_settings
-    find_all_refinery_settings if @refinery_settings.nil?
-
-    @refinery_settings = @refinery_settings.paginate(:page => params[:page], :per_page => RefinerySetting.per_page)
+    # First find normal results, then weight them with the query.
+     @refinery_settings = find_all_refinery_settings.with_query(term)
   end
 
 private
