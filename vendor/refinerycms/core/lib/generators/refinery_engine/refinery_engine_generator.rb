bbf45d8ca18fdbb16a07b95188027c5d3b18d191
djones <dgjones@gmail.com>
2010-09-20 23:10:36 +1200

    getting closer to the install generator working

--- vendor/refinerycms/core/lib/generators/refinery_engine/refinery_engine_generator.rb
+++ vendor/refinerycms/core/lib/generators/refinery_engine/refinery_engine_generator.rb
@@ -12,10 +12,7 @@ class RefineryEngineGenerator < Rails::Generators::NamedBase
       Dir.glob(File.expand_path('../templates/**/**', __FILE__)).each do |path|
         # ignore directories which are created automatically by template()
         unless File.directory?(path)
-          # ignore migrations which are installed via the rake task 
-          if(path.gsub(File.dirname(__FILE__), "") !~ /^\/templates\/db\/migrate\//)
-            template path, plugin_path_for(path)
-          end
+          template path, plugin_path_for(path)
         end
       end
 
@@ -44,40 +41,9 @@ protected
 
   def plugin_path_for(path)
     path = path.gsub(File.dirname(__FILE__) + "/templates/", "vendor/engines/#{plural_name}/")
+  
     path = path.gsub("plural_name", plural_name)
     path = path.gsub("singular_name", singular_name)
   end
 
-end
-
-
-
-
-
-
-# Below is a hack until this issue:
-# https://rails.lighthouseapp.com/projects/8994/tickets/3820-make-railsgeneratorsmigrationnext_migration_number-method-a-class-method-so-it-possible-to-use-it-in-custom-generators
-# is fixed on the Rails project.
-
-require 'rails/generators/named_base'
-require 'rails/generators/migration'
-require 'rails/generators/active_model'
-require 'active_record'
-
-module ActiveRecord
-  module Generators
-    class Base < Rails::Generators::NamedBase #:nodoc:
-      include Rails::Generators::Migration
-
-      # Implement the required interface for Rails::Generators::Migration.
-      def self.next_migration_number(dirname) #:nodoc:
-        next_migration_number = current_migration_number(dirname) + 1
-        if ActiveRecord::Base.timestamped_migrations
-          [Time.now.utc.strftime("%Y%m%d%H%M%S"), "%.14d" % next_migration_number].max
-        else
-          "%.3d" % next_migration_number
-        end
-      end
-    end
-  end
 end
--- vendor/refinerycms/core/lib/generators/refinery_engine/templates/lib/generators/refinerycms_plural_name_generator.rb
+++ vendor/refinerycms/core/lib/generators/refinery_engine/templates/lib/generators/refinerycms_plural_name_generator.rb
@@ -7,10 +7,24 @@ module Refinery
     class EngineInstaller < Rails::Generators::Base
       include Rails::Generators::Migration
       
+      # Implement the required interface for Rails::Generators::Migration.
+      # taken from http://github.com/rails/rails/blob/master/activerecord/lib/generators/active_record.rb
+      def self.next_migration_number(dirname)
+        if ActiveRecord::Base.timestamped_migrations
+          Time.now.utc.strftime("%Y%m%d%H%M%S")
+        else
+          "%.3d" % (current_migration_number(dirname) + 1)
+        end
+      end
+      
       def generate
-        Dir.glob(File.expand_path('../templates/db/**', __FILE__)).each do |path|
+        Dir.glob(File.expand_path('../../../db/**/**', __FILE__)).each do |path|
           unless File.directory?(path)
-            migration_template plugin_path_for(path)
+            if path =~ /.*migrate.*/
+              migration_template plugin_path_for(path)
+            else
+              template plugin_path_for(path)
+            end
           end
         end
       end
@@ -18,9 +32,35 @@ module Refinery
     protected
 
       def plugin_path_for(path)
-        path = path.gsub(File.dirname(__FILE__) + "../../db/migrate", "db/migrate")
-        path = path.gsub("plural_name", plural_name)
-        path = path.gsub("singular_name", singular_name)
+        puts path
+        path = path.gsub(File.dirname(__FILE__) + "../db", "db")
+      end
+    end
+  end
+end
+
+# Below is a hack until this issue:
+# https://rails.lighthouseapp.com/projects/8994/tickets/3820-make-railsgeneratorsmigrationnext_migration_number-method-a-class-method-so-it-possible-to-use-it-in-custom-generators
+# is fixed on the Rails project.
+
+require 'rails/generators/named_base'
+require 'rails/generators/migration'
+require 'rails/generators/active_model'
+require 'active_record'
+
+module ActiveRecord
+  module Generators
+    class Base < Rails::Generators::NamedBase #:nodoc:
+      include Rails::Generators::Migration
+
+      # Implement the required interface for Rails::Generators::Migration.
+      def self.next_migration_number(dirname) #:nodoc:
+        next_migration_number = current_migration_number(dirname) + 1
+        if ActiveRecord::Base.timestamped_migrations
+          [Time.now.utc.strftime("%Y%m%d%H%M%S"), "%.14d" % next_migration_number].max
+        else
+          "%.3d" % next_migration_number
+        end
       end
     end
   end
