a9decb898b1a5c1621b86303b71c2392eb0abd11
Joe Sak <joe@joesak.com>
2010-09-21 20:13:51 -0500

    More Page RSpec coverage, and deprecated the .path() variable because reversing output by sending false feels weird. I honestly feel a little violated.

--- vendor/refinerycms/pages/spec/models/page_spec.rb
+++ vendor/refinerycms/pages/spec/models/page_spec.rb
@@ -15,30 +15,55 @@ describe Page do
     @page.destroy.should == false
   end
   
+  def create_child
+    @child = @page.children.create(:title => 'The child page')
+  end
+  
   before(:each) do
     reset_page
   end
   
-  context "a page cannot delete under certain rules" do
-    it "shouldn't allow me to destroy pages that are links" do
+  context "cannot be deleted under certain rules" do
+    it "if link_url is present" do
       reset_page({:link_url => '/plugin-name'})
       page_cannot_be_destroyed
     end
     
     
-    it "cannot be deleted if refinery team deems it so" do
+    it "if refinery team deems it so" do
       reset_page({:deletable => false})
       page_cannot_be_destroyed
     end
     
-    it "cannot be destroyed if have menu_match is filled out" do
+    it "if menu_match is present" do
       reset_page({:menu_match => '^/RSpec is great for testing too.*$'})
       page_cannot_be_destroyed
     end
     
-    it "will destroy if you really want it to! >:]" do
+    it "unless you really want it to! >:]" do
       reset_page
       @page.destroy!
+    end
+  end
+  
+  context "page urls" do
+    it "should return a full path" do
+      @page.path.should == 'RSpec is great for testing too'
+    end
+    
+    it "and all of its parent page titles, reversed" do
+      create_child
+      @child.path.should == 'RSpec is great for testing too - The child page'
+    end
+    
+    it "or normally ;-)" do
+      create_child
+      @child.path({:reversed => false}).should == 'The child page - RSpec is great for testing too'
+    end
+    
+    it ".path() still responds to the deprecated boolean" do
+      create_child
+      @child.path(false).should == 'The child page - RSpec is great for testing too'
     end
   end
   
