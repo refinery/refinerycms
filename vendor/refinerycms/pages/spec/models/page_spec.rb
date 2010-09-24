9f98335c2d10cbb26e9585788e8f9c817be6c0b8
Philip Arndt <parndt@gmail.com>
2010-09-25 02:17:10 +1200

    it 'should help the pages engine detect bugs more easily' do

--- vendor/refinerycms/pages/spec/models/page_spec.rb
+++ vendor/refinerycms/pages/spec/models/page_spec.rb
@@ -19,6 +19,11 @@ describe Page do
     @child = @page.children.create(:title => 'The child page')
   end
 
+  def create_page_parts
+    @page.parts.create(:title => 'body', :content => "I'm the first page part for this page.")
+    @page.parts.create(:title => 'side body', :content => "Closely followed by the second page part.")
+  end
+
   before(:each) do
     reset_page
   end
@@ -64,6 +69,56 @@ describe Page do
     it ".path() still responds to the deprecated boolean" do
       create_child
       @child.path(false).should == 'The child page - RSpec is great for testing too'
+    end
+  end
+
+  context "home page" do
+    it "should respond as the home page" do
+      @page.link_url = '/'
+      @page.home?.should == true
+    end
+
+    it "should not respond as the home page" do
+      @page.home?.should == false
+    end
+  end
+
+  context "content sections (page parts)" do
+    it "should return the content when using []" do
+      create_page_parts
+
+      @page[:body].should == "<p>I'm the first page part for this page.</p>"
+      @page["BoDY"].should == "<p>I'm the first page part for this page.</p>"
+    end
+
+    it "should return all page part content" do
+      create_page_parts
+
+      @page.all_page_part_content.should == "<p>I'm the first page part for this page.</p> <p>Closely followed by the second page part.</p>"
+    end
+
+    it "should reposition correctly" do
+      create_page_parts
+      @page.parts.first.position = 6
+      @page.parts.last.position = 4
+
+      @page.parts.first.position.should == 6
+      @page.parts.last.position.should == 4
+
+      @page.reposition_parts!
+
+      @page.parts.first.position.should == 0
+      @page.parts.last.position.should == 1
+    end
+  end
+
+  context "draft pages" do
+    it "should not be a live page when set to draft" do
+      @page.draft = true
+      @page.live?.should == false
+
+      @page.draft = false
+      @page.live?.should == true
     end
   end
 
