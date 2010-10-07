8c92c94ed591ef3ebc77f4fc637fa946486995fa
Bo Frederiksen <bofrede@bofrede.dk>
2010-10-07 12:36:39 +0200

    Rename old refinery setting; image_thumbnails to new user_image_sizes, used by Dragonfly.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100929035252) do
+ActiveRecord::Schema.define(:version => 20101006211228) do
 
   create_table "images", :force => true do |t|
     t.string   "image_mime_type"
