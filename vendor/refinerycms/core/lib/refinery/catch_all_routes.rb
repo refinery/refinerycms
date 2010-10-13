85f1e5914a82fd9f68b03928fb5b64fd72cad49d
Philip Arndt <parndt@gmail.com>
2010-10-13 14:45:02 +1300

    Deprecate /admin from the routes to have Refinery play nice with other applications/frameworks that you may be using.

--- vendor/refinerycms/core/lib/refinery/catch_all_routes.rb
+++ vendor/refinerycms/core/lib/refinery/catch_all_routes.rb
@@ -1,5 +1,11 @@
 Refinery::Application.routes.draw do
-  match '/admin' => redirect('/refinery')
-  match '/admin(/*path)' => redirect('/refinery/%{path}')
+  match '/admin(/*path)', :to => redirect {|params, request|
+    request.flash[:message] = "<p>
+      The URL '/<strong>admin</strong>#{"/#{params[:path]}" unless params[:path].blank?}' will be removed in Refinery CMS version 1.0
+      <br/>
+      Please use '/<strong>refinery</strong>#{"/#{params[:path]}" unless params[:path].blank?}' instead.
+    </p>".html_safe
+    "/refinery#{"/#{params[:path]}" unless params[:path].blank?}"
+  }
   match '/refinery/*path' => 'admin/base#error_404'
 end
