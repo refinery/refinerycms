f2ac720764efbd09c7534b95d3fc1f2ccc7ccb11
Peter Berkenbosch <peter@pero-ict.nl>
2010-10-13 21:55:31 +0200

    added basic skeleton for user specs.

---
+++ vendor/refinerycms/authentication/spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe User do
+  include AuthenticatedTestHelper
+  
+  context "Roles" do
+    it "does something" do
+      pending
+    end
+  end
+  
+  context "Plugins" do
+    it "does something" do
+      pending
+    end
+  end
+  
+  context "Authlogic config" do
+    it "does something" do
+      
+    end
+  end
+  
+end
