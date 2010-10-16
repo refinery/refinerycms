cd4594242ef73777bd61783903a9029d41b911e4
Peter Berkenbosch <peter@pero-ict.nl>
2010-10-16 10:35:53 +0200

    added factories.rb from auth/features to user_spec so I can use the factories in the rspec as well. added specs for adding a role to a user.

--- vendor/refinerycms/authentication/spec/models/user_spec.rb
+++ vendor/refinerycms/authentication/spec/models/user_spec.rb
@@ -1,23 +1,40 @@
 require 'spec_helper'
 
+Dir[File.expand_path('../../../features/support/factories.rb', __FILE__)].each {|f| require f}
+
 describe User do
   include AuthenticatedTestHelper
   
   context "Roles" do
-    it "does something" do
-      pending
+    context "add_role" do
+      it "raises Exception when Role object is passed" do
+        user = Factory(:user)
+        lambda{ user.add_role(Role.new)}.should raise_exception
+      end
+      
+      it "adds a Role to the User when role not yet assigned to User" do
+        user = Factory(:user)
+        lambda {
+          user.add_role(:new_role)
+        }.should change(user.roles, :count).by(1)
+        user.roles.collect(&:title).should include("NewRole")
+      end
+      
+      it "does not add a Role to the User when this Role is already assigned to User" do
+        user = Factory(:refinery_user)
+        lambda {
+          user.add_role(:refinery)
+        }.should_not change(user.roles, :count).by(1)
+        user.roles.collect(&:title).should include("Refinery")
+      end
     end
+    
   end
   
-  context "Plugins" do
-    it "does something" do
-      pending
-    end
-  end
   
   context "Authlogic config" do
     it "does something" do
-      
+      pending
     end
   end
   
