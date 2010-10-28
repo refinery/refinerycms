955b378e6c2e0e6a7c86c27156850938980cfc66
Philip Arndt <parndt@gmail.com>
2010-10-29 09:27:28 +1300

    Not a hackety hack

--- vendor/refinerycms/images/lib/images.rb
+++ vendor/refinerycms/images/lib/images.rb
@@ -45,13 +45,13 @@ module Refinery
         app_images.analyser.register(Dragonfly::Analysis::RMagickAnalyser)
         app_images.analyser.register(Dragonfly::Analysis::FileCommandAnalyser)
 
-        # This eval makes it so that dragonfly urls work in traditional
+        # This url_suffix makes it so that dragonfly urls work in traditional
         # situations where the filename and extension are required, e.g. lightbox.
         # What this does is takes the url that is about to be produced e.g.
         # /system/images/BAhbB1sHOgZmIiMyMDEwLzA5LzAxL1NTQ19DbGllbnRfQ29uZi5qcGdbCDoGcDoKdGh1bWIiDjk0MngzNjAjYw
         # and adds the filename onto the end (say the image was 'refinery_is_awesome.jpg')
         # /system/images/BAhbB1sHOgZmIiMyMDEwLzA5LzAxL1NTQ19DbGllbnRfQ29uZi5qcGdbCDoGcDoKdGh1bWIiDjk0MngzNjAjYw/refinery_is_awesome.jpg
-        # from: http://markevans.github.com/dragonfly/file.URLs.html
+        # Officially the way to do it, from: http://markevans.github.com/dragonfly/file.URLs.html
         app_images.url_suffix = proc{|job|
           "/#{job.uid_basename}#{job.encoded_extname || job.uid_extname}"
         }
