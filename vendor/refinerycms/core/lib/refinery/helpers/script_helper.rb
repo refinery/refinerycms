7c343fb1bc597b1a6496003861e99773745bef5a
Philip Arndt <parndt@gmail.com>
2010-11-01 11:49:46 +1300

    Lock to jQuery 1.4.2 and jQuery UI 1.8.5. Closes GH-261 and maybe some more?

--- vendor/refinerycms/core/lib/refinery/helpers/script_helper.rb
+++ vendor/refinerycms/core/lib/refinery/helpers/script_helper.rb
@@ -25,8 +25,8 @@ module Refinery
         else
           "#{javascript_include_tag("http://www.google.com/jsapi").gsub(".js", "")}
           <script type='text/javascript'>
-            google.load('jquery', '1.4');
-            #{"google.load('jqueryui', '1.8');" if options[:jquery_ui]}
+            google.load('jquery', '1.4.2');
+            #{"google.load('jqueryui', '1.8.5');" if options[:jquery_ui]}
           </script>".html_safe
         end
       end
