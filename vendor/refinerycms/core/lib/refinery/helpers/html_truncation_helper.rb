d39d3d8d1ef9c569429af22cbee47ec596cf3432
Philip Arndt <parndt@gmail.com>
2010-11-05 19:16:19 +1300

    settings now can be check_box too.

--- vendor/refinerycms/core/lib/refinery/helpers/html_truncation_helper.rb
+++ vendor/refinerycms/core/lib/refinery/helpers/html_truncation_helper.rb
@@ -8,11 +8,10 @@ module Refinery
       # <script> tags pass through and are not counted in the total.
       # the omission specified _does_ count toward the total length count.
       # use :link => link_to('more', post_path), or something to that effect
-      def truncate(text, *args)
+      def truncate(text, options = {})
         return unless text.present?
-        return super unless ((arguments = args.dup).extract_options![:preserve_html_tags] == true) # don't ruin the current args object
+        return super unless options.delete(:preserve_html_tags) == true # ensure preserve_html_tags doesn't pass through
 
-        options = args.extract_options!
         max_length = options[:length] || 30
         omission = options[:omission] || "..."
 
