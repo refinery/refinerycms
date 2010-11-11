73dde1db689301256096be8ec3bbd17ad5e255a0
Philip Arndt <parndt@gmail.com>
2010-11-12 12:22:55 +1300

    Wrote some more specs for RefinerySetting when using check_box form_value_type which exposed some errors so I fixed them. Sorted out some whitespace in the specs.

--- vendor/refinerycms/images/spec/models/image_spec.rb
+++ vendor/refinerycms/images/spec/models/image_spec.rb
@@ -19,30 +19,30 @@ describe Image do
   before(:each) do
     reset_image
   end
-  
+
   context "with valid attributes" do
     it "should create successfully" do
       @image.errors.empty?
     end
   end
-  
+
   context "image url" do
     it "should respond to .thumbnail" do
       @image.respond_to?(:thumbnail).should == true
     end
-    
+
     it "should contain its filename at the end" do
       @image.thumbnail(nil).url.should =~ %r{#{@image.image_uid.split('/').last}$}
     end
-    
+
     it "should be different when supplying geometry" do
       @image.thumbnail(nil).url.should_not == @image.thumbnail('200x200').url
     end
-    
+
     it "should have different urls for each geometry string" do
       @image.thumbnail('200x200').url.should_not == @image.thumbnail('200x201').url
     end
-    
+
   end
 
 
