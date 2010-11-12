d1bea6dd8d60b13ab4860757e6ba28fc8f812f6a
Philip Arndt <parndt@gmail.com>
2010-11-12 14:22:29 +1300

    Markedly improve the display of the site bar editor switch button in IE.

--- vendor/refinerycms/core/public/javascripts/refinery/site_bar.js
+++ vendor/refinerycms/core/public/javascripts/refinery/site_bar.js
@@ -3,8 +3,9 @@ if (typeof($) == 'function') {
     $logo = $('#site_bar_content #site_bar_refinery_cms_logo');
     $logo.css('left', ($('#site_bar_content').width() / 2) - ($logo.width() / 2));
 
-    if ($.isFunction($('#editor_switch a').corner)) {
-      $('#editor_switch a').corner('6px');
+    $switch_anchor = $('#editor_switch a').not('.ie7 #editor_switch a, .ie6 #editor_swtich a');
+    if ($.isFunction($switch_anchor.corner)) {
+      $switch_anchor.corner('6px');
     }
 
     $('#editor_switch a').appendTo((span = $('<span></span>').prependTo('#editor_switch')));
