47a5da1bdd8fdbd37c437b918294216174ff5dc6
Philip Arndt <parndt@gmail.com>
2010-11-30 17:34:28 +1300

    Implemented Pathname for less code and more fun. Added rails generate refinerycms as a generator to copy the migrations and seeds to the Rails app from the Refinery app.

--- vendor/refinerycms/core/lib/generators/refinerycms_generator.rb
+++ vendor/refinerycms/core/lib/generators/refinerycms_generator.rb
@@ -1,1 +1,6 @@
-class RefinerycmsGenerator < 
+class RefinerycmsGenerator < ::Refinery::Generators::EngineInstaller
+
+  engine_name :refinerycms
+  source_root Refinery.root
+
+end
--- vendor/refinerycms/core/lib/refinery/generators.rb
+++ vendor/refinerycms/core/lib/refinery/generators.rb
@@ -1,3 +1,5 @@
+require 'pathname'
+
 module Refinery
   module Generators
     # The core engine installer streamlines the installation of custom generated
@@ -6,31 +8,36 @@ module Refinery
     class EngineInstaller < Rails::Generators::Base
       include Rails::Generators::Migration
 
-      def self.engine_name(name = nil)
-        @engine_name = name unless name.nil?
-        @engine_name
-      end
+      class << self
+        def engine_name(name = nil)
+          @engine_name = name.to_s unless name.nil?
+          @engine_name
+        end
 
-      # Implement the required interface for Rails::Generators::Migration.
-      # taken from http://github.com/rails/rails/blob/master/activerecord/lib/generators/active_record.rb
-      # can be removed once this issue is fixed:
-      # # https://rails.lighthouseapp.com/projects/8994/tickets/3820-make-railsgeneratorsmigrationnext_migration_number-method-a-class-method-so-it-possible-to-use-it-in-custom-generators
-      def self.next_migration_number(dirname)
-        if ActiveRecord::Base.timestamped_migrations
-          Time.now.utc.strftime("%Y%m%d%H%M%S")
-        else
-          "%.3d" % (current_migration_number(dirname) + 1)
+        def source_root(root = nil)
+          Pathname.new(super.to_s)
+        end
+
+        # Implement the required interface for Rails::Generators::Migration.
+        # taken from http://github.com/rails/rails/blob/master/activerecord/lib/generators/active_record.rb
+        # can be removed once this issue is fixed:
+        # # https://rails.lighthouseapp.com/projects/8994/tickets/3820-make-railsgeneratorsmigrationnext_migration_number-method-a-class-method-so-it-possible-to-use-it-in-custom-generators
+        def next_migration_number(dirname)
+          if ActiveRecord::Base.timestamped_migrations
+            Time.now.utc.strftime("%Y%m%d%H%M%S")
+          else
+            "%.3d" % (current_migration_number(dirname) + 1)
+          end
         end
       end
 
       def generate
-        Dir.glob(File.expand_path(File.join(self.class.source_root, '../db/**/**'))).each do |path|
-          unless File.directory?(path)
-            if path =~ /.*migrate.*/
-              migration_template path, Rails.root.join("db/migrate/#{path.split(File::SEPARATOR).last.split('.rb').first}")
-            else
-              template path, Rails.root.join("db/seeds/#{path.split(File::SEPARATOR).last}")
-            end
+        Pathname.glob(self.class.source_root.join('db', '**', '*.rb')) do |path|
+          case path.to_s
+          when %r{.*/migrate/[^\d].*}
+            migration_template path.to_s, Rails.root.join("db/migrate/#{path.split.last.to_s.split('.rb').first}")
+          when %r{.*/seeds.*}
+            template path.to_s, Rails.root.join(path.to_s.split('/db/').first, 'db', path.to_s.split('/db/').last)
           end
         end
 
