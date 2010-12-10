6204ca8d904f21be6f75a5a1a391dae4c7f560a0
Philip Arndt <parndt@gmail.com>
2010-12-10 20:03:59 +1300

    Trying to get things to load without copying so many files in.

--- Rakefile
+++ Rakefile
@@ -5,4 +5,3 @@ require File.expand_path('../config/application', __FILE__)
 require 'rake'
 
 RefineryApp::Application.load_tasks
-Refinery::Application.load_tasks
--- vendor/refinerycms/core/lib/refinery/application.rb
+++ vendor/refinerycms/core/lib/refinery/application.rb
@@ -53,7 +53,7 @@ module Refinery
         # Bring in our tasks to the main app.
         Dir[Refinery.root.join('vendor', 'refinerycms', 'core', 'lib', 'tasks', '*.rake').to_s].each do |task|
           load task.to_s
-        end
+        end unless Refinery.root == Rails.root
       end
     end
   end
