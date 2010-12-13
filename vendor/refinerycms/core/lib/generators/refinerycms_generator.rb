69b074c67761a277971b9bf3e74fde94886c4549
Philip Arndt <parndt@gmail.com>
2010-12-13 23:23:57 +1300

    Most of the work can be done for us with thor.

--- vendor/refinerycms/core/lib/generators/refinerycms_generator.rb
+++ vendor/refinerycms/core/lib/generators/refinerycms_generator.rb
@@ -6,59 +6,55 @@ class RefinerycmsGenerator < ::Refinery::Generators::EngineInstaller
   source_root Refinery.root
 
   def generate
-    # First, rename files that get in the way of Refinery CMS
-    %w(public/index.html app/views/layouts/application.html.erb db/seeds.rb).each do |roadblock|
+    # First, effectively move / rename files that get in the way of Refinery CMS
+    %w(public/index.html app/views/layouts/application.html.erb config/cucumber.yml).each do |roadblock|
       if (roadblock = Rails.root.join(roadblock)).file?
-        rename_to = roadblock.to_s.gsub(roadblock.split.last.to_s, "your_#{roadblock.split.last}")
-        FileUtils::mv roadblock, rename_to
-        puts "Renamed #{roadblock.to_s.split("#{Rails.root}/").last} to #{rename_to.to_s.split("#{Rails.root}/").last}"
+        append_file "your_#{roadblock.split.last}", :verbose => true do roadblock.read end
+        remove_file roadblock, :verbose => true
       end
+    end
+
+    # Append seeds.
+    append_file 'db/seeds.rb', :verbose => true do
+      Refinery.root.join('db', 'seeds.rb').read
     end
 
     # Overwrite rails defaults with refinery defaults
-    %w(spec).each do |folder|
-      FileUtils::cp_r Refinery.root.join(folder), Rails.root, :verbose => true
+    Dir[Refinery.root.join('spec', '*.*').to_s].each do |file|
+      copy_file file, :verbose => true
     end
 
     # Copy in 'important' files to the Rails app.
-    %w(.rspec config/cucumber.yml).each do |file|
-      # Backup their version if it exists
-      if Rails.root.join(file).file? and !(backup = Rails.root.join(file)).file?
-        (backup = Rails.root.join(file).split('/'))[-1] = "your_#{backup.last}"
-        FileUtils::mv Rails.root.join(file), backup.to_s, :verbose => true
-      end
-
-      FileUtils::cp Refinery.root.join(file), Rails.root.join(file),
-                    :verbose => true if Refinery.root.join(file).exist?
+    %w(.rspec config/cucumber.yml).map{|f| Refinery.root.join(f)}.reject{|f| !f.exist?}.each do |file|
+      copy_file Refinery.root.join(file), :verbose => true
     end
 
-    # Copy the 'client app version' of the admin javascript file, unless it exists.
-    if (admin_js = Refinery.root.join('vendor', 'refinerycms', 'core', 'public', 'javascripts', 'admin.js')).file?
-      FileUtils::cp admin_js,
-                    Rails.root.join('public', 'javascripts').to_s,
-                    :verbose => true unless Rails.root.join('public', 'javascripts', 'admin.js').file?
-    end
-
-    # Copy stylesheets so they're ready to use, unless they exist.
+    # Copy asset files (JS, CSS) so they're ready to use.
     %w(application.css formatting.css home.css theme.css).map{ |ss|
       Refinery.root.join('vendor', 'refinerycms', 'core', 'public', 'stylesheets', ss)
-    }.reject{|ss| !ss.file? or Rails.root.join('public', 'stylesheets', ss.split.last).file?}.each do |stylesheet|
-      FileUtils::cp stylesheet, Rails.root.join('public', 'stylesheets').to_s,
-                    :verbose => true
+    }.reject{|ss| !ss.file?}.each do |stylesheet|
+      copy_file stylesheet,
+                Rails.root.join('public', 'stylesheets', stylesheet.split.last),
+                :verbose => true
     end
+    copy_file Refinery.root.join('vendor', 'refinerycms', 'core', 'public', 'javascripts', 'admin.js'),
+              Rails.root.join('public', 'javascripts', 'admin.js'),
+              :verbose => true
+
 
     # Ensure the config.serve_static_assets setting is present and enabled
     %w(development test production).map{|e| "config/environments/#{e}.rb"}.each do |env|
       contents = Rails.root.join(env).read
       contents.gsub! %r{#.*config.serve_static_assets}, 'config.serve_static_assets'
       contents.gsub! "serve_static_assets = false", "serve_static_assets = true # Refinery CMS requires this to be true"
-      contents << "\nRefinery.rescue_not_found = #{env.split('/').last.split('.rb').first == 'production'}"
+
       Rails.root.join(env).open('w') { |file| file.puts(contents) }
+
+      append_file env, "Refinery.rescue_not_found = #{env.split('/').last.split('.rb').first == 'production'}"
     end
 
     # Ensure .gitignore exists and append our stuff to it.
-    FileUtils::touch(gitignore = Rails.root.join('.gitignore'))
-    gitignore.open('a+').puts Refinery.root.join('.gitignore').read.split('# REFINERY CMS DEVELOPMENT').first
+    append_file ".gitignore", Refinery.root.join('.gitignore').read.split('# REFINERY CMS DEVELOPMENT').first
 
     super
   end
