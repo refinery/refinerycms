3290e4468d539abd98b967f744b9598f42157da9
Philip Arndt <parndt@gmail.com>
2010-12-17 11:45:09 +1300

    Update to friendly_id 3.1.8 which has fixes for AR 3.0.3 compatibility ( and we use that version, so... )

--- lib/gemspec.rb
+++ lib/gemspec.rb
@@ -46,7 +46,7 @@ Gem::Specification.new do |s|
   s.add_dependency    'moretea-awesome_nested_set', '= 1.4.3.1'
 
   s.add_dependency    'acts_as_indexed',      '~> 0.6.6'
-  s.add_dependency    'friendly_id',          '~> 3.1.6'
+  s.add_dependency    'friendly_id',          '~> 3.1.8'
   s.add_dependency    'truncate_html',        '= 0.4'
   s.add_dependency    'will_paginate',        '~> 3.0.pre'
   s.add_dependency    'authlogic',            '~> 2.1.6'
