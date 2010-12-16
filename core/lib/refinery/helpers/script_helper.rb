74d8573c6f7de68004628a109fea12e2603c3b5f
Philip Arndt <parndt@gmail.com>
2010-12-16 15:41:41 +1300

    Clean up

--- core/lib/refinery/helpers/script_helper.rb
+++ core/lib/refinery/helpers/script_helper.rb
@@ -9,7 +9,7 @@ module Refinery
       def jquery_include_tags(options={})
         # Merge in options
         options = {
-          :caching => Rails.root.writable? and RefinerySetting.find_or_set(:use_resource_caching, true),
+          :caching => (Rails.root.writable? and RefinerySetting.find_or_set(:use_resource_caching, true)),
           :google => RefinerySetting.find_or_set(:use_google_ajax_libraries, false),
           :jquery_ui => true
         }.merge(options)
