c3076df0368498599853f0189b1af6fb7c9d6847
Philip Arndt <parndt@gmail.com>
2010-12-20 15:25:27 +1300

    We're now using switch_locale and making this more generic so that it can be used in more places.

--- pages/app/controllers/admin/pages_controller.rb
+++ pages/app/controllers/admin/pages_controller.rb
@@ -1,39 +1,39 @@
-class Admin::PagesController < Admin::BaseController
+module Admin
+  class PagesController < Admin::BaseController
 
-  crudify :page,
-          :conditions => {:parent_id => nil},
-          :order => "lft ASC",
-          :include => [:parts, :slugs, :children, :parent, :translations],
-          :paging => false
+    crudify :page,
+            :conditions => {:parent_id => nil},
+            :order => "lft ASC",
+            :include => [:parts, :slugs, :children, :parent, :translations],
+            :paging => false
 
-  before_filter :globalize
+    rescue_from FriendlyId::ReservedError, :with => :show_errors_for_reserved_slug
 
-  rescue_from FriendlyId::ReservedError, :with => :show_errors_for_reserved_slug
+    cache_sweeper :page_sweeper, :only => [:create, :update, :destroy, :update_positions]
 
-  cache_sweeper :page_sweeper, :only => [:create, :update, :destroy, :update_positions]
-
-  def new
-    @page = Page.new
-    Page.default_parts.each_with_index do |page_part, index|
-      @page.parts << PagePart.new(:title => page_part, :position => index)
+    def new
+      @page = Page.new
+      Page.default_parts.each_with_index do |page_part, index|
+        @page.parts << PagePart.new(:title => page_part, :position => index)
+      end
     end
-  end
 
-protected
+  protected
 
-  def globalize
-    Thread.current[:globalize_locale] = (params[:page_locale] || (@page.present? && @page.slug.locale) || ::Refinery::I18n.default_frontend_locale)
-  end
-
-  def show_errors_for_reserved_slug(exception)
-    flash[:error] = "Sorry, but that title is a reserved system word."
-    if params[:action] == 'update'
-      find_page
-      render :edit
-    else
-      @page = Page.new(params[:page])
-      render :new
+    def globalize!
+      Thread.current[:globalize_locale] = (params[:switch_locale] || (@page.present? && @page.slug.locale) || ::Refinery::I18n.default_frontend_locale)
     end
-  end
 
+    def show_errors_for_reserved_slug(exception)
+      flash[:error] = "Sorry, but that title is a reserved system word."
+      if params[:action] == 'update'
+        find_page
+        render :edit
+      else
+        @page = Page.new(params[:page])
+        render :new
+      end
+    end
+
+  end
 end
