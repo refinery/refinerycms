074f9005f85ceb9535ac46a1486ef7beb5737b39
Earle Clubb <earle@handfulofclubbs.net>
2010-12-20 22:15:52 -0500

    Add :from key to UserMailer for password reset.

--- vendor/refinerycms/authentication/app/mailers/user_mailer.rb
+++ vendor/refinerycms/authentication/app/mailers/user_mailer.rb
@@ -5,6 +5,12 @@ class UserMailer < ActionMailer::Base
     subject I18n.translate('user_mailer.link_to_reset_your_password')
     @url = reset_users_url(:host => request.host_with_port,
                            :reset_code => user.perishable_token)
+
+    domain = request.domain(RefinerySetting.find_or_set(:tld_length, 1))
+
+    mail(:to => user.email,
+         :subject => I18n.translate('user_mailer.link_to_reset_your_password'),
+         :from => "\"#{RefinerySetting[:site_name]}\" <no-reply@#{domain}>")
   end
 
 protected
