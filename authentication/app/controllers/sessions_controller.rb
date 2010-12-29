17278f5bd9a9bb0450c5a8c1b552b9cfb5485a29
Uģis Ozols <ugis.ozolss@gmail.com>
2010-12-29 07:56:23 +0200

    Force password reset for passwords which aren't encrypted using bcrypt.

--- authentication/app/controllers/sessions_controller.rb
+++ authentication/app/controllers/sessions_controller.rb
@@ -1,3 +1,10 @@
 class SessionsController < Devise::SessionsController
   layout 'login'
+
+  def create
+    super
+  rescue BCrypt::Errors::InvalidSalt
+    flash[:error] = t('.password_encryption', :scope => 'users.forgot')
+    redirect_to new_user_password_path
+  end
 end
