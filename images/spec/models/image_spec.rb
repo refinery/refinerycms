b8d6759f606f36517fa06bf94d2cf0e730c30324
Uģis Ozols <ugis.ozolss@gmail.com>
2011-01-15 15:57:40 +0200

    Added more rspec tests for image model.

--- images/spec/models/image_spec.rb
+++ images/spec/models/image_spec.rb
@@ -50,4 +50,38 @@ describe Image do
 
   end
 
+  describe "#title" do
+    it "returns a titleized version of the filename" do
+      @image.title.should == "Beach"
+    end
+  end
+
+  describe ".per_page" do
+    context "dialog is true" do
+      context "has_size_options is true" do
+        it "returns image count specified by PAGES_PER_DIALOG_THAT_HAS_SIZE_OPTIONS constant" do
+          Image.per_page(true, true).should == Image::PAGES_PER_DIALOG_THAT_HAS_SIZE_OPTIONS
+        end
+      end
+
+      context "has_size_options is false" do
+        it "returns image count specified by PAGES_PER_DIALOG constant" do
+          Image.per_page(true).should == Image::PAGES_PER_DIALOG
+        end
+      end
+    end
+
+    context "dialog is false" do
+      it "returns image count specified by PAGES_PER_ADMIN_INDEX constant" do
+        Image.per_page.should == Image::PAGES_PER_ADMIN_INDEX
+      end
+    end
+  end
+
+  describe ".user_image_sizes" do
+    it "sets and returns a hash consisiting of 3 image sizes" do
+      Image.user_image_sizes.keys.should == [:small, :medium, :large]
+    end
+  end
+
 end
