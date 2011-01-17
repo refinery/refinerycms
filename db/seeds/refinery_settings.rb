2b8abed94f3768ffb7c656ea4725d08cc2d3fc1d
Philip Arndt <parndt@gmail.com>
2011-01-17 19:58:21 +1300

    Removed all default refinery_setting seeds in favour of using find_or_set to set them as required.

--- db/seeds/refinery_settings.rb
+++
@@ -1,9 +0,0 @@
-[
-  {:name => "site_name", :value => "Company Name"},
-  {:name => "new_page_parts", :value => false, :form_type => 'check_box'},
-  {:name => "activity_show_limit", :value => 7},
-  {:name => "preferred_image_view", :value => :grid},
-  {:name => "analytics_page_code", :value => "UA-xxxxxx-x"}
-].each do |setting|
-  RefinerySetting.create(:name => setting[:name].to_s, :value => setting[:value], :destroyable => false, :form_value_type => (setting[:form_type] || 'text_area'))
-end
