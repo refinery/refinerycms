5df2808b9e8ae5059afb45a029b7d6b10b78b609
Philip Arndt <parndt@gmail.com>
2011-01-18 21:38:31 +1300

    Fix incorrect translation call (don't put . when scoping)

--- authentication/app/controllers/sessions_controller.rb
+++ authentication/app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < Devise::SessionsController
   def create
     super
   rescue BCrypt::Errors::InvalidSalt
-    flash[:error] = t('.password_encryption', :scope => 'users.forgot')
+    flash[:error] = t('password_encryption', :scope => 'users.forgot')
     redirect_to new_user_password_path
   end
 
