9c41eae8b1eeaa77f87df8f42cb9e9854e35fb30
Philip Arndt <parndt@gmail.com>
2011-01-19 14:20:09 +1300

    Made it so that the generator actually copies the files that it was always supposed to.

---
+++ config/settings.rb
@@ -0,0 +1,3 @@
+# Other settings go here as below.
+# These settings are ones that are set for all environments and need to be set AFTER the refinery boot process.
+Refinery.authentication_login_field = 'login'
