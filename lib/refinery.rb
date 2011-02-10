d8a8bb0c3efbcf339890a70f39753ffc005183e4
Philip Arndt <parndt@gmail.com>
2011-02-10 20:05:38 +1300

    Complete time profiling. Core is slowest, obviously. Let's get this down.

--- lib/refinery.rb
+++ lib/refinery.rb
@@ -1,11 +1,26 @@
 time = Time.now
 require 'refinerycms-base'
+last_time = Time.now
+$stdout.puts "It took #{Time.now - time} seconds to load refinerycms-base"
 require 'refinerycms-core'
+$stdout.puts "It took #{Time.now - last_time} seconds to load refinerycms-core"
+last_time = Time.now
 
 require 'refinerycms-authentication'
+$stdout.puts "It took #{Time.now - last_time} seconds to load refinerycms-authentication"
+last_time = Time.now
 require 'refinerycms-settings'
+$stdout.puts "It took #{Time.now - last_time} seconds to load refinerycms-settings"
+last_time = Time.now
 require 'refinerycms-images'
+$stdout.puts "It took #{Time.now - last_time} seconds to load refinerycms-images"
+last_time = Time.now
 require 'refinerycms-resources'
+$stdout.puts "It took #{Time.now - last_time} seconds to load refinerycms-resources"
+last_time = Time.now
 require 'refinerycms-pages'
+$stdout.puts "It took #{Time.now - last_time} seconds to load refinerycms-pages"
+last_time = Time.now
 require 'refinerycms-dashboard'
+$stdout.puts "It took #{Time.now - last_time} seconds to load refinerycms-dashboard"
 $stdout.puts "It took #{Time.now - time} seconds to load all required engines."
