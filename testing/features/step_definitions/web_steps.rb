e7bba885b25ea44f7b89b5f2c52246d40145a5d5
Josef Simanek <retro@ballgag.cz>
2011-03-03 14:38:26 +0100

    Yoda conditions like matcher in web step. Patch sent to cucumber-rails also.

--- testing/features/step_definitions/web_steps.rb
+++ testing/features/step_definitions/web_steps.rb
@@ -195,7 +195,7 @@ end
 Then /^(?:|I )should be on (.+)$/ do |page_name|
   current_path = URI.parse(current_url).path
   if current_path.respond_to? :should
-    current_path.should == path_to(page_name)
+    path_to(page_name).should == current_path
   else
     assert_equal path_to(page_name), current_path
   end
