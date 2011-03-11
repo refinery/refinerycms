4492f27b90c00f1fdc53a6b791f4606a632530ee
Philip Arndt <parndt@gmail.com>
2011-03-11 14:18:51 +1300

    Refactored the test_url method to use get_response which should be quicker and with less conditional code.

--- pages/app/controllers/admin/pages_dialogs_controller.rb
+++ pages/app/controllers/admin/pages_dialogs_controller.rb
@@ -48,27 +48,27 @@ module Admin
     end
 
     def test_url
-      unless params[:url].blank?
-        url = URI.parse(params[:url])
-        if url.host.nil? && params[:url].start_with?('/')
-          url.host = URI.parse(request.url).host
+      begin
+        timeout(5) do
+          unless params[:url].blank?
+            url = URI.parse(params[:url])
+            if url.host.nil? && params[:url].start_with?('/')
+              url.host = URI.parse(request.url).host
+            end
+
+            result = case Net::HTTP.get_response(url)
+              when Net::HTTPSuccess, Net::HTTPRedirection
+                'success'
+              else
+                'failure'
+              end
+
+            render :json => {:result => result}
+          end
         end
-
-        http = Net::HTTP.new(url.host)
-        request = Net::HTTP::Get.new(url.path.blank? ? "/" : url.path)
-
-        response = http.request request
-
-        render :json => {:result => case response
-        when Net::HTTPSuccess, Net::HTTPRedirection
-          'success'
-        else
-          'failure'
-        end }
-      end
-
       rescue
         render :json => {:result => 'failure'}
+      end
     end
 
     def test_email
