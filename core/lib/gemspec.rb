ee8e0db2d08be5224cf15acc3dcc6a46199d9766
Philip Arndt <parndt@gmail.com>
2011-03-11 14:48:13 +1300

    We may someday want to update the moretea-awesome_nested_set dependency to a higher version.

--- core/lib/gemspec.rb
+++ core/lib/gemspec.rb
@@ -25,7 +25,7 @@ Gem::Specification.new do |s|
   s.add_dependency 'acts_as_indexed',             '~> 0.7'
   s.add_dependency 'friendly_id_globalize3',      '~> 3.2.1'
   s.add_dependency 'globalize3',                  '>= 0.1.0.beta'
-  s.add_dependency 'moretea-awesome_nested_set',  '~> 1.4.3.1'
+  s.add_dependency 'moretea-awesome_nested_set',  '~> 1.4'
   s.add_dependency 'rails',                       '~> 3.0.5'
   s.add_dependency 'rdoc',                        '>= 2.5.11' # helps fix ubuntu
   s.add_dependency 'truncate_html',               '~> 0.5'
