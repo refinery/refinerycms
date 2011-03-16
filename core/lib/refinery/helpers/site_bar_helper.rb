c686251226d32f7fc9df0e16f726208f2158870a
Philip Arndt <parndt@gmail.com>
2011-03-17 09:12:52 +1300

    wrapped up site bar translation locale logic into a method.

--- core/lib/refinery/helpers/site_bar_helper.rb
+++ core/lib/refinery/helpers/site_bar_helper.rb
@@ -4,18 +4,26 @@ module Refinery
 
       # Generates the link to determine where the site bar switch button returns to.
       def site_bar_switch_link
-        link_to_if(admin?, t('.switch_to_your_website', :locale => (defined?(::Refinery::I18n) ? ::Refinery::I18n.current_locale : :en),
+        link_to_if(admin?, t('.switch_to_your_website', site_bar_translate_locale_args),
                   (if session.keys.include?(:website_return_to) and session[:website_return_to].present?
                     session[:website_return_to]
                    else
                     root_path(:locale => (::Refinery::I18n.default_frontend_locale if defined?(::Refinery::I18n) && ::Refinery::I18n.enabled?))
                    end)) do
-          link_to t('.switch_to_your_website_editor', :locale => (defined?(::Refinery::I18n) ? ::Refinery::I18n.current_locale : :en)),
+          link_to t('.switch_to_your_website_editor', site_bar_translate_locale_args),
                   (if session.keys.include?(:refinery_return_to) and session[:refinery_return_to].present?
                     session[:refinery_return_to]
                    else
                     admin_root_path
                    end rescue admin_root_path)
+        end
+      end
+
+      def site_bar_translate_locale_args
+        if defined?(::Refinery::I18n)
+          {:locale => ::Refinery::I18n.current_locale}
+        else
+          {}
         end
       end
 
