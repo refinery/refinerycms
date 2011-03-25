d0d61d98b2e1d46d547e01b12ac64a109a79f38d
Philip Arndt <parndt@gmail.com>
2011-03-25 18:48:23 +1300

    The Refinery.root must be a pathname.

--- lib/refinery.rb
+++ lib/refinery.rb
@@ -4,4 +4,4 @@ require File.expand_path('../refinerycms', __FILE__)
 
 # Override Refinery.root
 # Must come after requiring 'refinerycms'
-Refinery.root = File.expand_path('../../', __FILE__)
+Refinery.root = Pathname.new(File.expand_path('../../', __FILE__))
