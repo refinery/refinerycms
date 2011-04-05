c8bc1c6639bfce7ad8d1aabf3d23c943aa598c52
Philip Arndt <parndt@gmail.com>
2011-04-06 11:24:20 +1200

    Refinery on Rails 3.0.6 (upped requirement to Rails 3.0.6 - see http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-talk/381027)

--- core/lib/gemspec.rb
+++ core/lib/gemspec.rb
@@ -26,7 +26,7 @@ Gem::Specification.new do |s|
   s.add_dependency 'friendly_id_globalize3',      '~> 3.2.1'
   s.add_dependency 'globalize3',                  '>= 0.1.0.beta'
   s.add_dependency 'moretea-awesome_nested_set',  '~> 1.4'
-  s.add_dependency 'rails',                       '~> 3.0.5'
+  s.add_dependency 'rails',                       '~> 3.0.6'
   s.add_dependency 'rdoc',                        '>= 2.5.11' # helps fix ubuntu
   s.add_dependency 'truncate_html',               '~> 0.5'
   s.add_dependency 'will_paginate',               '~> 3.0.pre'
