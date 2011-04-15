0f9b918e3f4277d3acd36978a3fd82a1bf7ec68f
Mark Haylock <mark@resolvedigital.co.nz>
2011-04-15 15:25:47 +1200

    Fixed Refinery.root, Fixed generator templates, Added refinerycms-i18n generator to refinerycms generator if i18n is included.

--- lib/refinery.rb
+++ lib/refinery.rb
@@ -1,7 +1,1 @@
-require 'pathname'
-
 require File.expand_path('../refinerycms', __FILE__)
-
-# Override Refinery.root
-# Must come after requiring 'refinerycms'
-Refinery.root = Pathname.new(File.expand_path('../../', __FILE__))
