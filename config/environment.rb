b57249e93f6e790a385738881e7ac1de2e7df70e
Philip Arndt <parndt@gmail.com>
2011-05-22 20:01:53 +1200

    Refinery::RefineryApp -> ::RefineryApp

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-::Refinery::RefineryApp::Application.initialize!
+::::RefineryApp::Application.initialize!
