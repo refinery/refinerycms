58541c988be9db192c0bf4e6b3694f7ac9629668
Philip Arndt <parndt@gmail.com>
2011-05-22 20:03:16 +1200

    :::: -> ::

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-::::RefineryApp::Application.configure do
+::RefineryApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
