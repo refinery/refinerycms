57638566083d9116f36eb8e118f08ca1622e2b66
Rodrigo Dominguez <rorra@rorra.com.ar>
2011-05-23 07:10:42 -0300

    Due to last test, I figure out it's impossible to execute the 'show' action of the Admin::DialogsController with params[:id] being anything else than a String, so there is not reason to check if params[:id] supports (or not) the method downcase. Cleaning the method.

--- core/app/controllers/admin/dialogs_controller.rb
+++ core/app/controllers/admin/dialogs_controller.rb
@@ -2,20 +2,17 @@ module Admin
   class DialogsController < Admin::BaseController
 
     def show
-      if (@dialog_type = params[:id].try(:downcase))
-        url_params = params.reject {|key, value| key =~ /(action)|(controller)/}
+      @dialog_type = params[:id].downcase
 
-        @iframe_src = if @dialog_type == 'image'
-          insert_admin_images_path(url_params.merge(:id => nil, :modal => true))
-        elsif @dialog_type == 'link'
-          link_to_admin_pages_dialogs_path(url_params.merge(:id => nil))
-        end
+      url_params = params.reject {|key, value| key =~ /(action)|(controller)/}
 
-        render :layout => false
-
-      else
-        render :nothing => true
+      @iframe_src = if @dialog_type == 'image'
+        insert_admin_images_path(url_params.merge(:id => nil, :modal => true))
+      elsif @dialog_type == 'link'
+        link_to_admin_pages_dialogs_path(url_params.merge(:id => nil))
       end
+
+      render :layout => false
     end
 
     def from_dialog?
