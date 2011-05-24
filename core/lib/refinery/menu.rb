1367996e9ba05ff12b5374eb73043c7cdf882c54
Philip Arndt <parndt@gmail.com>
2011-05-24 20:11:31 +1200

    Deprecate anything that was called in a partial previously (incase you overrode it).

--- core/lib/refinery/menu.rb
+++ core/lib/refinery/menu.rb
@@ -34,5 +34,22 @@ module Refinery
     end
 
     delegate *(Array.instance_methods - Object.instance_methods), :to => :items
+
+    # Ensure that things still work from how the menu partials worked before.
+    def live
+      warn "\n-- DEPRECATION WARNING --"
+      warn "The use of 'live' on a ::Refinery::Menu object is deprecated and will be removed at version 1.1 with no replacement."
+      warn "Called from: #{caller.detect{|c| c =~ %r{#{Rails.root.to_s}}}.inspect.to_s.split(':in').first}\n"
+
+      self
+    end
+
+    def in_menu
+      warn "\n-- DEPRECATION WARNING --"
+      warn "The use of 'in_menu' on a ::Refinery::Menu object is deprecated and will be removed at version 1.1 with no replacement."
+      warn "Called from: #{caller.detect{|c| c =~ %r{#{Rails.root.to_s}}}.inspect.to_s.split(':in').first}\n"
+
+      self
+    end
   end
 end
