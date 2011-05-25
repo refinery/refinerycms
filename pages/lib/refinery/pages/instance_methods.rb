8d2e9c7a167f93604951da5611e26b6c4c494bf6
Philip Arndt <parndt@gmail.com>
2011-05-25 16:50:53 +1200

    Seems like it would be a great idea to order the menu by lft ASC.

--- pages/lib/refinery/pages/instance_methods.rb
+++ pages/lib/refinery/pages/instance_methods.rb
@@ -17,7 +17,7 @@ module Refinery
       def find_pages_for_menu
         # First, apply a filter to determine which pages to show.
         # We need to join to the page's slug to avoid multiple queries.
-        pages = ::Page.live.in_menu.includes(:slug)
+        pages = ::Page.live.in_menu.includes(:slug).order('lft ASC')
 
         # Now we only want to select particular columns to avoid any further queries.
         # Title is retrieved in the next block below so it's not here.
