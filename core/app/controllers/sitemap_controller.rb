84b98262b1cc1bec6f2e1308b4b45a9e46b4d230
Philip Arndt <parndt@gmail.com>
2011-05-29 16:22:57 +1200

    Fix some specs with proper placement of files, yo.

--- core/app/controllers/sitemap_controller.rb
+++ core/app/controllers/sitemap_controller.rb
@@ -1,18 +1,20 @@
-class SitemapController < ::Refinery::FastController
-  layout nil
+module Refinery
+  class SitemapController < ::Refinery::FastController
+    layout nil
 
-  def index
-    headers['Content-Type'] = 'application/xml'
+    def index
+      headers['Content-Type'] = 'application/xml'
 
-    respond_to do |format|
-      format.xml do
-        @locales = if ::Refinery.i18n_enabled?
-                     ::Refinery::I18n.frontend_locales
-                   else
-                     [::I18n.locale]
-                   end
+      respond_to do |format|
+        format.xml do
+          @locales = if ::Refinery.i18n_enabled?
+                       ::Refinery::I18n.frontend_locales
+                     else
+                       [::I18n.locale]
+                     end
+        end
       end
     end
-  end
 
+  end
 end
---
+++ core/spec/controllers/refinery/sitemap_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+module Refinery
+  describe SitemapController do
+    login_refinery_user
+
+    before (:each) do
+      @request.env['HTTP_ACCEPT'] = 'application/xml'
+    end
+
+    it "should show a valid xml answer with i18n enabled" do
+      ::Refinery.should_receive(:i18n_enabled?).and_return(true)
+
+      get :index
+
+      response.should be_success
+    end
+
+    it "should show a valid xml answer with i18n disabled" do
+      ::Refinery.should_receive(:i18n_enabled?).and_return(false)
+
+      get :index
+
+      response.should be_success
+    end
+
+  end
+end
