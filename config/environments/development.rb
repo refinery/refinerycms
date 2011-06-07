6ad97719ddc0ecb29774feeab87f71f3e33ac37d
Philip Arndt <parndt@gmail.com>
2011-06-07 13:42:45 +1200

    Reduced width of guide and removed suggestion about config/environment.rb and replaced it with config/environments/production.rb.
    Also fixed a few line spacing issues on environment files.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,6 +25,7 @@ RefineryApp::Application.configure do
 end
 
 Refinery.rescue_not_found = false
+
 # When true will use Amazon's Simple Storage Service on your production machine
 # instead of the default file system for resources and images
 Refinery.s3_backend = !(ENV['S3_KEY'].nil? || ENV['S3_SECRET'].nil?)
