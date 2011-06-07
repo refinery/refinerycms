85fc39f475858c35e52c40aa5d02aa5978ed68e9
Vaughn Draughon <vaughn@rocksolidwebdesign.com>
2011-06-07 11:06:17 -0400

    Forgot one mod to remove the seeds, here it is

--- pages/db/migrate/20110606151235_add_template_columns.rb
+++ pages/db/migrate/20110606151235_add_template_columns.rb
@@ -2,9 +2,6 @@ class CreateAddTemplateColumns < ActiveRecord::Migration
   def self.up
     add_column ::Refinery::Page.table_name, :view_template, :string
     add_column ::Refinery::Page.table_name, :layout_template, :string
-
-    load(Rails.root.join('db', 'seeds', 'page_layout_templates.rb'))
-    load(Rails.root.join('db', 'seeds', 'page_view_templates.rb'))
   end
 
   def self.down
