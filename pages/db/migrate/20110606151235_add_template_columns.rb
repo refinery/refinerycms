ba4a5ed655435d2ae086f6ef52a6f1f4fa42a73e
Vaughn Draughon <vaughn@rocksolidwebdesign.com>
2011-06-07 02:20:39 -0400

    Changed hardcoded pages' table-name symbol to dynamic table name

--- pages/db/migrate/20110606151235_add_template_columns.rb
+++ pages/db/migrate/20110606151235_add_template_columns.rb
@@ -1,14 +1,14 @@
 class CreateAddTemplateColumns < ActiveRecord::Migration
   def self.up
-    add_column :pages, :view_template, :string
-    add_column :pages, :layout_template, :string
+    add_column ::Refinery::Page.table_name, :view_template, :string
+    add_column ::Refinery::Page.table_name, :layout_template, :string
 
     load(Rails.root.join('db', 'seeds', 'page_layout_templates.rb'))
     load(Rails.root.join('db', 'seeds', 'page_view_templates.rb'))
   end
 
   def self.down
-    remove_column :pages, :layout_template
-    remove_column :pages, :view_template
+    remove_column ::Refinery::Page.table_name, :layout_template
+    remove_column ::Refinery::Page.table_name, :view_template
   end
 end
