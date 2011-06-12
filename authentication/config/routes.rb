ed6293ee75be04b1cd272292a80bfb7faeb59080
Shevaun Ryan <shevaun.ryan@abletech.co.nz>
2011-06-13 01:18:59 +1000

    use nested scopes in routes

--- authentication/config/routes.rb
+++ authentication/config/routes.rb
@@ -12,22 +12,23 @@
       :sign_in => 'login',
       :sign_up => 'register'
     }
+
+    # Override Devise's default after login redirection route.
+    # This will push a logged in user to the dashboard.
+    scope(:module => 'admin') do
+      get 'refinery', :to => 'dashboard#index', :as => :refinery_root
+      get 'refinery', :to => 'dashboard#index', :as => :user_root
+
+      scope(:path => 'refinery', :as => 'refinery_admin') do
+        resources :users, :except => :show
+      end
+    end
   end
 
-  # Override Devise's default after login redirection route.
-  # This will push a logged in user to the dashboard.
-  scope(:module => 'refinery/admin') do
-    get 'refinery', :to => 'dashboard#index', :as => :refinery_root
-    get 'refinery', :to => 'dashboard#index', :as => :user_root
-  end
   # Override Devise's other routes for convenience methods.
   #get 'refinery/login', :to => "sessions#new", :as => :new_user_session
   #get 'refinery/login', :to => "sessions#new", :as => :refinery_login
   #get 'refinery/logout', :to => "sessions#destroy", :as => :destroy_user_session
   #get 'refinery/logout', :to => "sessions#destroy", :as => :logout
-
-  scope(:path => 'refinery', :as => 'refinery_admin', :module => 'refinery/admin') do
-    resources :users, :except => :show
-  end
 
 end
