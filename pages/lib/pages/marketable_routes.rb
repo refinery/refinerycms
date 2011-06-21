38ac9fd1191b1eaec67c616971a0e07c0cac78ee
Kyle Wilkinson <kai@wikyd.org>
2011-06-20 21:35:08 -0700

    Make the catch-all pages route for marketable URLs be controlled by the configuration switch.

--- pages/lib/pages/marketable_routes.rb
+++ pages/lib/pages/marketable_routes.rb
@@ -1,9 +1,10 @@
-::Refinery::Application.routes.draw do
-  match '*path' => 'pages#show'
-end
 
-# Add any parts of routes as reserved words.
 if Page.use_marketable_urls?
+  ::Refinery::Application.routes.draw do
+    match '*path' => 'pages#show'
+  end
+
+  # Add any parts of routes as reserved words.
   route_paths = ::Refinery::Application.routes.named_routes.routes.map{|name, route| route.path}
   Page.friendly_id_config.reserved_words |= route_paths.map { |path|
     path.to_s.gsub(/^\//, '').to_s.split('(').first.to_s.split(':').first.to_s.split('/')
