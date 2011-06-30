62f4eff93c149aa0c60d29bc3e1ac668749f471e
Philip Arndt <parndt@gmail.com>
2011-06-30 17:14:15 +1200

    This works the same without the ternary.

--- core/config/initializers/kaminari_monkeypatches.rb
+++ core/config/initializers/kaminari_monkeypatches.rb
@@ -3,7 +3,7 @@ module Kaminari
     class Tag
       # Handle isolate_namespace properly
       def page_url_for(page)
-        @template.main_app.url_for @params.merge(@param_name => (page <= 1 ? nil : page))
+        @template.main_app.url_for @params.merge(@param_name => (page unless page <= 1))
       end
     end
   end
