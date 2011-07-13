ae463e1da944e352b57a0144410c69052cd69f1c
Philip Arndt <parndt@gmail.com>
2011-07-14 00:22:25 +1200

    Transfer marketable routes code to the initializer that's using it. It's only used in this one place anyway and this allows us to use app.

--- pages/lib/refinerycms-pages.rb
+++ pages/lib/refinerycms-pages.rb
@@ -49,9 +49,22 @@ module Refinery
         end
       end
 
-      initializer 'add marketable routes' do |app|
-        require File.expand_path('../pages/marketable_routes.rb', __FILE__)
-        app.routes_reloader.paths << File.expand_path('../pages/marketable_routes.rb', __FILE__)
+      initializer 'add marketable routes', :after => :set_routes_reloader do |app|
+        if ::Refinery::Page.use_marketable_urls?
+          app.routes.append do
+            scope(:module => 'refinery') do
+              get '*path' => 'pages#show'
+            end
+          end
+
+          app.config.after_initialize do
+            # Add any parts of routes as reserved words.
+            route_paths = ::Refinery::Application.routes.named_routes.routes.map{|name, route| route.path}
+            ::Refinery::Page.friendly_id_config.reserved_words |= route_paths.map { |path|
+              path.to_s.gsub(/^\//, '').to_s.split('(').first.to_s.split(':').first.to_s.split('/')
+            }.flatten.reject{|w| w =~ /\_/}.uniq
+          end
+        end
       end
 
     end
