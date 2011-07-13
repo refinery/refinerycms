11cb1a5bf5b97a6744e350b6e4ab84516a858a10
Philip Arndt <parndt@gmail.com>
2011-07-14 01:23:59 +1200

    Whitespace fix

--- authentication/spec/requests/sign_up.rb
+++ authentication/spec/requests/sign_up.rb
@@ -2,24 +2,24 @@ require 'spec_helper'
 
 module Refinery
   module Users
-    
+
     describe 'user sign up' do
       before(:each) do
         User.delete_all
       end
-      
+
       describe 'when there are no users' do
         it 'invites us to create a user' do
           visit root_url
           has_content?("There are no users yet, so we'll set you up first").should be_true
         end
-        
+
         it 'allows user creation' do
           # Verify that we can access the sign up page.
           visit root_url
           click_link "Continue..."
           has_content?("Fill out your details below so that we can get you started.").should be_true
-          
+
           # Fill in user details.
           fill_in 'Username', :with => 'rspec'
           fill_in 'Email', :with => 'rspec@example.com'
