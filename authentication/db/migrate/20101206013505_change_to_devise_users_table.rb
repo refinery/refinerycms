4b927b11f9f16279e5a994decebc71435e3ea191
Philip Arndt <parndt@gmail.com>
2011-07-14 19:00:16 +1200

    Fixed more migrations that were out of sync

--- authentication/db/migrate/20101206013505_change_to_devise_users_table.rb
+++ authentication/db/migrate/20101206013505_change_to_devise_users_table.rb
@@ -1,27 +1,31 @@
 class ChangeToDeviseUsersTable < ActiveRecord::Migration
   def self.up
-    add_column ::User.table_name, :current_sign_in_at, :datetime
-    add_column ::User.table_name, :last_sign_in_at, :datetime
-    add_column ::User.table_name, :current_sign_in_ip, :string
-    add_column ::User.table_name, :last_sign_in_ip, :string
-    add_column ::User.table_name, :sign_in_count, :integer
-    add_column ::User.table_name, :remember_token, :string
-    add_column ::User.table_name, :reset_password_token, :string
+    add_column ::Refinery::User.table_name, :current_sign_in_at, :datetime
+    add_column ::Refinery::User.table_name, :last_sign_in_at, :datetime
+    add_column ::Refinery::User.table_name, :current_sign_in_ip, :string
+    add_column ::Refinery::User.table_name, :last_sign_in_ip, :string
+    add_column ::Refinery::User.table_name, :sign_in_count, :integer
+    add_column ::Refinery::User.table_name, :remember_token, :string
+    add_column ::Refinery::User.table_name, :reset_password_token, :string
 
-    rename_column ::User.table_name, :crypted_password, :encrypted_password
-    rename_column ::User.table_name, :login, :username
+    rename_column ::Refinery::User.table_name, :crypted_password, :encrypted_password
+    rename_column ::Refinery::User.table_name, :login, :username
+
+    ::Refinery::User.reset_column_information
   end
 
   def self.down
-    remove_column ::User.table_name, :current_sign_in_at
-    remove_column ::User.table_name, :last_sign_in_at
-    remove_column ::User.table_name, :current_sign_in_ip
-    remove_column ::User.table_name, :last_sign_in_ip
-    remove_column ::User.table_name, :sign_in_count
-    remove_column ::User.table_name, :remember_token
-    remove_column ::User.table_name, :reset_password_token
+    remove_column ::Refinery::User.table_name, :current_sign_in_at
+    remove_column ::Refinery::User.table_name, :last_sign_in_at
+    remove_column ::Refinery::User.table_name, :current_sign_in_ip
+    remove_column ::Refinery::User.table_name, :last_sign_in_ip
+    remove_column ::Refinery::User.table_name, :sign_in_count
+    remove_column ::Refinery::User.table_name, :remember_token
+    remove_column ::Refinery::User.table_name, :reset_password_token
 
-    rename_column ::User.table_name, :encrypted_password, :crypted_password
-    rename_column ::User.table_name, :username, :login
+    rename_column ::Refinery::User.table_name, :encrypted_password, :crypted_password
+    rename_column ::Refinery::User.table_name, :username, :login
+
+    ::Refinery::User.reset_column_information
   end
 end
