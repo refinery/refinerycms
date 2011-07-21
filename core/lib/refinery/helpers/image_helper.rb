da313821c2da94b067240be4c2ea83fbb08f6720
Joe Sak <joe@joesak.com>
2011-07-21 13:26:03 -0500

    Image fu: get the height and width in any case

--- core/lib/refinery/helpers/image_helper.rb
+++ core/lib/refinery/helpers/image_helper.rb
@@ -25,21 +25,11 @@ module Refinery
       # <%= image_fu @model.image, '200x200' %> or with no thumbnail: <%= image_fu @model.image %>
       def image_fu(image, geometry = nil, options={})
         if image.present?
-          # call rails' image tag function with default alt tag.
-          # if any other options were supplied these are merged in and can replace the defaults.
-          # if the geomtry is nil, then we know the image height and width already.
-          # detect nil geometry or cropping presence which is where we can guess the dimensions
-          unless geometry.nil? or !(split_geometry = geometry.to_s.split('#')).many? or !(split_geometry = split_geometry.first.split('x')).many?
-            image_width, image_height = split_geometry
-          else
-            image_width = nil
-            image_height = nil
-          end
-
-          image_tag(image.thumbnail(geometry).url, {
+          generated_image = image.thumbnail(geometry)
+          image_tag(generated_image.url, {
             :alt => image.respond_to?(:title) ? image.title : image.image_name,
-            :width => image_width,
-            :height => image_height
+            :width => generated_image.width,
+            :height => generated_image.height
           }.merge(options))
         end
       end
