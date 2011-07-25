049a1502eeaaefbc177ccb7a9084add8d0c1286e
Uģis Ozols <ugis.ozolss@gmail.com>
2011-07-25 13:55:04 +0300

    More request specs for authentication.

--- authentication/spec/requests/lost_password_spec.rb
+++ authentication/spec/requests/lost_password_spec.rb
@@ -1,10 +1,10 @@
 require "spec_helper"
 
-require File.expand_path("../../../features/support/factories", __FILE__)
+#require File.expand_path("../../../features/support/factories", __FILE__)
 
 module Refinery
   module Users
-    describe "password recovery form" do
+    describe "password recovery" do
       let!(:user) { Factory(:refinery_user, :email => "refinery@refinerycms.com") }
 
       it "asks user to specify email address" do
@@ -29,6 +29,31 @@ module Refinery
           click_button "Reset password"
           has_content?("Sorry, 'none@refinerycms.com' isn't associated with any accounts.").should be_true
           has_content?("Are you sure you typed the correct email address?").should be_true
+        end
+      end
+
+      context "when good reset code" do
+        before do
+          user.reset_password_token = "refinerycms"
+          user.save
+        end
+
+        it "allows to change password" do
+          visit edit_refinery_user_password_url(:reset_password_token => user.reset_password_token)
+          page.should have_content("Pick a new password for #{user.email}")
+
+          fill_in "refinery_user_password", :with => "123456"
+          fill_in "refinery_user_password_confirmation", :with => "123456"
+          click_button "Reset password"
+
+          page.should have_content("Password reset successfully for '#{user.email}'")
+        end
+      end
+
+      context "when invalid reset code" do
+        it "shows error message" do
+          visit edit_refinery_user_password_url(:reset_password_token => "hmmm")
+          page.should have_content("We're sorry, but this reset code has expired or is invalid.")
         end
       end
     end
