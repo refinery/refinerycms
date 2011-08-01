1f2c3a64c9f3577456f891df170a90710d4ccc29
Uģis Ozols <ugis.ozolss@gmail.com>
2011-08-01 14:34:06 +0300

    Namespace.

--- authentication/spec/support/refinery/rspec_configuration.rb
+++ authentication/spec/support/refinery/rspec_configuration.rb
@@ -1,5 +1,5 @@
 ::RSpec.configure do |config|
   config.include ::Devise::TestHelpers, :type => :controller
-  config.extend Refinery::ControllerMacros, :type => :controller
-  config.extend Refinery::RequestMacros, :type => :request
+  config.extend Refinery::Authentication::ControllerMacros, :type => :controller
+  config.extend Refinery::Authentication::RequestMacros, :type => :request
 end
