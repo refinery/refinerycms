078ca1a0bce54f251a1dbcdacb44a96d87867bc8
Uģis Ozols <ugis.ozolss@gmail.com>
2011-08-02 15:18:08 +0300

    Fix site bar specs.

--- core/spec/requests/site_bar_spec.rb
+++ core/spec/requests/site_bar_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe "site bar" do
   login_refinery_user
-
+  
   it "have logout link" do
     page.should have_content("Log out")
     page.should have_selector("a[href='/refinery/users/logout']")
@@ -10,7 +10,7 @@ describe "site bar" do
 
   context "when in backend" do
     before(:each) do
-      visit refinery_admin_root_path
+      visit refinery_admin_dashboard_path
     end
 
     it "have a 'switch to your website button'" do
@@ -19,7 +19,7 @@ describe "site bar" do
     end
 
     it "switches to frontend" do
-      page.current_path.should == refinery_admin_root_path
+      page.current_path.should == refinery_admin_dashboard_path
       click_link "Switch to your website"
       page.current_path.should == root_path
     end
@@ -27,6 +27,9 @@ describe "site bar" do
 
   context "when in frontend" do
     before(:each) do
+      # make a page in order to avoid 404
+      Factory(:page, :link_url => "/")
+      
       visit root_path
     end
 
