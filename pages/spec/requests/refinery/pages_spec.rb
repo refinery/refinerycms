6c1666ddd4af7ac6d0d4cc02d03c48eaf0f1f29f
Uģis Ozols <ugis.ozolss@gmail.com>
2011-08-02 15:23:11 +0300

    Use page factory.

--- pages/spec/requests/refinery/pages_spec.rb
+++ pages/spec/requests/refinery/pages_spec.rb
@@ -3,16 +3,13 @@ require 'spec_helper'
 describe 'page frontend' do
 
   before(:all) do
-    # Create a refinery user unless we have one.
-    Factory.create :refinery_user if ::Refinery::Role[:refinery].users.empty?
-
     # Delete all pages and their slugs.
     ::Refinery::Page.all.each(&:destroy!)
 
     # Create some pages for these specs
-    ::Refinery::Page.create(:title => 'Home', :link_url => '/')
-    ::Refinery::Page.create(:title => 'About')
-    ::Refinery::Page.create(:title => 'Draft', :draft => true)
+    Factory(:page, :title => 'Home', :link_url => '/')
+    Factory(:page, :title => 'About')
+    Factory(:page, :title => 'Draft', :draft => true)
   end
 
   describe 'when marketable urls are' do
