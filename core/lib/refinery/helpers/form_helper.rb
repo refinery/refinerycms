c5f70ed033d56c133cea9797d0093126cb6a4b02
Philip Arndt <parndt@gmail.com>
2011-08-05 10:29:11 +1200

    Revert "Refactor require_label form helper in order to use it with namespaced classes." We can't change APIs in 1.0.x
    This reverts commit 8c9b76a145f83820b082e3e5c587dc1ef40b3b12.

--- core/lib/refinery/helpers/form_helper.rb
+++ core/lib/refinery/helpers/form_helper.rb
@@ -3,29 +3,29 @@ require 'action_view/helpers'
 
 ActionView::Helpers::FormHelper.module_eval do
 
-  def required_label(object_name, method, options = {})
+  def required_label(object_name, method, text = nil, options = {})
     options = {:class => "required"}.merge!(options)
 
-    label(object_name, method, "#{label_humanize_text(method, options)} *", options)
+    label(object_name, method, "#{label_humanize_text(object_name, method, text)} *", options)
   end
 
 
-  def label_humanize_text method, options = {}
-    object = options[:object]
-
-    content ||= if object && object.class.respond_to?(:human_attribute_name)
-      object.class.human_attribute_name(method)
+  def label_humanize_text object_name, method, text = nil
+    content = text unless text.is_a?(Hash)
+    if content.blank?
+      content = object_name.classify.constantize.respond_to?(:human_attribute_name) ? object_name.classify.constantize.human_attribute_name(method) : method.to_s
+    else
+      content = content.to_s
     end
-
-    content ||= method.humanize
+    content.humanize
   end
 
 end
 
 ActionView::Helpers::FormBuilder.module_eval do
 
-  def required_label(method, options = {})
-    @template.required_label(@object_name, method, objectify_options(options))
+  def required_label(method, text = nil, options = {})
+    @template.required_label(@object_name, method, text, objectify_options(options))
   end
 
 end
@@ -39,4 +39,28 @@ ActionView::Helpers::FormTagHelper.module_eval do
     label_tag(name, text, options)
   end
 
+end
+
+# I18n labels automatically
+module ActionView
+  module Helpers
+    class InstanceTag
+      def to_label_tag(text = nil, options = {})
+        options = options.stringify_keys
+        name_and_id = options.dup
+        add_default_name_and_id(name_and_id)
+        options.delete("index")
+        options["for"] ||= name_and_id["id"]
+        if text.blank?
+          content = method_name.humanize
+          if object.class.respond_to?(:human_attribute_name)
+            content = object.class.human_attribute_name(method_name)
+          end
+        else
+          content = text.to_s
+        end
+        label_tag(name_and_id["id"], content, options)
+      end
+    end
+  end
 end
