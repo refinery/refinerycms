87d08c2a86b2e220a11294836924dca1dda88207
Uģis Ozols <ugis.ozolss@gmail.com>
2011-08-10 11:02:16 +0300

    Add image request specs.

---
+++ images/spec/requests/manage_images_spec.rb
@@ -0,0 +1,82 @@
+require "spec_helper"
+
+describe "manage images" do
+  login_refinery_user
+
+  context "when no images" do
+    it "invites to add one" do
+      visit refinery_admin_images_path
+      page.should have_content("There are no images yet. Click \"Add new image\" to add your first image.")
+    end
+  end
+
+  it "shows add new image link" do
+    visit refinery_admin_images_path
+    page.should have_content("Add new image")
+    page.should have_selector("a[href*='/refinery/images/new']")
+  end
+
+  context "new/create" do
+    it "uploads image", :js => true do
+      visit refinery_admin_images_path
+      click_link "Add new image"
+
+      within_frame "dialog_iframe" do
+        attach_file "image_image", File.expand_path("../../uploads/beach.jpeg", __FILE__)
+        click_button "Save"
+      end
+
+      page.should have_content("'Beach' was successfully added.")
+      Refinery::Image.count.should == 1
+    end
+  end
+
+  context "edit/update" do
+    let!(:image) { Factory(:image) }
+
+    it "updates image" do
+      visit refinery_admin_images_path
+      page.should have_selector("a[href='/refinery/images/#{image.id}/edit']")
+
+      click_link "Edit this image"
+
+      page.should have_content("Use current image or replace it with this one...")
+      page.should have_selector("a[href='/refinery/images']")
+
+      attach_file "image_image", File.expand_path("../../uploads/id-rather-be-here.jpg", __FILE__)
+      click_button "Save"
+
+      page.should have_content("'Id Rather Be Here' was successfully updated.")
+      Refinery::Image.count.should == 1
+    end
+  end
+
+  context "destroy" do
+    let!(:image) { Factory(:image) }
+
+    it "removes image" do
+      visit refinery_admin_images_path
+      page.should have_selector("a[href='/refinery/images/#{image.id}']")
+
+      click_link "Remove this image forever"
+
+      page.should have_content("'Beach' was successfully removed.")
+      Refinery::Image.count.should == 0
+    end
+  end
+
+  describe "switch view" do
+    let!(:image) { Factory(:image) }
+
+    it "shows images in grid" do
+      visit refinery_admin_images_path
+      page.should have_content("Switch to list view")
+      page.should have_selector("a[href='/refinery/images?view=list']")
+
+      click_link "Switch to list view"
+
+      page.should have_content("Switch to grid view")
+      page.should have_selector("a[href='/refinery/images?view=grid']")
+    end
+  end
+end
--- images/spec/support/refinery/factories.rb
+++ images/spec/support/refinery/factories.rb
@@ -1,3 +1,3 @@
-Factory.define :image do |i|
+Factory.define :image, :class => Refinery::Image do |i|
   i.image File.new(File.expand_path('../../../uploads/beach.jpeg', __FILE__))
 end
