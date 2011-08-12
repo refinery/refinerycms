1e60e73f7a7ad7030bdaed56e56dfd6ccb814698
Uģis Ozols <ugis.ozolss@gmail.com>
2011-08-12 11:16:20 +0300

    Don't need to assign factory to instance variable.

--- testing/lib/refinery/testing/request_macros/authentication.rb
+++ testing/lib/refinery/testing/request_macros/authentication.rb
@@ -4,7 +4,7 @@ module Refinery
       module Authentication
         def login_refinery_user
           before(:each) do
-            @refinery_user = Factory(:refinery_user, :username => "refinerycms",
+            Factory(:refinery_user, :username => "refinerycms",
                                     :password => "123456",
                                     :password_confirmation => "123456",
                                     :email => "refinerycms@refinerycms.com")
