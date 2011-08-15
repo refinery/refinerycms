b87218477e0381c71bc19c702ab89d477022714a
Jamie Winsor <jamie@enmasse.com>
2011-08-14 19:34:06 -0700

    Ruby 187 syntax fixes

--- testing/lib/generators/templates/spec/dummy/config/application.rb
+++ testing/lib/generators/templates/spec/dummy/config/application.rb
@@ -3,7 +3,12 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 require 'bundler/setup'
-Bundler.require
+
+# If you have a Gemfile, require the default gems, the ones in the
+# current environment and also include :assets gems if in development
+# or test environments.
+Bundler.require *Rails.groups(:assets) if defined?(Bundler)
+
 require '<%= gem_name %>'
 
 module Dummy
