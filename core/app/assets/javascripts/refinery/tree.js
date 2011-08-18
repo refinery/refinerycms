41105d231deeebe364690ff35af9347f445387d6
Josh Nussbaum <joshnuss@gmail.com>
2011-08-18 13:37:44 -0400

    adds ability expand tree via ajax. it greatly reduces the database queries required when managing large page hierarchies

--- core/app/assets/javascripts/refinery/tree.js
+++ core/app/assets/javascripts/refinery/tree.js
@@ -1,8 +1,22 @@
 $(function() {
-  $('.tree .toggle').live('click', function(event) { 
-    event.preventDefault(); 
-    $(this)
-      .toggleClass('expanded')
-      .parents('div:first').next('.nested').slideToggle(); 
-  }).trigger('click');
+  $('.tree .toggle').live('click', function(e) { 
+    e.preventDefault(); 
+    var $icon = $(this);
+    var $li   = $icon.parents('li:first');
+    var $nested = $li.find('.nested');
+
+    if ($icon.hasClass('expanded')) {
+      $icon.removeClass('expanded');
+      $nested.slideUp(); 
+    }
+    else {
+      var contentUrl = $nested.attr('data-ajax-content');
+      
+      $nested.load(contentUrl, function() {
+        $nested.find('li:last').addClass('branch_end');
+        $icon.addClass('expanded');
+        $nested.slideDown(); 
+      });
+    }
+  });
 });
