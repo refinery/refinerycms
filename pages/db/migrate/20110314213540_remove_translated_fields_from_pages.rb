7db285a954055bd24bd7f8bd5b55ef18113bdd51
jimjimovich <jim@mendenhall.com>
2011-08-22 13:54:22 +0400

    Fixing discrepancies in migration files

--- pages/db/migrate/20110314213540_remove_translated_fields_from_pages.rb
+++ pages/db/migrate/20110314213540_remove_translated_fields_from_pages.rb
@@ -3,11 +3,15 @@ class RemoveTranslatedFieldsFromPages < ActiveRecord::Migration
     ::Refinery::Page.translated_attribute_names.map(&:to_sym).each do |column_name|
       remove_column ::Refinery::Page.table_name, column_name if ::Refinery::Page.column_names.map(&:to_sym).include?(column_name)
     end
+
+    ::Refinery::Page.reset_column_information
   end
 
   def self.down
     ::Refinery::Page.translated_attribute_names.map(&:to_sym).each do |column_name|
       add_column ::Refinery::Page.table_name, column_name, Page::Translation.columns.detect{|c| c.name.to_sym == column_name}.type
     end
+
+    ::Refinery::Page.reset_column_information
   end
 end
