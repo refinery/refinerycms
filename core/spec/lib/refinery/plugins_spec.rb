04ec4bf8709d30e33b5c4717bec757d4ca7f81cf
Philip Arndt <parndt@gmail.com>
2011-09-03 11:09:47 +1200

    Use subject.class in place of Plugins as Plugins is in the describe already. (ht: jswanner)

--- core/spec/lib/refinery/plugins_spec.rb
+++ core/spec/lib/refinery/plugins_spec.rb
@@ -27,65 +27,65 @@ module Refinery
 
     before(:each) do
       # First, deactivate all.
-      Plugins.set_active([])
+      subject.class.set_active([])
     end
 
     describe "self.activate" do
       it "should activate a plugin" do
-        Plugins.activate("my_plugin")
+        subject.class.activate("my_plugin")
 
-        Plugins.active.names.should include("my_plugin")
+        subject.class.active.names.should include("my_plugin")
       end
 
       it "should only active the same plugin once" do
-         Plugins.activate("my_other_plugin")
-         Plugins.activate("my_other_plugin")
+         subject.class.activate("my_other_plugin")
+         subject.class.activate("my_other_plugin")
 
-         Plugins.active.names.count("my_other_plugin").should == 1
+         subject.class.active.names.count("my_other_plugin").should == 1
       end
 
       it "should not deactivate the first plugin when another is activated" do
-        Plugins.activate("my_plugin")
-        Plugins.activate("my_other_plugin")
+        subject.class.activate("my_plugin")
+        subject.class.activate("my_other_plugin")
 
-        Plugins.active.names.should include("my_plugin")
-        Plugins.active.names.should include("my_other_plugin")
+        subject.class.active.names.should include("my_plugin")
+        subject.class.active.names.should include("my_other_plugin")
       end
     end
 
     describe "self.deactivate" do
       it "should deactivate a plugin" do
-        Plugins.activate("my_plugin")
-        Plugins.deactivate("my_plugin")
+        subject.class.activate("my_plugin")
+        subject.class.deactivate("my_plugin")
 
-        Plugins.active.count.should == 0
+        subject.class.active.count.should == 0
       end
     end
 
     describe "self.set_active" do
 
       it "should activate a single plugin" do
-        Plugins.set_active(%w(my_plugin))
+        subject.class.set_active(%w(my_plugin))
 
-        Plugins.active.names.should include("my_plugin")
+        subject.class.active.names.should include("my_plugin")
       end
 
       it "should activate a list of plugins" do
-        Plugins.set_active(%w(my_plugin my_other_plugin))
+        subject.class.set_active(%w(my_plugin my_other_plugin))
 
-        Plugins.active.names.should include("my_plugin")
-        Plugins.active.names.should include("my_plugin")
+        subject.class.active.names.should include("my_plugin")
+        subject.class.active.names.should include("my_plugin")
 
-        Plugins.active.count.should == 2
+        subject.class.active.count.should == 2
       end
 
       it "should deactivate the initial plugins when another set is set_active" do
-        Plugins.set_active(%w(my_plugin))
-        Plugins.set_active(%w(my_other_plugin))
+        subject.class.set_active(%w(my_plugin))
+        subject.class.set_active(%w(my_other_plugin))
 
-        Plugins.active.names.should_not include("my_plugin")
-        Plugins.active.names.should include("my_other_plugin")
-        Plugins.active.count.should == 1
+        subject.class.active.names.should_not include("my_plugin")
+        subject.class.active.names.should include("my_other_plugin")
+        subject.class.active.count.should == 1
       end
 
     end
