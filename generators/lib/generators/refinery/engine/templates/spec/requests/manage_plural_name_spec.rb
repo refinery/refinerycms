2c6284944ac61715d6b6340566f3ab54925acd2d
Uģis Ozols <ugis.ozolss@gmail.com>
2011-09-05 20:49:06 +0300

    Convert cucumber feature to rspec.

--- generators/lib/generators/refinery/engine/templates/spec/requests/manage_plural_name_spec.rb
+++ generators/lib/generators/refinery/engine/templates/spec/requests/manage_plural_name_spec.rb
@@ -15,5 +15,79 @@ describe "manage <%= plural_name %>" do
       page.should have_content("UniqueTitleTwo")
     end
   end
+
+  describe "create" do
+    before(:each) do
+      visit refinery_admin_<%= plural_name %>_path
+
+      click_link "Add New <%= singular_name.titleize %>"
+    end
+
+    context "valid data" do
+      it "should succeed" do
+        fill_in "<%= title.name.titleize %>", :with => "This is a test of the first string field"
+        click_button "Save"
+
+        page.should have_content("'This is a test of the first string field' was successfully added.")
+        Refinery::<%= class_name %>.count.should == 1
+      end
+    end
+
+    context "invalid data" do
+      it "should fail" do
+        click_button "Save"
+
+        page.should have_content("<%= title.name.titleize %> can't be blank")
+        Refinery::<%= class_name %>.count.should == 0
+      end
+    end
+
+    context "duplicate" do
+      before(:each) { Factory(:<%= singular_name %>, :<%= title.name %> => "UniqueTitle") }
+
+      it "should fail" do
+        visit refinery_admin_<%= plural_name %>_path
+
+        click_link "Add New <%= singular_name.titleize %>"
+
+        fill_in "<%= title.name.titleize %>", :with => "UniqueTitle"
+        click_button "Save"
+
+        page.should have_content("There were problems")
+        Refinery::<%= class_name %>.count.should == 1
+      end
+    end
+  end
+
+  describe "edit" do
+    before(:each) { Factory(:<%= singular_name %>, :<%= title.name %> => "A <%= title.name %>") }
+
+    it "should succeed" do
+      visit refinery_admin_<%= plural_name %>_path
+
+      within ".actions" do
+        click_link "Edit this <%= singular_name %>"
+      end
+
+      fill_in "<%= title.name.titleize %>", :with => "A different <%= title.name %>"
+      click_button "Save"
+
+      page.should have_content("'A different <%= title.name %>' was successfully updated.")
+      page.should have_no_content("A <%= title.name %>")
+    end
+  end
+
+  describe "destroy" do
+    before(:each) { Factory(:<%= singular_name %>, :<%= title.name %> => "UniqueTitleOne") }
+
+    it "should succeed" do
+      visit refinery_admin_<%= plural_name %>_path
+
+      click_link "Remove this <%= singular_name.titleize.downcase %> forever"
+
+      page.should have_content("'UniqueTitleOne' was successfully removed.")
+      Refinery::<%= class_name %>.count.should == 0
+    end
+  end
 <% end %>
 end
