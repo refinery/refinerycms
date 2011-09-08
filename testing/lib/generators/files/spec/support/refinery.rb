e1a1e0ac7d77dc4891d8e2339658ae8be0464d24
Jamie Winsor <jamie@enmasse.com>
2011-09-07 17:08:39 -0700

    Add Option reset before and after rspec filters to generated refinerycms-testing refinery support file

--- testing/lib/generators/files/spec/support/refinery.rb
+++ testing/lib/generators/files/spec/support/refinery.rb
@@ -3,4 +3,14 @@ require 'refinerycms-testing'
 RSpec.configure do |config|
   config.extend Refinery::ControllerMacros::Authentication, :type => :controller
   config.extend Refinery::RequestMacros::Authentication, :type => :request
+  
+  config.before(:each) do
+    Refinery::Images::Options.reset!
+    Refinery::Resources::Options.reset!
+  end
+  
+  config.after(:each) do
+    Refinery::Images::Options.reset!
+    Refinery::Resources::Options.reset!
+  end
 end
