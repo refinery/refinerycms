cee82b0f00b27a3ec6c783141f5c38302af7fb3f
Jamie Winsor <jamie@enmasse.com>
2011-09-08 16:54:25 -0400

    Replace generator cucumber tests with rspec tests

--- spec/lib/refinery/generators/engine/engine_generator_spec.rb
+++ spec/lib/refinery/generators/engine/engine_generator_spec.rb
@@ -13,13 +13,51 @@ module Refinery
     
     specify do
       destination_root.should have_structure {
-        directory "db" do
-          directory "migrate"
-        end
-        directory "config" do
-          directory "initializers" do
-            file "refinery_images.rb" do
-              contains "Refinery::Images::Options.configure"
+        directory "vendor" do
+          directory "engines" do
+            directory "rspec_product_tests" do
+              directory "app" do
+                directory "controllers" do
+                  directory "refinery" do
+                    directory "admin" do
+                      file "rspec_product_tests_controller.rb"
+                    end
+                    file "rspec_product_tests_controller.rb"
+                  end
+                end
+                directory "models" do
+                  directory "refinery" do
+                    file "rspec_product_test.rb"
+                  end
+                end
+                directory "views" do
+                  directory "refinery" do
+                    directory "admin" do
+                      directory "rspec_product_tests" do
+                        file "_form.html.erb"
+                        file "_sortable_list.html.erb"
+                        file "edit.html.erb"
+                        file "index.html.erb"
+                        file "new.html.erb"
+                        file "_rspec_product_test.html.erb"
+                      end
+                    end
+                    directory "rspec_product_tests" do
+                      file "index.html.erb"
+                      file "show.html.erb"
+                    end
+                  end
+                end
+              end
+              directory "lib" do
+                file "refinerycms-rspec_product_tests.rb"
+              end
+              directory "config" do
+                directory "locales" do
+                  file "en.yml"
+                end
+                file "routes.rb"
+              end
             end
           end
         end
