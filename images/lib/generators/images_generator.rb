0d4cad60e37ae5d74223ad17d22ac0004fad6289
Jamie Winsor <jamie@enmasse.com>
2011-09-07 18:10:00 -0700

    Fix incorrectly named function in images generator

--- images/lib/generators/images_generator.rb
+++ images/lib/generators/images_generator.rb
@@ -6,7 +6,7 @@ module Refinery
     source_root File.expand_path('../../../', __FILE__)
     engine_name "images"
 
-    def generate_resource_config_files
+    def generate_images_initializer
       template "lib/generators/templates/config/initializers/refinery_images.rb.erb", destination_path.join("config", "initializers", "refinery_images.rb")
     end
     
