4f712d89348b20e1c191e9dd8542389a78043252
Jamie Winsor <jamie@enmasse.com>
2011-09-19 18:22:41 -0700

    Add generator steps to generate decorator directories
    Improve test coverage for CmsGenerator

--- core/lib/generators/cms_generator.rb
+++ core/lib/generators/cms_generator.rb
@@ -14,7 +14,7 @@ module ::Refinery
       self.silence_puts = true
 
       # Only pretend to do the next actions if this is Refinery to stay DRY
-      if Rails.root == Refinery.root
+      if destination_path == Refinery.root.to_s
         say_status :'-- pretending to make changes that happen in an actual installation --', nil, :yellow
         old_pretend = self.options[:pretend]
         new_options = self.options.dup
@@ -25,7 +25,7 @@ module ::Refinery
       unless self.options[:update]
         # First, effectively move / rename files that get in the way of Refinery CMS
         %w(public/index.html app/views/layouts/application.html.erb).each do |roadblock|
-          if (roadblock_path = Rails.root.join(roadblock)).file?
+          if (roadblock_path = destination_path.join(roadblock)).file?
             create_file "#{roadblock}.backup",
                         :verbose => true do roadblock_path.read end
             remove_file roadblock_path, :verbose => true
@@ -35,9 +35,11 @@ module ::Refinery
 
       # Massage environment files
       %w(development test production).map{|e| "config/environments/#{e}.rb"}.each do |env|
+        next unless destination_path.join(env).file?
+        
         gsub_file env, "config.assets.compile = false", "config.assets.compile = true", :verbose => false
 
-        unless (env_file_contents = Rails.root.join(env).read) =~ %r{Refinery.rescue_not_found}
+        unless (env_file_contents = destination_path.join(env).read) =~ %r{Refinery.rescue_not_found}
           append_file env, "Refinery.rescue_not_found = #{env.split('/').last.split('.rb').first == 'production'}\n"
         end
 
@@ -50,7 +52,7 @@ module ::Refinery
       end
 
       # Stop pretending
-      if Rails.root == Refinery.root
+      if destination_path == Refinery.root.to_s
         say_status :'-- finished pretending --', nil, :yellow
         new_options = self.options.dup
         new_options[:pretend] = old_pretend
@@ -58,13 +60,13 @@ module ::Refinery
       end
 
       # Ensure .gitignore exists and append our rules to it.
-      create_file ".gitignore" unless Rails.root.join('.gitignore').file?
+      create_file ".gitignore" unless destination_path.join('.gitignore').file?
       our_ignore_rules = self.class.source_root.join('.gitignore').read
-      our_ignore_rules = our_ignore_rules.split('# REFINERY CMS DEVELOPMENT').first if Rails.root != Refinery.root
+      our_ignore_rules = our_ignore_rules.split('# REFINERY CMS DEVELOPMENT').first if destination_path != Refinery.root.to_s
       append_file ".gitignore", our_ignore_rules
 
       # If the admin/base_controller.rb file exists, ensure it does not do the old inheritance
-      if (admin_base = Rails.root.join('app', 'controllers', 'refinery', 'admin_controller.rb')).file?
+      if (admin_base = destination_path.join('app', 'controllers', 'refinery', 'admin_controller.rb')).file?
         gsub_file admin_base,
                   "# You can extend refinery backend with your own functions here and they will likely not get overriden in an update.",
                   "",
@@ -75,7 +77,7 @@ module ::Refinery
       end
 
       # Append seeds.
-      create_file "db/seeds.rb" unless Rails.root.join('db', 'seeds.rb').file?
+      create_file "db/seeds.rb" unless destination_path.join('db', 'seeds.rb').file?
       append_file 'db/seeds.rb', :verbose => true do
         self.class.source_root.join('db', 'seeds.rb').read
       end
@@ -97,7 +99,13 @@ module ::Refinery
       Pathname.glob(self.class.source_root.join('**', '*')).reject{|f|
         f.directory? or f.to_s =~ /\/db\//
       }.sort.each do |path|
-        copy_file path, path.to_s.gsub(self.class.source_root.to_s, Rails.root.to_s)
+        copy_file path, path.to_s.gsub(self.class.source_root.to_s, destination_path.to_s)
+      end
+      
+      # Create decorator directories
+      ['controllers', 'models'].each do |decorator_namespace|
+        src_file_path = "app/decorators/#{decorator_namespace}/refinery/.gitkeep"
+        copy_file self.class.source_root.join(src_file_path), destination_path.join(src_file_path)
       end
 
       # Ensure i18n exists and is up to date.
