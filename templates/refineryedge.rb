847a4884eb2eeb74e362b4a68d7dbfc451e1a259
Garrett Heinlen <heinleng@gmail.com>
2011-09-23 12:28:10 -0500

    Idea for implementation for templating to easily install exactly what is desied

---
+++ templates/refineryedge.rb
@@ -0,0 +1,25 @@
+# Ideal ->
+# rails new app_name -m http://refinerycms.com/latest.rb
+# Pratical -> 
+# rails new app_name -m https://raw.github.com/gist/1237830/cda5dfb7459092ee41f2f77816812d844f6e3005/gistfile1.rb
+gem 'refinerycms'
+run 'bundle install'
+
+generate 'refinery:cms' 
+rake 'db:migrate'
+
+remove_file 'public/index.html'
+remove_file 'rm public/images/rails.png'
+
+# Could easily add options for each engine during installation
+# if yes?("Do you want to use RefineryCMS - Testing?")
+#   gem 'refinerycms-testing', '~> 1.0.4'
+#   run 'refinery:testing'
+#   run 'rake'
+# end
+
+say <<-eos
+  ============================================================================
+          Your new RefineryCMS application is now running on edge.
+  ============================================================================
+eos
