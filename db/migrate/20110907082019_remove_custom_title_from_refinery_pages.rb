c46ff2ce6d898cc5f852c70db0311083e2a50afd
Uģis Ozols <ugis.ozolss@gmail.com>
2011-09-27 22:13:57 +0300

    Reset column information.

--- db/migrate/20110907082019_remove_custom_title_from_refinery_pages.rb
+++ db/migrate/20110907082019_remove_custom_title_from_refinery_pages.rb
@@ -2,10 +2,14 @@ class RemoveCustomTitleFromRefineryPages < ActiveRecord::Migration
   def up
     if ::Refinery::Page.column_names.map(&:to_sym).include?(:custom_title)
       remove_column ::Refinery::Page.table_name, :custom_title
+
+      ::Refinery::Page.reset_column_information
     end
   end
 
   def down
     add_column ::Refinery::Page.table_name, :custom_title, :string
+
+    ::Refinery::Page.reset_column_information
   end
 end
