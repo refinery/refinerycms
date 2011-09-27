8685abe5cd33b4a713c935c0168da0cc23451c16
Garrett Heinlen <heinleng@gmail.com>
2011-09-26 20:15:07 -0500

    removed unnecessary comments

--- templates/refineryedge.rb
+++ templates/refineryedge.rb
@@ -4,25 +4,16 @@ generate 'refinery:cms'
 rake 'db:migrate'
 
 append_file 'Gemfile' do 
-" #  RefineryCMS -- Testing ============================
-
-  #  group :development, :test do
+" #  group :development, :test do
   #    gem 'refinerycms-testing', '~> 2.0'
   #  end
 
   #   USER DEFINED
 
   # Specify additional Refinery CMS Engines here (all optional):
-  #  RefineryCMS - Blog
   #  gem 'refinerycms-blog', :git => 'git://github.com/resolve/refinerycms-blog.git', :branch => 'rails-3-1'
-  #
-  #  RefineryCMS - Inquiries
   #  gem 'refinerycms-inquiries', :git => 'git://github.com/resolve/refinerycms-inquiries.git', :branch => 'rails-3-1'
-
-  #  RefineryCMS - Search
   #  gem 'refinerycms-search', :git => 'git://github.com/resolve/refinerycms-search.git', :branch => 'rails-3-1'
-  
-  #  RefineryCMS - Page-Images
   #  gem 'refinerycms-page-images', :git => 'git://github.com/resolve/refinerycms-page-images.git', :branch => 'rails-3-1'
 
   #  END USESR DEFINED"
