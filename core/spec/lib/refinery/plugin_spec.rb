b375b60b6061a7b075d3f097d131cb2d409d41a1
Uģis Ozols <ugis.ozolss@gmail.com>
2011-09-29 10:22:42 +0300

    Fix backend menu item highlithing.

--- core/spec/lib/refinery/plugin_spec.rb
+++ core/spec/lib/refinery/plugin_spec.rb
@@ -113,6 +113,7 @@ module Refinery
 
     describe '#highlighted?' do
       it 'returns true if params[:controller] match menu_match regexp' do
+        plugin.highlighted?({:controller => 'refinery/admin/refinery_rspec'}).should be
         plugin.highlighted?({:controller => 'refinery/refinery_rspec'}).should be
       end
 
