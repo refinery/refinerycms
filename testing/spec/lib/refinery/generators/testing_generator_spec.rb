33c862befcd1643ac1605116a512ed6d8dffa087
Jamie Winsor <jamie@enmasse.com>
2011-10-10 19:05:57 -0700

    Fix failing build
    Add rake task to run specs

--- testing/spec/lib/refinery/generators/testing_generator_spec.rb
+++ testing/spec/lib/refinery/generators/testing_generator_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require "generator_spec/test_case"
 
 module Refinery
-  describe Generators::TestingGenerator do
+  describe TestingGenerator do
     include GeneratorSpec::TestCase
 
     destination File.expand_path("../../../../../tmp/vendor/engines/rspec_engine_tests", __FILE__)
@@ -11,7 +11,7 @@ module Refinery
       prepare_destination
       quietly do
         args = %w{ rspec_engine_test title:string description:text image:image brochure:resource }
-        Refinery::Generators::EngineGenerator.new(args, { :force => true },
+        Refinery::EngineGenerator.new(args, { :force => true },
           { :destination_root => File.expand_path("../../../../../tmp", __FILE__) }
         ).generate
       end
