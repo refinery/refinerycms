51b8865a3ea6cdc7f844257e6898810586bfbaf1
Jamie Winsor <jamie@enmasse.com>
2011-10-11 17:25:42 -0700

    Refactor Settings Engine
    Move settings generator to uniform location
    Refactor settings engine into it's own file

--- settings/lib/refinery/generators/settings_generator.rb
+++ settings/lib/refinery/generators/settings_generator.rb
@@ -3,7 +3,7 @@ require 'refinery/generators'
 module ::Refinery
   class SettingsGenerator < ::Refinery::Generators::EngineInstaller
 
-    source_root File.expand_path('../../../', __FILE__)
+    source_root File.expand_path('../../../../', __FILE__)
     engine_name "settings"
 
   end
--- settings/lib/refinerycms-settings.rb
+++ settings/lib/refinerycms-settings.rb
@@ -1,30 +1,15 @@
 require 'refinerycms-base'
-require File.expand_path('../generators/settings_generator', __FILE__)
 
 module Refinery
   module Settings
-
+    require 'refinery/settings/engine' if defined?(Rails)
+    require 'refinery/generators/settings_generator'
+    
     class << self
       attr_accessor :root
       def root
         @root ||= Pathname.new(File.expand_path('../../', __FILE__))
       end
     end
-
-    class Engine < ::Rails::Engine
-      isolate_namespace ::Refinery
-
-      initializer "init plugin", :after => :set_routes_reloader do |app|
-        ::Refinery::Plugin.register do |plugin|
-          plugin.pathname = root
-          plugin.name = 'refinery_settings'
-          plugin.url = app.routes.url_helpers.refinery_admin_settings_path
-          plugin.version = %q{2.0.0}
-          plugin.menu_match = /refinery\/settings$/
-        end
-      end
-    end
   end
 end
-
-::Refinery.engines << 'settings'
