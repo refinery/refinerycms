7ed8f803025ac63f1e6b92fdd496aed8c6152fd9
Joe Sak <joe@joesak.com>
2011-10-22 12:51:59 -0500

    use railscasts way of allowing focus

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,7 +32,10 @@ def setup_environment
     config.mock_with :rspec
 
     config.fixture_path = ::Rails.root.join('spec', 'fixtures').to_s
+    
+    config.treat_symbols_as_metadata_keys_with_true_values = true
     config.filter_run :focus => true
+    config.run_all_when_everything_filtered = true
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, comment the following line or assign false
     # instead of true.
