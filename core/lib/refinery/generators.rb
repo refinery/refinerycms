b14250df47846699350d48f8551a8cbddb1150f2
Jamie Winsor <jamie@enmasse.com>
2011-10-25 15:36:24 -0700

    Do not autoload generators that we want made available to the end user

--- core/lib/refinery/generators.rb
+++ core/lib/refinery/generators.rb
@@ -1,11 +1,13 @@
+require 'rails/generators'
+
 module Refinery
   module Generators
     autoload :EngineInstaller, 'refinery/generators/engine_installer'
     autoload :Migrations, 'refinery/generators/migrations'
   end
 
-  autoload :EngineGenerator, 'refinery/generators/engine/engine_generator'
-  autoload :CmsGenerator, 'refinery/generators/cms/cms_generator'
-  autoload :CoreGenerator, 'refinery/generators/core/core_generator'
-  autoload :DummyGenerator, 'refinery/generators/dummy/dummy_generator'
+  require 'refinery/generators/engine/engine_generator'
+  require 'refinery/generators/cms/cms_generator'
+  require 'refinery/generators/core/core_generator'
+  require 'refinery/generators/dummy/dummy_generator'
 end
