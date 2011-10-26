45975b4cb93f3e106dc01a15aed2a22ff939e51e
Jamie Winsor <jamie@enmasse.com>
2011-10-26 16:05:11 -0700

    Remove Engine Installer migration
    This custom migration installer no longer needs to exist, with Rails 3.1 we have full support for copying migrations from engines to the main app

--- core/lib/refinery/core.rb
+++ core/lib/refinery/core.rb
@@ -24,7 +24,6 @@ module Refinery
   # These have to be specified after the autoload to correct load issues on some systems.
   # As per commit 12af0e3e83a147a87c97bf7b29f343254c5fcb3c
   require 'refinerycms-settings'
-  require 'refinery/generators'
 
   require 'refinery/ext/action_view/helpers/form_builder'
   require 'refinery/ext/action_view/helpers/form_helper'
--- core/lib/refinery/generators.rb
+++
@@ -1,8 +0,0 @@
-require 'rails/generators'
-
-module Refinery
-  module Generators
-    autoload :EngineInstaller, 'refinery/generators/engine_installer'
-    autoload :Migrations, 'refinery/generators/migrations'
-  end
-end
