c0861513882cb5bd6d118b307fab85ae95bdaeda
Philip Arndt <parndt@gmail.com>
2011-10-27 09:11:05 +1300

    Ensure that plugins aren't added to the list of registered plugins if they're invalid.
    Conflicts:
    
    	core/lib/refinery/plugin.rb

--- core/lib/refinery/plugin.rb
+++ core/lib/refinery/plugin.rb
@@ -27,6 +27,9 @@ module Refinery
         def self.called_from; "#{new_called_from}"; end
       RUBY
       Object.const_set(plugin.class_name.to_sym, klass)
+      
+      # add the new plugin to the collection of registered plugins
+      ::Refinery::Plugins.registered << plugin
     end
 
     # Returns the class name of the plugin
@@ -101,7 +104,6 @@ module Refinery
       # provide a default pathname to where this plugin is using its lib directory.
       depth = RUBY_VERSION >= "1.9.2" ? 4 : 3
       self.pathname ||= Pathname.new(caller(depth).first.match("(.*)#{File::SEPARATOR}lib")[1])
-      ::Refinery::Plugins.registered << self # add me to the collection of registered plugins
     end
   end
 end
