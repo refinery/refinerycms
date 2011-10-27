fdf7845205c39d64fb983562a1e90f9e5fb34f42
Jamie Winsor <jamie@enmasse.com>
2011-10-26 18:19:05 -0700

    Use rails way to load seed data from pages engine

--- pages/lib/generators/refinery/pages/pages_generator.rb
+++ pages/lib/generators/refinery/pages/pages_generator.rb
@@ -5,6 +5,17 @@ module Refinery
     def generate_pages_initializer
       template "config/initializers/refinery_pages.rb.erb", File.join(destination_root, "config", "initializers", "refinery_pages.rb")
     end
+    
+    def append_load_seed_data
+      create_file "db/seeds.rb" unless File.exists?(File.join(destination_root, 'db', 'seeds.rb'))
+      append_file 'db/seeds.rb', :verbose => true do
+        <<-EOH
+
+# Added by RefineryCMS Pages engine
+Refinery::Pages::Engine.load_seed
+        EOH
+      end
+    end
 
   end
 end
