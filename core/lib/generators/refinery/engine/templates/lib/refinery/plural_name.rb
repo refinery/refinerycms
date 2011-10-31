460e91d9f88b6510020b2ed980ad7967f4d8c6f8
Uģis Ozols <ugis.ozolss@gmail.com>
2011-10-31 22:18:14 +0200

    Specify factory_paths for generated engines.

--- core/lib/generators/refinery/engine/templates/lib/refinery/plural_name.rb
+++ core/lib/generators/refinery/engine/templates/lib/refinery/plural_name.rb
@@ -8,6 +8,10 @@ module Refinery
       def root
         @root ||= Pathname.new(File.expand_path('../../../', __FILE__))
       end
+
+      def factory_paths
+        @factory_paths ||= [ root.join("spec/factories").to_s ]
+      end
     end
   end
 end
