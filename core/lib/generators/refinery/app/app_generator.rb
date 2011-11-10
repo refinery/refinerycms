32d5f35dc7a660f85a61477e53c0b5ce1fe2a3de
Jamie Winsor <jamie@vialstudios.com>
2011-11-09 18:30:52 -0800

    Add generate rails section to app_generator

--- core/lib/generators/refinery/app/app_generator.rb
+++ core/lib/generators/refinery/app/app_generator.rb
@@ -1,9 +1,9 @@
 require 'thor'
 require 'thor/group'
+require 'rails/generators'
 
 module Refinery
-  class AppGenerator < Thor::Group
-    include Thor::Actions
+  class AppGenerator < Rails::Generators::Base
     
     argument :app_path
     desc File.read(File.expand_path('../USAGE', __FILE__))
@@ -56,10 +56,11 @@ module Refinery
       end
     end
     
-    def generate
-      validate_options!
+    def run!
+      @rails_version_to_use = options[:rails_version] || rails_version_in_path
       
-      # nothing yet
+      validate_options!
+      generate_rails!
     end
     
     protected
@@ -67,9 +68,8 @@ module Refinery
       def validate_options!
         app_name = app_path.split(File::SEPARATOR).last
         
-        rails_version_to_use = options[:rails_version] || rails_version_in_path
-        unless valid_rails_version?(rails_version_to_use)
-          puts "\nRails #{rails_version_to_use} is not supported by Refinery #{Refinery.version}, " \
+        unless valid_rails_version?(@rails_version_to_use)
+          puts "\nRails #{@rails_version_to_use} is not supported by Refinery #{Refinery.version}, " \
                "please use Rails #{RAILS_MINOR_VERSION}.x instead."
           puts "\nYou can tell Refinery CMS an installed and compatible rails version to use like so:\n"
           puts "\nrefinerycms #{app_name} --rails-version #{RAILS_MINOR_VERSION}"
@@ -117,6 +117,67 @@ module Refinery
         end
       end
       
+      def generate_rails!
+        # Generate a rails application
+        rails_command = "rails _#{@rails_version_to_use}_ new \"#{app_path}\""
+        rails_command << " --database #{options[:adapter]}"
+        rails_command << " --force" if options[:force]
+        rails_command << " --skip-test-unit"
+        rails_command << " --skip-bundle" # Rails automatically installs the bundle, but so do we!
+        rails_command << " -m http://jruby.org" if defined? JRUBY_VERSION
+        
+        rails_output = nil
+        inside(app_path) do
+          rails_output = run(rails_command, :verbose => false, :capture => true)
+        end
+        puts app_path
+        puts rails_output
+  
+        # Detect non-success or a blank rails output or starting with "Can't initialize" or "Error"
+        if !$?.success? or rails_output.to_s.length == 0 or rails_output =~ /^(Can't\ initialize|Error)/
+          puts "\nGenerating Rails application failed. Exiting..."
+          if run('gem list rails', :verbose => false, :capture => true) !~ %r{[( ]#{@rails_version_to_use}(.0)*[,\)]}
+            puts "\nDo you have Rails #{@rails_version_to_use} installed?"
+          end
+          puts "\n"
+          exit(1)
+        else
+          if defined? JRUBY_VERSION
+            find_and_replace(app_path.join('Gemfile'), /['|"]sqlite3['|"]/, "'activerecord-jdbcsqlite3-adapter'")
+          end
+
+          # Remove rails from the Gemfile so that Refinery can manage it
+          find_and_replace('Gemfile', %r{^gem 'rails'}, "# gem 'rails'")
+  
+          # Override database host
+          if options[:host] != 'localhost' && (adapter = options[:adapter]) != 'sqlite3'
+            adapter = 'mysql2' if adapter == 'mysql'
+            find_and_replace('config/database.yml', "\n  adapter: #{adapter}", "\n  adapter: #{adapter}\n  host: #{options[:host]}")
+          end
+  
+          # Override database username and password
+          if options[:ident]
+            find_and_replace('config/database.yml', %r{username:}, '#username:')
+            find_and_replace('config/database.yml', %r{password:}, '#password:')
+          else
+            find_and_replace('config/database.yml', %r{username:.*}, "username: #{options[:username]}")
+            find_and_replace('config/database.yml', %r{password:.*}, "password: \"#{options[:password]}\"")
+          end
+  
+          puts "\n---------"
+          puts "Refinery successfully installed in '#{@app_path}'!\n\n"
+        end
+      end
+      
+    private
+    
+      def find_and_replace(file, find, replace)
+        contents = File.read(File.join(@app_path, file)).gsub!(find, replace)
+        File.open(File.join(@app_path, file), "w") do |f|
+          f.puts contents
+        end
+      end
+      
       def valid_rails_version?(rails_version)
         !rails_version !~ %r{\b#{RAILS_MINOR_VERSION}}
       end
@@ -124,8 +185,6 @@ module Refinery
       def valid_app_name?(app_name)
         !self.class.reserved_app_names.include?(app_name.downcase)
       end
-      
-    private
       
       # Returns a string representation of the rails version of what is currently found in your path
       def rails_version_in_path
