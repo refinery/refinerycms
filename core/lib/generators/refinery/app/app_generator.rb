87ecd36731792b0be1a5c08866a8cddc15ae1521
Philip Arndt <parndt@gmail.com>
2011-11-16 16:41:25 +1300

    Added @app_pathname to handle the find_and_replace cases until they're optimised away.

--- core/lib/generators/refinery/app/app_generator.rb
+++ core/lib/generators/refinery/app/app_generator.rb
@@ -4,44 +4,44 @@ require 'rails/version'
 
 module Refinery
   class AppGenerator < Rails::Generators::AppBase
-          
+
     source_root Pathname.new(File.expand_path('../templates', __FILE__))
-      
+
     add_shared_options_for("RefineryCMS")
 
     class_option :force,          :type => :boolean, :aliases => ['-f'], :default => false,
                                   :desc => "Force overwriting of directory"
-                                  
+
     class_option :gems,           :type => :array, :aliases => ['-g'],
                                   :desc => "Additional gems to install"
-                                  
+
     class_option :heroku,         :type => :string, :default => nil,
                                   :desc => "Set up and deploy to Heroku"
-                                  
+
     class_option :confirm,        :type => :boolean, :aliases => ['-c'], :default => false,
                                   :desc => "Confirm any prompts that require input"
-                                  
+
     class_option :testing,        :type => :boolean, :aliases => ['-t'], :default => true,
                                   :desc => "Automatically set up the project with refinerycms-testing support"
-                                  
+
     class_option :trace,          :type => :boolean, :default => false,
                                   :desc => "Investigate any problems with the installer"
-                                  
+
     class_option :version,        :type => :boolean, :default => false,
                                   :desc => "Display the installed version of RefineryCMS"
 
     class_option :refinery_edge,  :type => :boolean, :default => false,
                                   :desc => "Setup your application's Gemfile to point to the RefineryCMS repository"
-                                  
+
     class_option :skip_db,        :type => :boolean, :default => false,
                                   :desc => "Skip database creation and migration tasks"
-    
+
     # Remove overridden or non-relevant class options
     remove_class_option(:skip_test_unit, :skip_bundle, :skip_gemfile, :skip_active_record, :skip_sprockets)
-    
+
     TARGET_MAJOR_VERSION = 3
     TARGET_MINOR_VERSION = 1
-    
+
     class << self
       def reserved_app_names
         @reserved_app_names ||= [
@@ -53,49 +53,50 @@ module Refinery
         ]
       end
     end
-    
+
     def run!
       self.destination_root = app_path
-      
+      @app_pathname = Pathname.new(File.expand_path(app_path))
+
       validate_options!
       generate_rails!
       prepare_gemfile!
       run_bundle
-      
+
       create_database!
-      
+
       Refinery::CmsGenerator.start [], :destination_root => app_path
       Refinery::CoreGenerator.start [], :destination_root => app_path
       Refinery::ResourcesGenerator.start [], :destination_root => app_path
       Refinery::PagesGenerator.start [], :destination_root => app_path
       Refinery::ImagesGenerator.start [], :destination_root => app_path
-      
+
       rake('refinery_core:install:migrations')
       rake('refinery_authentication:install:migrations')
       rake('refinery_settings:install:migrations')
       rake('refinery_images:install:migrations')
       rake('refinery_pages:install:migrations')
       rake('refinery_resources:install:migrations')
-      
+
       rake("db:migrate#{' --trace' if options[:trace]}")
-      
+
       puts "\n---------"
-      puts "Refinery successfully installed in '#{@app_path}'!\n\n"
-      
+      puts "Refinery successfully installed in '#{@app_pathname}'!\n\n"
+
       heroku_deploy! if options[:heroku]
-      
+
       note = ["\n=== ACTION REQUIRED ==="]
       if options[:skip_db]
         note << "Because you elected to skip database creation and migration in the installer"
         note << "you will need to run the following tasks manually to maintain correct operation:"
-        note << "\ncd #{app_path}"
+        note << "\ncd #{@app_pathname}"
         note << "bundle exec rake db:create"
         note << "bundle exec rails generate refinerycms"
         note << "bundle exec rake db:migrate"
         note << "\n---------\n"
       end
       note << "Now you can launch your webserver using:"
-      note << "\ncd #{app_path}"
+      note << "\ncd #{@app_pathname}"
       note << "rails server"
       note << "\nThis will launch the built-in webserver at port 3000."
       note << "You can now see your site running in your browser at http://localhost:3000"
@@ -110,32 +111,32 @@ module Refinery
 
       puts note.join("\n")
     end
-        
+
     protected
-    
+
       def validate_options!
-        app_name = app_path.split(File::SEPARATOR).last
-        
+        app_name = @app_pathname.split.last.to_s
+
         unless valid_rails_version?
           puts "\nRails #{Rails::VERSION::STRING} is not supported by Refinery #{Refinery.version}, " \
                "please use Rails #{TARGET_MAJOR_VERSION}.#{TARGET_MINOR_VERSION}.x instead."
           exit(1)
         end
-        
+
         unless valid_app_name?(app_name)
           puts "\nYou can't use '#{app_name}' as a name for your project, this is a reserved word that will cause conflicts."
           puts "Please choose another name for your new project."
           puts "\n"
           exit(1)
         end
-        
-        unless options[:force] || !File.directory?(app_path)
-          puts "\nThe directory '#{app_path}' that you specified already exists."
+
+        unless options[:force] || !@app_pathname.directory?
+          puts "\nThe directory '#{@app_pathname}' that you specified already exists."
           puts "Use --force to overwrite an existing directory."
           puts "\n"
           exit(1)
         end
-        
+
         if options[:heroku]
           if options[:heroku].to_s.include?('_') or options[:heroku].to_s.length > 30
             message = ["\nThe application name '#{options[:heroku]}' that you specified is invalid for Heroku."]
@@ -161,22 +162,22 @@ module Refinery
           end
         end
       end
-      
-      def generate_rails!        
+
+      def generate_rails!
         rails_options = options.dup
         rails_options[:skip_test_unit] = true
         rails_options[:skip_bundle]    = true
-        
+
         Rails::Generators::AppGenerator.new([app_path], rails_options).invoke_all
-        
+
         if defined?(JRUBY_VERSION)
-          find_and_replace(app_path.join('Gemfile'), /['|"]sqlite3['|"]/, "'activerecord-jdbcsqlite3-adapter'")
+          find_and_replace('Gemfile', /['|"]sqlite3['|"]/, "'activerecord-jdbcsqlite3-adapter'")
         end
 
         # Remove rails from the Gemfile so that Refinery can manage it
-        find_and_replace(app_path.join('Gemfile'), %r{^gem 'rails'}, "# gem 'rails'")
+        find_and_replace('Gemfile', %r{^gem 'rails'}, "# gem 'rails'")
       end
-      
+
       def prepare_gemfile!
         if options[:refinery_edge]
           gem 'refinerycms', :git => 'git://github.com/resolve/refinerycms.git'
@@ -187,21 +188,21 @@ module Refinery
         if options[:testing]
           gem 'refinerycms-testing', :git => 'git://github.com/resolve/refinerycms.git'
         end
-        
+
         if options[:gems].present?
           options[:gems].each do |g|
             gem g
           end
         end
-        
+
         gem 'fog' if options[:heroku]
       end
-      
+
       def create_database!
         unless options[:database] == 'sqlite3'
           # Ensure the database exists so that queries like .table_exists? don't fail.
           puts "\nCreating a new database.."
-  
+
           # Warn about incorrect username or password.
           if options[:ident]
             note = "NOTE: If ident authentication fails then the installer will stall or fail here.\n\n"
@@ -210,11 +211,11 @@ module Refinery
             note << " or your password is not '#{options[:password]}' then the installer will stall here.\n\n"
           end
           puts note
-          
+
           rake("db:create#{' --trace' if options[:trace]}")
         end
       end
-      
+
       def heroku_deploy!
         puts "\n\nInitializing and committing to git..\n"
         run("git init && git add . && git commit -am 'Initial Commit'")
@@ -231,22 +232,22 @@ module Refinery
         puts "\n\nRestarting servers...\n"
         run("heroku restart")
       end
-      
+
     private
-    
+
       def find_and_replace(file, find, replace)
-        contents = File.read(File.join(@app_path, file)).gsub!(find, replace)
-        File.open(File.join(@app_path, file), "w") do |f|
+        contents = @app_pathname.join(file).read.gsub!(find, replace)
+        @app_pathname.join(file).open("w") do |f|
           f.puts contents
         end
       end
-      
+
       def valid_rails_version?
         Rails::VERSION::MAJOR >= TARGET_MAJOR_VERSION && Rails::VERSION::MINOR >= TARGET_MINOR_VERSION
       end
-      
+
       def valid_app_name?(app_name)
-        !self.class.reserved_app_names.include?(app_name.downcase)
+        self.class.reserved_app_names.exclude?(app_name.downcase)
       end
   end
 end
@@ -259,7 +260,7 @@ module Rails
         true
       end
     end
-    
+
     class AppGenerator
       def self.exit_on_failure?
         true
