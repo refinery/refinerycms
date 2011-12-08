b87eaaf4ed48503f37acb0fcdb3d66f815b652ad
Garrett Heinlen <heinleng@gmail.com>
2011-12-08 01:18:36 -0600

    changed method in forms engine

--- core/lib/generators/refinery/form/templates/lib/refinery/plural_name/engine.rb
+++ core/lib/generators/refinery/form/templates/lib/refinery/plural_name/engine.rb
@@ -5,7 +5,7 @@ module Refinery
     class Engine < Rails::Engine
       include Refinery::Engine
 
-      isolate_namespace Refinery::<%= class_name.plural_name %>
+      isolate_namespace Refinery::<%= class_name.pluralize %>
       engine_name :refinery_<%= plural_name %>
 
       config.after_initialize do
