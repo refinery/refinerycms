b4b38c926259178fb827d040878788677fbf0976
Uģis Ozols <ugis.ozolss@gmail.com>
2011-12-08 15:27:26 +0200

    Couple more specs for .home_page?

--- core/spec/lib/refinery/application_controller_spec.rb
+++ core/spec/lib/refinery/application_controller_spec.rb
@@ -7,6 +7,16 @@ end
 describe "Refinery::ApplicationController" do
   describe DummyController, :type => :controller do
     describe ".home_page?" do
+      it "matches root url" do
+        controller.stub(:root_path).and_return("/")
+        controller.home_page?.should be_true
+      end
+
+      it "matches localised root url" do
+        controller.stub(:root_path).and_return("/en/")
+        controller.home_page?.should be_true
+      end
+
       it "escapes regexp" do
         request.stub(:path).and_return("\/huh)")
 
