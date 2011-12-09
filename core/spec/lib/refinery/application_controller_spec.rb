842125d73cde39ad8ecf1da48b91c5c0ec04429b
Uģis Ozols <ugis.ozolss@gmail.com>
2011-12-09 11:44:36 +0200

    Fix specs and use anonymous controller.

--- core/spec/lib/refinery/application_controller_spec.rb
+++ core/spec/lib/refinery/application_controller_spec.rb
@@ -1,25 +1,26 @@
 require "spec_helper"
 
-class DummyController < ::ApplicationController
-  include ::Refinery::ApplicationController
-end
-
 describe "Refinery::ApplicationController" do
-  describe DummyController, :type => :controller do
+  describe "DummyController", :type => :controller do
+    controller do
+      include ::Refinery::ApplicationController
+    end
+
     describe ".home_page?" do
       it "matches root url" do
         controller.stub(:root_path).and_return("/")
+        request.stub(:path).and_return("/")
         controller.home_page?.should be_true
       end
 
       it "matches localised root url" do
-        controller.stub(:root_path).and_return("/en/")
+        controller.main_app.stub(:root_path).and_return("/en/")
+        request.stub(:path).and_return("/en")
         controller.home_page?.should be_true
       end
 
       it "escapes regexp" do
         request.stub(:path).and_return("\/huh)")
-
         expect { controller.home_page? }.to_not raise_error(RegexpError)
       end
     end
