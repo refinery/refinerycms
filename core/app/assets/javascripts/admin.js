305586eb681dffa29e2976e839f6d9a5b7f2f857
Paul Sponagl <paul@sponagl.de>
2011-12-12 13:38:36 +0100

    be able to define changes to wymeditor in advance

--- core/app/assets/javascripts/admin.js
+++ core/app/assets/javascripts/admin.js
@@ -2,6 +2,5 @@
 // Just mirror the options specified in boot_wym.js with the new options here.
 // This will completely override anything specified in boot_wym.js for that key.
 // e.g. skin: 'something_else'
-var custom_wymeditor_boot_options = {
+if (typeof(custom_wymeditor_boot_options) == "undefined") { custom_wymeditor_boot_options = {}; }
 
-};
