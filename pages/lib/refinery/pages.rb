e76670a5ccf5b26f3743b42e753cc5f9826229ba
Uģis Ozols <ugis.ozolss@gmail.com>
2011-12-15 11:57:05 +0200

    Move cache_pages_backend setting to Refinery::Pages.

--- pages/lib/refinery/pages.rb
+++ pages/lib/refinery/pages.rb
@@ -17,7 +17,7 @@ module Refinery
 
     config_accessor :pages_per_dialog, :pages_per_admin_index, :new_page_parts,
                     :marketable_urls, :approximate_ascii, :strip_non_ascii,
-                    :default_parts, :use_custom_slugs
+                    :default_parts, :use_custom_slugs, :cache_pages_backend
 
     self.pages_per_dialog = 14
     self.pages_per_admin_index = 20
@@ -27,6 +27,7 @@ module Refinery
     self.strip_non_ascii = false
     self.default_parts = ["Body", "Side Body"]
     self.use_custom_slugs = false
+    self.cache_pages_backend = false
 
     class << self
       def root
