f7a58ca392c92914ff0d755d77ebe6fbe8834a5b
Philip Arndt <parndt@gmail.com>
2011-12-17 12:59:17 +1300

    Not using capture_stdout

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -70,15 +70,3 @@ else
   setup_environment
   each_run
 end
-
-def capture_stdout(stdin_str = '')
-  begin
-    require 'stringio'
-    $o_stdin, $o_stdout, $o_stderr = $stdin, $stdout, $stderr
-    $stdin, $stdout, $stderr = StringIO.new(stdin_str), StringIO.new, StringIO.new
-    yield
-    {:stdout => $stdout.string, :stderr => $stderr.string}
-  ensure
-    $stdin, $stdout, $stderr = $o_stdin, $o_stdout, $o_stderr
-  end
-end
