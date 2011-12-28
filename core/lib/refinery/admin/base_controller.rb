9b9743aaa36bfe86ded2f40b89a02404d67659ba
Uģis Ozols <ugis.ozolss@gmail.com>
2011-12-28 16:19:16 +0200

    Remove deprecation messages about InstanceMethods (ActiveSupport::Concern).

--- core/lib/refinery/admin/base_controller.rb
+++ core/lib/refinery/admin/base_controller.rb
@@ -16,77 +16,75 @@ module Refinery
         helper_method :searching?, :group_by_date
       end
 
-      module InstanceMethods
-        def admin?
-          true # we're in the admin base controller, so always true.
-        end
+      def admin?
+        true # we're in the admin base controller, so always true.
+      end
 
-        def searching?
-          params[:search].present?
-        end
+      def searching?
+        params[:search].present?
+      end
 
-        def error_404(exception=nil)
-          # fallback to the default 404.html page.
-          render :file => Rails.root.join("public", "404.html").cleanpath.to_s,
-                 :layout => false,
-                 :status => 404
-        end
+      def error_404(exception=nil)
+        # fallback to the default 404.html page.
+        render :file => Rails.root.join("public", "404.html").cleanpath.to_s,
+               :layout => false,
+               :status => 404
+      end
 
       protected
 
-        def group_by_date(records)
-          new_records = []
+      def group_by_date(records)
+        new_records = []
 
-          records.each do |record|
-            key = record.created_at.strftime("%Y-%m-%d")
-            record_group = new_records.collect{|records| records.last if records.first == key }.flatten.compact << record
-            (new_records.delete_if {|i| i.first == key}) << [key, record_group]
-          end
-
-          new_records
+        records.each do |record|
+          key = record.created_at.strftime("%Y-%m-%d")
+          record_group = new_records.collect{|records| records.last if records.first == key }.flatten.compact << record
+          (new_records.delete_if {|i| i.first == key}) << [key, record_group]
         end
 
-        def restrict_plugins
-          current_length = (plugins = current_refinery_user.authorized_plugins).length
-
-          # Superusers get granted access if they don't already have access.
-          if current_refinery_user.has_role?(:superuser)
-            if (plugins = plugins | ::Refinery::Plugins.registered.names).length > current_length
-              current_refinery_user.plugins = plugins
-            end
-          end
-
-          ::Refinery::Plugins.set_active(plugins)
-        end
-
-        def restrict_controller
-          # if ::Refinery::Plugins.active.reject { |plugin| params[:controller] !~ Regexp.new(plugin.menu_match)}.empty?
-          #   warn "'#{current_refinery_user.username}' tried to access '#{params[:controller]}' but was rejected."
-          #   error_404
-          # end
-        end
-
-        # Override method from application_controller. Not needed in this controller.
-        def find_pages_for_menu; end
-
-      private
-        def layout?
-          "refinery/admin#{"_dialog" if from_dialog?}"
-        end
-
-        # Check whether it makes sense to return the user to the last page they
-        # were at instead of the default e.g. refinery_admin_pages_path
-        # right now we just want to snap back to index actions and definitely not to dialogues.
-        def store_location?
-          store_location unless action_name !~ /index/ or request.xhr? or from_dialog?
-        end
-
-        # Override authorized? so that only users with the Refinery role can admin the website.
-        def authorized?
-          refinery_user?
-        end
+        new_records
       end
 
+      def restrict_plugins
+        current_length = (plugins = current_refinery_user.authorized_plugins).length
+
+        # Superusers get granted access if they don't already have access.
+        if current_refinery_user.has_role?(:superuser)
+          if (plugins = plugins | ::Refinery::Plugins.registered.names).length > current_length
+            current_refinery_user.plugins = plugins
+          end
+        end
+
+        ::Refinery::Plugins.set_active(plugins)
+      end
+
+      def restrict_controller
+        # if ::Refinery::Plugins.active.reject { |plugin| params[:controller] !~ Regexp.new(plugin.menu_match)}.empty?
+        #   warn "'#{current_refinery_user.username}' tried to access '#{params[:controller]}' but was rejected."
+        #   error_404
+        # end
+      end
+
+      # Override method from application_controller. Not needed in this controller.
+      def find_pages_for_menu; end
+
+      private
+
+      def layout?
+        "refinery/admin#{"_dialog" if from_dialog?}"
+      end
+
+      # Check whether it makes sense to return the user to the last page they
+      # were at instead of the default e.g. refinery_admin_pages_path
+      # right now we just want to snap back to index actions and definitely not to dialogues.
+      def store_location?
+        store_location unless action_name !~ /index/ or request.xhr? or from_dialog?
+      end
+
+      # Override authorized? so that only users with the Refinery role can admin the website.
+      def authorized?
+        refinery_user?
+      end
     end
   end
 end
--- core/lib/refinery/application_controller.rb
+++ core/lib/refinery/application_controller.rb
@@ -37,65 +37,64 @@ module Refinery
       end
     end
 
-    module InstanceMethods
-      def admin?
-        controller_name =~ %r{^admin/}
-      end
+    def admin?
+      controller_name =~ %r{^admin/}
+    end
 
-      def error_404(exception=nil)
-        # fallback to the default 404.html page.
-        file = Rails.root.join('public', '404.html')
-        file = Refinery.roots(:'refinery/core').join('public', '404.html') unless file.exist?
-        render :file => file.cleanpath.to_s,
-               :layout => false,
-               :status => 404
-      end
+    def error_404(exception=nil)
+      # fallback to the default 404.html page.
+      file = Rails.root.join('public', '404.html')
+      file = Refinery.roots(:'refinery/core').join('public', '404.html') unless file.exist?
+      render :file => file.cleanpath.to_s,
+             :layout => false,
+             :status => 404
+    end
 
-      def from_dialog?
-        params[:dialog] == 'true' or params[:modal] == 'true'
-      end
+    def from_dialog?
+      params[:dialog] == 'true' or params[:modal] == 'true'
+    end
 
-      def home_page?
-        main_app.root_path =~ /^#{Regexp.escape(request.path)}\/?/
-      end
+    def home_page?
+      main_app.root_path =~ /^#{Regexp.escape(request.path)}\/?/
+    end
 
-      def just_installed?
-        ::Refinery::Role[:refinery].users.empty?
-      end
+    def just_installed?
+      ::Refinery::Role[:refinery].users.empty?
+    end
 
-      def local_request?
-        Rails.env.development? or request.remote_ip =~ /(::1)|(127.0.0.1)|((192.168).*)/
-      end
+    def local_request?
+      Rails.env.development? or request.remote_ip =~ /(::1)|(127.0.0.1)|((192.168).*)/
+    end
 
-      def login?
-        (controller_name =~ /^(user|session)(|s)/ and not admin?) or just_installed?
-      end
+    def login?
+      (controller_name =~ /^(user|session)(|s)/ and not admin?) or just_installed?
+    end
 
     protected
 
-      # get all the pages to be displayed in the site menu.
-      def find_pages_for_menu
-        raise NotImplementedError, 'Please implement protected method find_pages_for_menu'
-      end
+    # get all the pages to be displayed in the site menu.
+    def find_pages_for_menu
+      raise NotImplementedError, 'Please implement protected method find_pages_for_menu'
+    end
 
-      # use a different model for the meta information.
-      def present(model)
-        presenter = (Object.const_get("#{model.class}Presenter") rescue ::Refinery::BasePresenter)
-        @meta = presenter.new(model)
-      end
+    # use a different model for the meta information.
+    def present(model)
+      presenter = (Object.const_get("#{model.class}Presenter") rescue ::Refinery::BasePresenter)
+      @meta = presenter.new(model)
+    end
 
-      def show_welcome_page?
-        if just_installed? and controller_name != 'users'
-          render :template => 'refinery/welcome', :layout => 'login'
-        end
+    def show_welcome_page?
+      if just_installed? and controller_name != 'users'
+        render :template => 'refinery/welcome', :layout => 'login'
       end
+    end
 
     private
-      def store_current_location!
-        if admin? and request.get? and !request.xhr? and !from_dialog?
-          # ensure that we don't redirect to AJAX or POST/PUT/DELETE urls
-          session[:refinery_return_to] = request.path
-        end
+
+    def store_current_location!
+      if admin? and request.get? and !request.xhr? and !from_dialog?
+        # ensure that we don't redirect to AJAX or POST/PUT/DELETE urls
+        session[:refinery_return_to] = request.path
       end
     end
   end
