48dcf58310bcb8b0c62f9494e8fbc79c0d3e9671
Philip Arndt <parndt@gmail.com>
2012-01-12 17:48:02 +1300

    DRY the authentication macros a bit

--- testing/lib/refinery/testing/request_macros/authentication.rb
+++ testing/lib/refinery/testing/request_macros/authentication.rb
@@ -4,26 +4,37 @@ module Refinery
       module Authentication
         def login_refinery_user
           before(:each) do
-            FactoryGirl.create(:refinery_user, :username => "refinerycms",
-                                    :password => "123456",
-                                    :password_confirmation => "123456",
-                                    :email => "refinerycms@refinerycms.com")
+            password = '123456'
+            refinery_user = FactoryGirl.create(:refinery_user, {
+              :username => "refinerycms",
+              :password => password,
+              :password_confirmation => password,
+              :email => "refinerycms@refinerycms.com"
+            })
+
             visit new_refinery_user_session_path
-            fill_in "Login", :with => "refinerycms"
-            fill_in "Password", :with => "123456"
+
+            fill_in "Login", :with => refinery_user.username
+            fill_in "Password", :with => password
+
             click_button "Sign in"
           end
         end
 
         def login_refinery_translator
           before(:each) do
+            password = '123456'
             FactoryGirl.create(:refinery_user)
-            user = FactoryGirl.create(:refinery_translator, :password => "123456",
-                                      :password_confirmation => "123456")
+            user = FactoryGirl.create(:refinery_translator, {
+              :password => password,
+              :password_confirmation => password
+            })
 
             visit new_refinery_user_session_path
+
             fill_in "Login", :with => user.username
-            fill_in "Password", :with => "123456"
+            fill_in "Password", :with => password
+
             click_button "Sign in"
           end
         end
