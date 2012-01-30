f44241e8c81f4b82bd66ad433fb20fcef499abcf
Philip Arndt <parndt@gmail.com>
2012-01-30 16:57:13 +1300

    Added configurable whitelist tags to WYMeditor which fixes #1202

--- core/lib/refinery/core/configuration.rb
+++ core/lib/refinery/core/configuration.rb
@@ -5,7 +5,7 @@ module Refinery
     config_accessor :rescue_not_found, :s3_backend, :base_cache_key, :site_name,
                     :google_analytics_page_code, :authenticity_token_on_frontend,
                     :menu_hide_children, :dragonfly_secret, :ie6_upgrade_message_enabled,
-                    :show_internet_explorer_upgrade_message
+                    :show_internet_explorer_upgrade_message, :wymeditor_whitelist_tags
 
     self.rescue_not_found = false
     self.s3_backend = false
@@ -17,5 +17,6 @@ module Refinery
     self.dragonfly_secret = Array.new(24) { rand(256) }.pack('C*').unpack('H*').first
     self.ie6_upgrade_message_enabled = true
     self.show_internet_explorer_upgrade_message = false
+    self.wymeditor_whitelist_tags = {}
   end
 end
