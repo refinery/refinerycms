073214f20c1b64e693d7a7f2f6158cae4681c703
Philip Arndt <parndt@gmail.com>
2012-02-01 15:34:07 +1300

    Updated routes for custom assets

--- core/spec/requests/refinery/admin/custom_assets_spec.rb
+++ core/spec/requests/refinery/admin/custom_assets_spec.rb
@@ -11,7 +11,7 @@ module Refinery
 
       it "should be rendered when specified" do
         ::Refinery::Core.config.register_javascript('custom_js')
-        visit refinery_admin_dashboard_path
+        visit admin_dashboard_path
         page.body.include?('custom_js.js').should be
       end
     end
@@ -23,7 +23,7 @@ module Refinery
 
       it "should be rendered when specified" do
         ::Refinery::Core.config.register_stylesheet('custom_css')
-        visit refinery_admin_dashboard_path
+        visit admin_dashboard_path
         page.body.include?('custom_css.css').should be
       end
     end
