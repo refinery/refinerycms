b6a4a5441ea6aace386ca5a8f07d87312d2ead11
Philip Arndt <parndt@gmail.com>
2012-02-02 16:24:37 +1300

    Insert the namespace into the admin path if it's different to the plural_name.

--- core/lib/generators/refinery/engine/templates/config/routes.rb
+++ core/lib/generators/refinery/engine/templates/config/routes.rb
@@ -5,15 +5,15 @@ Refinery::Core::Engine.routes.draw do
     resources :<%= class_name.pluralize.underscore.downcase %>, :only => [:index, :show]
   end
 <% end %>
-
   # Admin routes
   namespace :<%= namespacing.underscore %>, :path => '' do
-    namespace :admin, :path => 'refinery/<%= namespacing.underscore %>' do
-      resources :<%= class_name.pluralize.underscore.downcase %><%= ", :path => ''" if namespacing.underscore.pluralize == class_name.pluralize.underscore.downcase %>, :except => :show do
+    namespace :admin, :path => 'refinery<%= "/#{namespacing.underscore}" if namespacing.underscore != plural_name %>' do
+      resources :<%= class_name.pluralize.underscore.downcase %>, :except => :show do
         collection do
           post :update_positions
         end
       end
     end
   end
+
 end
