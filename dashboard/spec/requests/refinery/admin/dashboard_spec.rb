94f6521bf252c2f2e78a32e97340af83d4303292
Philip Arndt <parndt@gmail.com>
2012-02-04 18:10:25 +1300

    Used translation keys and path helpers in dashboard specs.

--- dashboard/spec/requests/refinery/admin/dashboard_spec.rb
+++ dashboard/spec/requests/refinery/admin/dashboard_spec.rb
@@ -7,26 +7,26 @@ describe "dashboard" do
     specify "buttons" do
       visit refinery.admin_dashboard_path
 
-      page.should have_content("Quick Tasks")
+      page.should have_content(::I18n.t('quick_tasks', :scope => 'refinery.admin.dashboard.index'))
 
       # add new page
-      page.should have_content("Add a new page")
-      page.should have_selector("a[href='/refinery/pages/new']")
+      page.should have_content(::I18n.t('add_a_new_page', :scope => 'refinery.admin.dashboard.actions'))
+      page.should have_selector("a[href='#{refinery.new_admin_page_path}']")
 
       # update page
-      page.should have_content("Update a page")
-      page.should have_selector("a[href='/refinery/pages']")
+      page.should have_content(::I18n.t('update_a_page', :scope => 'refinery.admin.dashboard.actions'))
+      page.should have_selector("a[href='#{refinery.admin_pages_path}']")
 
       # upload file
       if defined? Refinery::Resource
-        page.should have_content("Upload a file")
-        page.should have_selector("a[href*='/refinery/resources/new']")
+        page.should have_content(::I18n.t('upload_a_file', :scope => 'refinery.admin.dashboard.actions'))
+        page.should have_selector("a[href*='#{refinery.new_admin_resource_path}']")
       end
 
       # upload image
       if defined? Refinery::Image
-        page.should have_content("Upload an image")
-        page.should have_selector("a[href*='/refinery/images/new']")
+        page.should have_content(::I18n.t('upload_a_image', :scope => 'refinery.admin.dashboard.actions'))
+        page.should have_selector("a[href*='#{refinery.new_admin_image_path}']")
       end
     end
   end
