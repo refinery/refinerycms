70ff8a9a771d447c80f57ad89b0bc1585e11c554
Philip Arndt <parndt@gmail.com>
2012-02-07 11:05:04 +1300

    Increase default password length to max out at 128 characters because 20 is shorter than most of my passwords.

--- core/lib/generators/refinery/cms/templates/config/initializers/devise.rb
+++ core/lib/generators/refinery/cms/templates/config/initializers/devise.rb
@@ -123,7 +123,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length. Default is 6..128.
-  config.password_length = 4..20
+  config.password_length = 4..128
 
   # Email regex used to validate email formats. It simply asserts that
   # an one (and only one) @ exists in the given string. This is mainly
