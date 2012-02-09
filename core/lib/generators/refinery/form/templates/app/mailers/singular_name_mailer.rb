59f49b698af3819d492918f5551a7901006774cb
Garrett Heinlen <heinleng@gmail.com>
2012-02-09 15:15:37 -0600

    trying to fix form mailer

--- core/lib/generators/refinery/form/templates/app/mailers/singular_name_mailer.rb
+++ core/lib/generators/refinery/form/templates/app/mailers/singular_name_mailer.rb
@@ -1,17 +1,17 @@
-class <%= class_name %>Mailer < ActionMailer::Base
+class Refinery_<%= namespacing %><%= class_name %>Mailer < ActionMailer::Base
 
   def confirmation(<%= singular_name %>, request)
-    subject     <%= class_name %>::Setting.confirmation_subject
+    subject     Refinery_<%= namespacing %><%= class_name %>::Setting.confirmation_subject
     recipients  <%= singular_name %>.email
     from        "\"#{Refinery::Core.site_name}\" <no-reply@#{request.domain}>"
-    reply_to    <%= class_name %>::Setting.notification_recipients.split(',').first
+    reply_to    Refinery_<%= namespacing %><%= class_name %>::Setting.notification_recipients.split(',').first
     sent_on     Time.now
     @<%= singular_name %> =  <%= singular_name %>
   end
 
   def notification(<%= singular_name %>, request)
-    subject     <%= class_name %>::Setting.notification_subject
-    recipients  <%= class_name %>::Setting.notification_recipients
+    subject     Refinery_<%= namespacing %><%= class_name %>::Setting.notification_subject
+    recipients  Refinery_<%= namespacing %><%= class_name %>::Setting.notification_recipients
     from        "\"#{Refinery::Core.site_name}\" <no-reply@#{request.domain}>"
     sent_on     Time.now
     @<%= singular_name %> =  <%= singular_name %>
