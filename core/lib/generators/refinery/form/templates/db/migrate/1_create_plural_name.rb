4383d14787b17a77e84bfe242560d5e47faa73d7
Garrett Heinlen <heinleng@gmail.com>
2012-02-09 15:08:48 -0600

    adjusted database migrations to proper namespacing

--- core/lib/generators/refinery/form/templates/db/migrate/1_create_plural_name.rb
+++ core/lib/generators/refinery/form/templates/db/migrate/1_create_plural_name.rb
@@ -1,7 +1,7 @@
-class Create<%= class_name.pluralize %> < ActiveRecord::Migration
+class Create<%= namespacing %><%= class_name.pluralize %> < ActiveRecord::Migration
 
   def self.up
-    create_table :refinery_<%= table_name %> do |t|
+    create_table :refinery_<%= "#{namespacing.underscore}_" if table_name != namespacing.underscore.pluralize -%><%= table_name %> do |t|
 <%
   attributes.each do |attribute|
     # turn image or resource into what it was supposed to be which is an integer reference to an image or resource.
@@ -20,7 +20,7 @@ class Create<%= class_name.pluralize %> < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :refinery_<%= table_name %>, :id
+    add_index :refinery_<%= "#{namespacing.underscore}_" if table_name != namespacing.underscore.pluralize -%><%= table_name %>, :id
 
     if (seed = Rails.root.join('db', 'seeds', '<%= class_name.pluralize.underscore.downcase %>.rb')).exist?
       load(seed)
@@ -29,14 +29,14 @@ class Create<%= class_name.pluralize %> < ActiveRecord::Migration
 
   def self.down
     if defined?(::Refinery::UserPlugin)
-      ::Refinery::UserPlugin.destroy_all({:name => "<%= class_name.pluralize.underscore.downcase %>"})
+      ::Refinery::UserPlugin.destroy_all({:name => "refinerycms-<%= namespacing.underscore %>"})
     end
 
     if defined?(::Refinery::Page)
-      ::Refinery::Page.delete_all({:link_url => "/<%= plural_name %>"})
+      ::Refinery::Page.delete_all({:link_url => "/<%= namespacing.underscore %>/<%= plural_name %>"})
     end
 
-    drop_table :refinery_<%= table_name %>
+    drop_table :refinery_<%= "#{namespacing.underscore}_" if table_name != namespacing.underscore.pluralize -%><%= table_name %>
   end
 
 end
