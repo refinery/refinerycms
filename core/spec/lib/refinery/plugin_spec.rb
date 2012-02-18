67be0f004183f370b14f875c6b1f8e86f3b3a1d3
Philip Arndt <parndt@gmail.com>
2012-02-18 16:59:38 +1300

    Update tests to just check the controller part of the hash.

--- core/spec/lib/refinery/plugin_spec.rb
+++ core/spec/lib/refinery/plugin_spec.rb
@@ -127,14 +127,14 @@ module Refinery
       context 'when @url is already defined' do
         it 'returns hash' do
           plugin.stub(:url).and_return({:controller => 'refinery/admin/testa'})
-          plugin.url.should == {:controller => 'refinery/admin/testa'}
+          plugin.url[:controller].should == 'refinery/admin/testa'
         end
       end
 
       context 'when controller is present' do
         it 'returns hash based on it' do
           plugin.stub(:controller).and_return('testb')
-          plugin.url.should == {:controller => 'refinery/admin/testb'}
+          plugin.url[:controller].should == 'refinery/admin/testb'
         end
       end
 
@@ -142,13 +142,13 @@ module Refinery
 
         it 'returns hash based on it' do
           plugin.stub(:directory).and_return('first/second/testc')
-          plugin.url.should == {:controller => 'refinery/admin/testc'}
+          plugin.url[:controller].should == 'refinery/admin/testc'
         end
       end
 
       context 'when controller and directory not present' do
         it 'returns hash based on plugins name' do
-          plugin.url.should == {:controller => 'refinery/admin/refinery_rspec'}
+          plugin.url[:controller].should == 'refinery/admin/refinery_rspec'
         end
       end
     end
