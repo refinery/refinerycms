110a019c379ab2b803a570ca17ff4b70ced9e299
Philip Arndt <parndt@gmail.com>
2012-02-20 10:54:32 +1300

    Refactored code in view to helper thumbnail_urls(image)

--- images/app/helpers/refinery/admin/images_helper.rb
+++ images/app/helpers/refinery/admin/images_helper.rb
@@ -6,6 +6,19 @@ module Refinery
           image_view.to_s == Refinery::Images.preferred_image_view.to_s
         }
       end
+
+      def thumbnail_urls(image)
+        thumbnail_urls = {
+          :"data-original" => asset_paths.compute_public_path(image.url, ''),
+          :"data-grid" => asset_paths.compute_public_path(image.thumbnail('135x135#c').url, '')
+        }
+
+        Refinery::Images.user_image_sizes.sort_by{|key,geometry| geometry}.each do |size, pixels|
+          thumbnail_urls[:"data-#{size.to_s.parameterize}"] = asset_paths.compute_public_path(image.thumbnail(pixels).url, '')
+        end
+
+        thumbnail_urls
+      end
     end
   end
 end
