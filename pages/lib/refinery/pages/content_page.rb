6643c6d29f30fedafa9e51a2e2d3473eccdfe4e1
Craig Ambrose <craig@craigambrose.com>
2012-02-21 08:59:38 +1300

    Content page now encapsulates array rather than inheriting from it

--- pages/lib/refinery/pages/content_page.rb
+++ pages/lib/refinery/pages/content_page.rb
@@ -1,19 +1,24 @@
 module Refinery
   module Pages
-    class ContentPage < Array
+    class ContentPage
       def self.build_for_page(page, page_title)
         sections = self.new
         sections.add_default_title_section(page_title) if page_title.present?
         sections.add_page_parts(page.parts) if page
         sections.add_default_post_page_sections
+        sections
+      end
+
+      def initialize(initial_sections = [])
+        @sections = initial_sections
       end
 
       def blank_section_css_classes(allowed_to_use_fallback)
-        reject {|section| section.has_content?(allowed_to_use_fallback)}.map(&:not_present_css_class)
+        @sections.reject {|section| section.has_content?(allowed_to_use_fallback)}.map(&:not_present_css_class)
       end
 
       def add_default_title_section(title)
-        self << SectionPresenter.new(:id => :body_content_title, :fallback_html => title, :title => true)
+        add_section SectionPresenter.new(:id => :body_content_title, :fallback_html => title, :title => true)
       end
 
       def add_default_post_page_sections
@@ -22,31 +27,43 @@ module Refinery
       end
 
       def hide_sections(ids_to_hide)
-        select {|section| ids_to_hide.include?(section.id)}.each(&:hide)
+        @sections.select {|section| ids_to_hide.include?(section.id)}.each(&:hide)
       end
 
       def add_page_parts(parts)
         parts.each do |part|
-          self << SectionPresenter.from_page_part(part)
+          add_section SectionPresenter.from_page_part(part)
         end
       end
 
       def fetch_template_overrides
-        each do |section|
+        @sections.each do |section|
           if section.id.present?
             section.override_html = yield section.id
           end
         end
       end
 
+      def add_section(new_section)
+        @sections << new_section
+      end
+
+      def get_section(index)
+        @sections[index]
+      end
+
+      def to_html(allowed_to_use_fallback = true)
+        @sections.map {|section| section.wrapped_html(allowed_to_use_fallback)}.join("\n").html_safe
+      end
+
       private
 
         def add_section_if_missing(options)
-          self << SectionPresenter.new(options) unless has_section?(options[:id])
+          add_section SectionPresenter.new(options) unless has_section?(options[:id])
         end
 
         def has_section?(id)
-          detect {|section| section.id == id}
+          @sections.detect {|section| section.id == id}
         end
     end
   end
--- pages/spec/lib/pages/content_page_spec.rb
+++ pages/spec/lib/pages/content_page_spec.rb
@@ -6,36 +6,38 @@ module Refinery
       let(:part)  { double(PagePart, :body => 'part_body', :title => 'A Wonderful Page Part') }
       let(:part2) { double(PagePart, :body => 'part_body2', :title => 'Another Wonderful Page Part') }
       let(:title) { 'This Great Page' }
+      let(:section1) { double(SectionPresenter, :id => 'foo') }
+      let(:section2) { double(SectionPresenter, :id => 'bar') }
 
       describe "when building for page" do
         let(:page_with_one_part) { double(Page, :parts => [part]) }
 
         it "adds page title section before page parts" do
           content = ContentPage.build_for_page(page_with_one_part, title)
-          content.first.fallback_html.should == title
+          content.get_section(0).fallback_html.should == title
         end
 
         it "adds a section for each page part" do
           page = double(Page, :parts => [part, part2])
           content = ContentPage.build_for_page(page, title)
-          content[1].fallback_html.should == 'part_body'
-          content[2].fallback_html.should == 'part_body2'
+          content.get_section(1).fallback_html.should == 'part_body'
+          content.get_section(2).fallback_html.should == 'part_body2'
         end
 
         it "adds body content left and right after page parts" do
           content = ContentPage.build_for_page(page_with_one_part, title)
-          content[2].id.should == :body_content_left
-          content[3].id.should == :body_content_right
+          content.get_section(2).id.should == :body_content_left
+          content.get_section(3).id.should == :body_content_right
         end
 
         it "doesnt add page parts if page is nil" do
           content = ContentPage.build_for_page(nil, title)
-          content[1].id.should == :body_content_left
+          content.get_section(1).id.should == :body_content_left
         end
 
         it "doesnt add title if it is blank" do
           content = ContentPage.build_for_page(nil, '')
-          content[0].id.should == :body_content_left
+          content.get_section(0).id.should == :body_content_left
         end
       end
 
@@ -45,7 +47,7 @@ module Refinery
         it "includes css class for any section which doesnt have content" do
           section.stub(:has_content?).with(true).and_return(false)
           content = ContentPage.new
-          content << section
+          content.add_section section
 
           content.blank_section_css_classes(true).should == ['no_section1']
         end
@@ -53,20 +55,17 @@ module Refinery
         it "doesnt include sections which have content" do
           section.stub(:has_content?).with(true).and_return(true)
           content = ContentPage.new
-          content << section
+          content.add_section section
 
           content.blank_section_css_classes(true).should be_empty
         end
       end
 
       describe "when hiding sections" do
-        let(:section1) { double(SectionPresenter, :id => 'foo') }
-        let(:section2) { double(SectionPresenter, :id => 'bar') }
-
         before do
           @content = ContentPage.new
-          @content << section1
-          @content << section2
+          @content.add_section section1
+          @content.add_section section2
         end
 
         it "hides a section specified by id" do
@@ -83,7 +82,7 @@ module Refinery
         it "yields a section with an id and stores the result in its override html" do
           section = double(SectionPresenter, :id => 'foo')
           section.should_receive(:override_html=).with('some override')
-          @content << section
+          @content.add_section section
 
           @content.fetch_template_overrides do |section_id|
             section_id.should == 'foo'
@@ -94,11 +93,31 @@ module Refinery
         it "doesnt yield a section without an id" do
           section = double(SectionPresenter, :id => nil)
           section.should_receive(:override_html=).never
-          @content << section
+          @content.add_section section
 
           @content.fetch_template_overrides do |section_id|
             raise "this should not occur"
           end
+        end
+      end
+
+      describe "when rendering as html" do
+        it "is blank if it has no sections" do
+          content = ContentPage.new
+          content.to_html.should be_blank
+        end
+
+        it "returns sections joined by a newline" do
+          section1.stub(:wrapped_html).and_return('foo')
+          section2.stub(:wrapped_html).and_return('bar')
+          content = ContentPage.new([section1, section2])
+          content.to_html.should == "foo\nbar"
+        end
+
+        it "passes allowed_to_use_fallback option on to sections" do
+          section1.should_receive(:wrapped_html).with(false).and_return('foo')
+          content = ContentPage.new([section1])
+          content.to_html(false)
         end
       end
     end
