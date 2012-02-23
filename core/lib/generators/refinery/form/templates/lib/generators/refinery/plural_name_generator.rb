9125c3135340244ebf15fc03342710bd3c648b9b
Philip Arndt <parndt@gmail.com>
2012-02-24 11:30:26 +1300

    We need a blank line before we append our seed line.

--- core/lib/generators/refinery/form/templates/lib/generators/refinery/plural_name_generator.rb
+++ core/lib/generators/refinery/form/templates/lib/generators/refinery/plural_name_generator.rb
@@ -11,6 +11,7 @@ module Refinery
       create_file 'db/seeds.rb' unless File.exists?(File.join(destination_root, 'db', 'seeds.rb'))
       append_file 'db/seeds.rb', :verbose => true do
         <<-EOH
+
 Refinery::<%= class_name.pluralize %>::Engine.load_seed
         EOH
       end
