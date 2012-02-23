508a383e63a1b11b7cb5d76397523f327278b138
Philip Arndt <parndt@gmail.com>
2012-02-23 16:27:50 +1300

    Fixes #1312 by generating seed data when a form extension's generator is run.

--- core/lib/generators/refinery/form/templates/lib/generators/refinery/plural_name_generator.rb
+++ core/lib/generators/refinery/form/templates/lib/generators/refinery/plural_name_generator.rb
@@ -7,5 +7,14 @@ module Refinery
       rake("refinery_<%= plural_name %>:install:migrations")
     end
 
+    def append_load_seed_data
+      create_file 'db/seeds.rb' unless File.exists?(File.join(destination_root, 'db', 'seeds.rb'))
+      append_file 'db/seeds.rb', :verbose => true do
+        <<-EOH
+Refinery::<%= class_name.pluralize %>::Engine.load_seed
+        EOH
+      end
+    end
+
   end
 end
