5e2e70c04c74cc580c4f6cf258df60c71b2a390b
Uģis Ozols <ugis.ozolss@gmail.com>
2012-02-29 22:20:14 +0200

    Selenium is the default Capybara.javascript_driver.

--- core/lib/generators/refinery/engine/templates/spec/spec_helper.rb
+++ core/lib/generators/refinery/engine/templates/spec/spec_helper.rb
@@ -15,9 +15,6 @@ def setup_environment
     config.filter_run :focus => true
     config.run_all_when_everything_filtered = true
   end
-
-  # set javascript driver for capybara
-  Capybara.javascript_driver = :selenium
 end
 
 def each_run
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,9 +23,6 @@ def setup_environment
     config.filter_run :js => nil if ENV['JS'] == 'false'
     config.run_all_when_everything_filtered = true
   end
-
-  # Set javascript driver for capybara
-  Capybara.javascript_driver = :selenium
 end
 
 def each_run
