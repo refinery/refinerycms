7646790fcf6121a50c43bbcc9247058b0eab99a8
Philip Arndt <parndt@gmail.com>
2012-03-01 00:59:53 +1300

    We don't need Refinery::AppGenerator anymore in favour of installing via Rails templates.

--- templates/refinery/installer.rb
+++ templates/refinery/installer.rb
@@ -6,10 +6,8 @@ append_file 'Gemfile' do
 # Refinery CMS
 gem 'refinerycms', '~> #{VERSION_BAND}'
 
-# Add i18n support to Refinery CMS (optional, you can remove this if you really want to but it is advised to keep it).
-gem 'refinerycms-i18n',   '~> #{VERSION_BAND}'
-
 # Specify additional Refinery CMS Extensions here (all optional):
+gem 'refinerycms-i18n',   '~> #{VERSION_BAND}'
 #  gem 'refinerycms-blog', '~> #{VERSION_BAND}'
 #  gem 'refinerycms-inquiries', '~> #{VERSION_BAND}'
 #  gem 'refinerycms-search', '~> #{VERSION_BAND}'
@@ -19,7 +17,7 @@ end
 
 run 'bundle install'
 rake 'db:create'
-generate 'refinery:cms --fresh-installation'
+generate "refinery:cms --fresh-installation #{ARGV.join(' ')}"
 
 rake 'railties:install:migrations db:migrate'
 rake 'db:seed'
