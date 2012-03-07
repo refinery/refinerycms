6de49e9486db070f5e9fa8d6261cc81c96ad101f
Philip Arndt <parndt@gmail.com>
2012-03-07 17:29:07 +1300

    Consistent formatting on PagePartSectionPresenter

--- pages/lib/refinery/pages/page_part_section_presenter.rb
+++ pages/lib/refinery/pages/page_part_section_presenter.rb
@@ -4,16 +4,16 @@ module Refinery
     # a PagePart model.
     class PagePartSectionPresenter < SectionPresenter
       def initialize(page_part)
-        super()
+        super
         self.fallback_html = page_part.body.html_safe if page_part.body
         self.id = convert_title_to_id(page_part.title) if page_part.title
       end
 
-      private
+    private
 
-        def convert_title_to_id(title)
-          title.to_s.gsub(/\ /, '').underscore.to_sym
-        end
+      def convert_title_to_id(title)
+        title.to_s.gsub(/\ /, '').underscore.to_sym
+      end
     end
   end
 end
