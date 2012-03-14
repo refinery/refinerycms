1f13203d51b862c9a0306f3c9b9284f35402f463
Philip Arndt <parndt@gmail.com>
2012-03-15 09:55:04 +1300

    Using :transaction strategy which certainly appears to speed things up but fails tests.

--- core/spec/support/database_cleaner.rb
+++ core/spec/support/database_cleaner.rb
@@ -2,7 +2,7 @@ require 'database_cleaner'
 
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.strategy = :transaction
   end
 
   config.before(:each) do
