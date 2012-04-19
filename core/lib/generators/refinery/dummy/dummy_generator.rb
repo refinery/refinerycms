927961013ec3cda3f8566c60ab98dac9cf6c0b0e
Philip Arndt <parndt@gmail.com>
2012-04-20 11:03:09 +1200

    Rails already generates the right database.yml, no need to replace it?

--- core/lib/generators/refinery/dummy/dummy_generator.rb
+++ core/lib/generators/refinery/dummy/dummy_generator.rb
@@ -28,9 +28,6 @@ module Refinery
     end
 
     def test_dummy_config
-      @database = options[:database]
-
-      template "rails/database.yml", "#{dummy_path}/config/database.yml", :force => true
       template "rails/boot.rb", "#{dummy_path}/config/boot.rb", :force => true
       template "rails/application.rb", "#{dummy_path}/config/application.rb", :force => true
       template "rails/routes.rb", "#{dummy_path}/config/routes.rb", :force => true
