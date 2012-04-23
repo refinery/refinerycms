e2e9fc096db1ae65a813b03155fce7ff5f719703
f3ng3r <frederikfenger@gmail.com>
2012-04-23 22:29:23 +0300

    Remove the `pages.html` file that is outside the `.../pages/` directory

--- pages/app/controllers/refinery/page_sweeper.rb
+++ pages/app/controllers/refinery/page_sweeper.rb
@@ -21,8 +21,13 @@ module Refinery
       page_cache_directory_path = Pathname.new(page_cache_directory.to_s)
 
       # Delete the full Cache
-      if (cache_root = page_cache_directory_path.join('refinery', 'cache', 'pages')).directory?
+      if (cache_root = page_cache_directory_path.join('refinery', 'cache')).directory?
         FileUtils.rm_rf cache_root
+      end
+      
+      # Delete the pages index file (/refinery/cache/pages.html)
+      if (cache_index = page_cache_directory_path.join('refinery', 'cache', 'pages.html')).file?
+        FileUtils.rm_f cache_index
       end
     end
   end
