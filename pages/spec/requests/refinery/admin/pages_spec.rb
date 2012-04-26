2ff2aa1c5bb690cd181771cce0dcddd2c9cfa175
Uģis Ozols <ugis.ozolss@gmail.com>
2012-04-26 09:27:12 +0300

    Experiment on Travis CI.

--- pages/spec/requests/refinery/admin/pages_spec.rb
+++ pages/spec/requests/refinery/admin/pages_spec.rb
@@ -657,6 +657,8 @@ module Refinery
 
               click_link "Add Link"
 
+              page.should have_selector("iframe#dialog_frame")
+
               page.within_frame("dialog_frame") do
                 page.should have_content("About")
                 page.should have_css("a[href$='/about']")
@@ -669,6 +671,8 @@ module Refinery
               end
 
               click_link "Add Link"
+
+              page.should have_selector("iframe#dialog_frame")
 
               page.within_frame("dialog_frame") do
                 page.should have_content("About Ru")
