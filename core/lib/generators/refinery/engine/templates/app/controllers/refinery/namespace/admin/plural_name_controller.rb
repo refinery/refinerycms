99fe741e51743803a343a36c9c506c77afa7ea69
Uģis Ozols <ugis.ozolss@gmail.com>
2012-04-26 08:49:18 +0300

    Match text attributes too in generated crudify code.

--- core/lib/generators/refinery/engine/templates/app/controllers/refinery/namespace/admin/plural_name_controller.rb
+++ core/lib/generators/refinery/engine/templates/app/controllers/refinery/namespace/admin/plural_name_controller.rb
@@ -3,10 +3,10 @@ module Refinery
     module Admin
       class <%= class_name.pluralize %>Controller < ::Refinery::AdminController
 
-        crudify :'refinery/<%= namespacing.underscore %>/<%= singular_name %>'<% if (title = attributes.detect { |a| a.type.to_s == "string" }).present? and title.name != 'title' %>,
-                :title_attribute => '<%= title.name %>'<% end %><% if plural_name == singular_name %>,
-                  :redirect_to_url => :refinery_<%= namespacing.underscore %>_admin_<%= singular_name %>_index_path
-    <% end %>, :xhr_paging => true
+        crudify :'refinery/<%= namespacing.underscore %>/<%= singular_name %>'<% if (title = attributes.detect { |a| a.type.to_s =~ /string|text/ }).present? and title.name != 'title' -%>,
+                :title_attribute => '<%= title.name %>'<% end -%><% if plural_name == singular_name -%>,
+                :redirect_to_url => :refinery_<%= namespacing.underscore %>_admin_<%= singular_name %>_index_path<% end %>,
+                :xhr_paging => true
 
       end
     end
