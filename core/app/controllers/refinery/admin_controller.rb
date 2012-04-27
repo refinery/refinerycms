c33cf5658f04c9e5ad04f73eae5e7c8bb1458362
thedarkone <thedarkone2@gmail.com>
2012-04-27 21:25:55 +0200

    Remove ApplicationHelper's file to prevent it from getting cached in Refinery::Core::Engine.helpers and thus stop picking any changes made to the application_helper.rb located in the main app.

--- core/app/controllers/refinery/admin_controller.rb
+++ core/app/controllers/refinery/admin_controller.rb
@@ -3,6 +3,7 @@
 module Refinery
   class AdminController < ::ActionController::Base
     include ::Refinery::ApplicationController
+    helper ApplicationHelper
     helper Refinery::Core::Engine.helpers
     include Refinery::Admin::BaseController
 
