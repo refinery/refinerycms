ef2393f5a5f1adbf54c405942b996921e90242ea
Uģis Ozols <ugis.ozolss@gmail.com>
2012-05-10 08:03:54 +0300

    Fhere are no 'refinery_login_with' and 'logged_in_user' methods available in 2-0-stable.

--- core/spec/requests/refinery/admin/controller_restriction_spec.rb
+++ core/spec/requests/refinery/admin/controller_restriction_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 module Refinery
   describe "plugin access" do
     context "as refinery user" do
-      refinery_login_with :refinery_user
+      login_refinery_user
 
       context "with permission" do
         it "allows access" do
@@ -14,7 +14,7 @@ module Refinery
 
       context "without permission" do
         before do
-          logged_in_user.stub(:plugins).and_return []
+          Refinery::User.first.plugins = []
         end
 
         it "denies access" do
