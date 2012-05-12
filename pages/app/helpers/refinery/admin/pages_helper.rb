e4c0aae722967b49a0153547531b7ff707995584
Philip Arndt <parndt@gmail.com>
2012-05-12 14:54:46 +1200

    Combined patches and linked to a PR in awesome_nested_set where passing a relation *will* work.

--- pages/app/helpers/refinery/admin/pages_helper.rb
+++ pages/app/helpers/refinery/admin/pages_helper.rb
@@ -2,16 +2,16 @@ module Refinery
   module Admin
     module PagesHelper
       def parent_id_nested_set_options(current_page)
-        all_pages = []
+        pages = []
         options = nested_set_options(::Refinery::Page, current_page) do |page|
-          all_pages << page
+          pages << page
           page
         end
         # page.title needs the :translations association, doing something like
-        # nested_set_options(::Refinery::Page.includes(:translations), page) doesn't work
-        ActiveRecord::Associations::Preloader.new(all_pages, :translations).run
-        options.map! {|page, id| ["#{'-' * page.level} #{page.title}", id]}
-        options
+        # nested_set_options(::Refinery::Page.includes(:translations), page) doesn't work, yet.
+        # See https://github.com/collectiveidea/awesome_nested_set/pull/123
+        ActiveRecord::Associations::Preloader.new(pages, :translations).run
+        options.map {|page, id| ["#{'-' * page.level} #{page.title}", id]}
       end
     end
   end
