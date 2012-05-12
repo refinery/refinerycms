c8dabaee9fd501d11adf4f0aeb252cd58b2ce5ef
Philip Arndt <parndt@gmail.com>
2012-05-12 14:57:11 +1200

    Implement feedback from @ugisozols.

--- pages/app/helpers/refinery/admin/pages_helper.rb
+++ pages/app/helpers/refinery/admin/pages_helper.rb
@@ -3,15 +3,12 @@ module Refinery
     module PagesHelper
       def parent_id_nested_set_options(current_page)
         pages = []
-        options = nested_set_options(::Refinery::Page, current_page) do |page|
-          pages << page
-          page
-        end
+        nested_set_options(::Refinery::Page, current_page) {|page| pages << page}
         # page.title needs the :translations association, doing something like
         # nested_set_options(::Refinery::Page.includes(:translations), page) doesn't work, yet.
         # See https://github.com/collectiveidea/awesome_nested_set/pull/123
         ActiveRecord::Associations::Preloader.new(pages, :translations).run
-        options.map {|page, id| ["#{'-' * page.level} #{page.title}", id]}
+        pages.map {|page| ["#{'-' * page.level} #{page.title}", page.id]}
       end
     end
   end
