0b1c89526227b9838faa8d2fd644092d27483b1e
Philip Arndt <parndt@gmail.com>
2012-05-23 21:56:33 +1200

    Updated to support new TestCase#process API.

--- testing/lib/refinery/testing/controller_macros/methods.rb
+++ testing/lib/refinery/testing/controller_macros/methods.rb
@@ -2,30 +2,29 @@ module Refinery
   module Testing
     module ControllerMacros
       module Methods
-        def get(action, parameters = nil, session = nil, flash = nil)
-          process_refinery_action(action, parameters, session, flash, "GET")
+        def get(action, *args)
+          process_refinery_action(action, 'GET', *args)
         end
 
         # Executes a request simulating POST HTTP method and set/volley the response
-        def post(action, parameters = nil, session = nil, flash = nil)
-          process_refinery_action(action, parameters, session, flash, "POST")
+        def post(action, *args)
+          process_refinery_action(action, 'POST', *args)
         end
 
         # Executes a request simulating PUT HTTP method and set/volley the response
-        def put(action, parameters = nil, session = nil, flash = nil)
-          process_refinery_action(action, parameters, session, flash, "PUT")
+        def put(action, *args)
+          process_refinery_action(action, 'PUT', *args)
         end
 
         # Executes a request simulating DELETE HTTP method and set/volley the response
-        def delete(action, parameters = nil, session = nil, flash = nil)
-          process_refinery_action(action, parameters, session, flash, "DELETE")
+        def delete(action, *args)
+          process_refinery_action(action, 'DELETE', *args)
         end
 
         private
 
-        def process_refinery_action(action, parameters = nil, session = nil, flash = nil, method = "GET")
-          parameters ||= {}
-          process(action, parameters.merge!(:use_route => :refinery), session, flash, method)
+        def process_refinery_action(action, http_method = 'GET', *args)
+          process(action, http_method, *args, :use_route => :refinery)
         end
       end
     end
