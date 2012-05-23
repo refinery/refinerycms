1ed6df3f8d8cf52911dd0ae0dc00a464e2411f01
Uģis Ozols <ugis.ozolss@gmail.com>
2012-05-23 16:15:08 +0300

    Make our overridden devise routes to be matched first.

--- authentication/config/routes.rb
+++ authentication/config/routes.rb
@@ -1,14 +1,6 @@
 Refinery::Core::Engine.routes.draw do
   begin
     require 'devise'
-    devise_for :refinery_user,
-               :class_name => 'Refinery::User',
-               :path => "#{Refinery::Core.backend_route}/users",
-               :controllers => { :registrations => 'refinery/users' },
-               :skip => [:registrations],
-               :path_names => { :sign_out => 'logout',
-                                :sign_in => 'login',
-                                :sign_up => 'register' }
 
     # Override Devise's other routes for convenience methods.
     devise_scope :refinery_user do
@@ -17,6 +9,15 @@ Refinery::Core::Engine.routes.draw do
       get "/#{Refinery::Core.backend_route}/users/register" => 'users#new', :as => :signup
       post "/#{Refinery::Core.backend_route}/users/register" => 'users#create', :as => :signup
     end
+
+    devise_for :refinery_user,
+               :class_name => 'Refinery::User',
+               :path => "#{Refinery::Core.backend_route}/users",
+               :controllers => { :registrations => 'refinery/users' },
+               :skip => [:registrations],
+               :path_names => { :sign_out => 'logout',
+                                :sign_in => 'login',
+                                :sign_up => 'register' }
   rescue RuntimeError => exc
     if exc.message =~ /ORM/
       # We don't want to complain on a fresh installation.
