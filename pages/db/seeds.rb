refinery/refinerycms

commit be6ff4acca0d0de5351587ab6707c0489495bff4

Refinery::I18n isn't necessarily available until 2.1

pages/db/seeds.rb
@@ -1,4 +1,4 @@
-Refinery::I18n.frontend_locales.each do |lang|
+(Refinery.i18n_enabled ? Refinery::I18n.frontend_locales : [:en]).each do |lang|
   I18n.locale = lang
 
   if Refinery::Page.where(:menu_match => "^/$").empty?

commit d3dcb330484718ba09126e3b0abd0d541bcbf65c

Fixes seeding bug introduced by me in be6ff4acca0d0de5351587ab6707c0489495bff4

pages/db/seeds.rb
@@ -1,4 +1,4 @@
-(Refinery.i18n_enabled ? Refinery::I18n.frontend_locales : [:en]).each do |lang|
+(Refinery.i18n_enabled? ? Refinery::I18n.frontend_locales : [:en]).each do |lang|
   I18n.locale = lang
 
   if Refinery::Page.where(:menu_match => "^/$").empty?