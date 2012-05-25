33878c5bd2fe9598e6579309f307587e0c0c496c
Uģis Ozols <ugis.ozolss@gmail.com>
2012-05-25 13:44:35 +0300

    Support multiple languages for seed data in extension and form generated seeds file.

--- core/lib/generators/refinery/engine/templates/db/seeds.rb
+++ core/lib/generators/refinery/engine/templates/db/seeds.rb
@@ -1,23 +1,27 @@
-if defined?(::Refinery::User)
-  ::Refinery::User.all.each do |user|
-    if user.plugins.where(:name => 'refinerycms-<%= namespacing.underscore %>').blank?
-      user.plugins.create(:name => 'refinerycms-<%= namespacing.underscore %>',
-                          :position => (user.plugins.maximum(:position) || -1) +1)
+Refinery::I18n.frontend_locales.each do |lang|
+  I18n.locale = lang
+  
+  if defined?(Refinery::User)
+    Refinery::User.all.each do |user|
+      if user.plugins.where(:name => 'refinerycms-<%= namespacing.underscore %>').blank?
+        user.plugins.create(:name => 'refinerycms-<%= namespacing.underscore %>',
+                            :position => (user.plugins.maximum(:position) || -1) +1)
+      end
     end
   end
-end
 
 <% unless skip_frontend? -%>
-url = "/<%= [(namespacing.underscore if namespacing.underscore != plural_name), plural_name].compact.join('/') %>"
-if defined?(::Refinery::Page) && ::Refinery::Page.where(:link_url => url).empty?
-  page = ::Refinery::Page.create(
-    :title => '<%= class_name.pluralize.underscore.titleize %>',
-    :link_url => url,
-    :deletable => false,
-    :menu_match => "^#{url}(\/|\/.+?|)$"
-  )
-  Refinery::Pages.default_parts.each_with_index do |default_page_part, index|
-    page.parts.create(:title => default_page_part, :body => nil, :position => index)
+  url = "/<%= [(namespacing.underscore if namespacing.underscore != plural_name), plural_name].compact.join('/') %>"
+  if defined?(Refinery::Page) && Refinery::Page.where(:link_url => url).empty?
+    page = Refinery::Page.create(
+      :title => '<%= class_name.pluralize.underscore.titleize %>',
+      :link_url => url,
+      :deletable => false,
+      :menu_match => "^#{url}(\/|\/.+?|)$"
+    )
+    Refinery::Pages.default_parts.each_with_index do |default_page_part, index|
+      page.parts.create(:title => default_page_part, :body => nil, :position => index)
+    end
   end
-end
 <% end -%>
+end
