67b949cb580a3d0041ceb2105827611a2ffcef35
Philip Arndt <parndt@gmail.com>
2012-05-26 02:02:12 +1200

    Added tests, found bugs, finished canonical support.

--- pages/app/models/refinery/page.rb
+++ pages/app/models/refinery/page.rb
@@ -163,7 +163,7 @@ module Refinery
     # Consists of:
     #   * The default locale's translated slug
     def canonical
-      with_locale_param url_marketable, ::Refinery::I18n.default_frontend_locale
+      Globalize.with_locale(::Refinery::I18n.default_frontend_locale){ url }
     end
 
     # Returns in cascading order: custom_slug or menu_title or title depending on
