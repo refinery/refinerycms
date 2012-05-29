aec30b8e1a6c2d98eb61f04b510e7582efd9af0d
ghoppe <ghoppe@gmail.com>
2012-05-28 18:18:00 -0600

    fix default template selected

--- pages/app/helpers/refinery/admin/pages_helper.rb
+++ pages/app/helpers/refinery/admin/pages_helper.rb
@@ -10,6 +10,20 @@ module Refinery
         ActiveRecord::Associations::Preloader.new(pages, :translations).run
         pages.map {|page| ["#{'-' * page.level} #{page.title}", page.id]}
       end
+      def selected_template(template_type, current_page)
+      	if current_page.send(template_type).nil? then
+      		if current_page.parent_id? then
+      			# Use Parent Template by default.
+      			current_page.parent.send(template_type)
+      		else
+      			# Use Default Template (First in whitelist)
+      			Refinery::Pages.send("#{template_type}_whitelist").first
+      		end
+      	else
+      		# Use Selected Template in Page
+      		current_page.send(template_type)
+      	end
+      end
     end
   end
 end
