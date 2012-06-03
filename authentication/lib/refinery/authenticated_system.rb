347b78407471848e504d2d3314b21990207b61c3
Alexander Wenzowski <alexander@wenzowski.com>
2012-06-03 15:51:04 -0400

    authentication redirection bugfix #1561 Fixed and added tests for post-authentication redirect bug where users would always be redirected to the admin root after successful auth.

--- authentication/lib/refinery/authenticated_system.rb
+++ authentication/lib/refinery/authenticated_system.rb
@@ -7,19 +7,47 @@ module Refinery
       session[:return_to] = request.fullpath.sub("//", "/")
     end
 
+    # Clear and return the stored location
+    def pop_stored_location
+      session.delete(:return_to)
+    end
+
     # Redirect to the URI stored by the most recent store_location call or
     # to the passed default.
     def redirect_back_or_default(default)
-      redirect_to(session[:return_to] || default)
-      session[:return_to] = nil
+      redirect_to(pop_stored_location || default)
+    end
+
+    # This defines the devise method for refinery routes
+    def signed_in_root_path(resource_or_scope)
+      scope = Devise::Mapping.find_scope!(resource_or_scope)
+      home_path = "#{scope}_root_path"
+      if respond_to?(home_path, true)
+        refinery.send(home_path)
+      elsif respond_to?(:admin_root_path)
+        refinery.admin_root_path
+      else
+        "/"
+      end
+    end
+
+    # Popps the stored url, trims the sneaky "//" from it, and returns it.
+    #
+    # Making sure bad urls aren't stored in the first place should probably be
+    # a part of the Devise::FailureApp
+    def sanitized_stored_location_for(resource_or_scope)
+      # `stored_location_for` is the devise method that popps the
+      # scoped `return_to` key
+      location = stored_location_for(resource_or_scope)
+      location.sub!("//", "/") if location.respond_to?(:sub!)
+      location
     end
 
     # This just defines the devise method for after sign in to support
     # extension namespace isolation...
     def after_sign_in_path_for(resource_or_scope)
-      scope = Devise::Mapping.find_scope!(resource_or_scope)
-      home_path = "#{scope}_root_path"
-      respond_to?(home_path, true) ? refinery.send(home_path) : refinery.admin_root_path
+      sanitized_stored_location_for(resource_or_scope) ||
+      signed_in_root_path(resource_or_scope)
     end
 
     def after_sign_out_path_for(resource_or_scope)
@@ -30,7 +58,8 @@ module Refinery
       refinery_user_signed_in? && current_refinery_user.has_role?(:refinery)
     end
 
-    protected :store_location, :redirect_back_or_default, :refinery_user?
+    protected :store_location, :pop_stored_location, :redirect_back_or_default,
+              :sanitized_stored_location_for, :refinery_user?
 
     def self.included(base)
       if base.respond_to? :helper_method
