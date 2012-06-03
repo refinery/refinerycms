47dde823dfa2920a71a0d4380a251f8c75fee6a9
Alexander Wenzowski <alexander@wenzowski.com>
2012-06-03 15:51:04 -0400

    authentication redirection improvement #1724 - add tests for #1561 behaviour - use unscoped session key to enable store_location to be used for login #1724

--- authentication/lib/refinery/authenticated_system.rb
+++ authentication/lib/refinery/authenticated_system.rb
@@ -7,19 +7,45 @@ module Refinery
       session[:return_to] = request.fullpath.sub("//", "/")
     end
 
+    # Clear and return the stored location
+    def pop_stored_location
+      session.delete(:return_to)
+    end
+
     # Redirect to the URI stored by the most recent store_location call or
     # to the passed default.
     def redirect_back_or_default(default)
-      redirect_to(session[:return_to] || default)
-      session[:return_to] = nil
+      redirect_to(pop_stored_location || default)
     end
 
-    # This just defines the devise method for after sign in to support
-    # extension namespace isolation...
-    def after_sign_in_path_for(resource_or_scope)
+    # Overrides default devise paths with refinery routes
+    def signed_in_root_path(resource_or_scope)
       scope = Devise::Mapping.find_scope!(resource_or_scope)
       home_path = "#{scope}_root_path"
-      respond_to?(home_path, true) ? refinery.send(home_path) : refinery.admin_root_path
+      if respond_to?(home_path, true)
+        refinery.send(home_path)
+      elsif respond_to?(:admin_root_path)
+        refinery.admin_root_path
+      else
+        "/"
+      end
+    end
+
+    # Trims the sneaky "//" from the popped stored url and returns it.
+    #
+    # Making sure bad urls aren't stored should probably be
+    # a part of the Devise::FailureApp
+    def sanitized_stored_location_for(resource_or_scope)
+      location = stored_location_for(resource_or_scope)
+      location.sub!("//", "/") if location.respond_to?(:sub!)
+      location
+    end
+
+    # Adds support for unscoped redirect_back_or_default key to devise default
+    def after_sign_in_path_for(resource_or_scope)
+      pop_stored_location ||
+      sanitized_stored_location_for(resource_or_scope) ||
+      signed_in_root_path(resource_or_scope)
     end
 
     def after_sign_out_path_for(resource_or_scope)
