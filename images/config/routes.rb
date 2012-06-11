f2661d03b8243cecdcf7c7c750f32b984685242d
Johan Frølich <johanfrolich@gmail.com>
2012-06-11 14:43:40 +0200

    did i just do that??

--- images/config/routes.rb
+++ images/config/routes.rb
@@ -2,9 +2,9 @@ Refinery::Core::Engine.routes.draw do
   
   match '/system/thumbs/:id(/:size)' => Dragonfly[:refinery_images].endpoint { |params, app|
     if params[:size].present? && Refinery::Images.user_image_sizes.keys.include?(params[:size])
-       app.fetch(Refinery::Image.find(params[:id]).image_uid).thumb(params[:size])
+      app.fetch(Refinery::Image.find(params[:id]).image_uid).thumb(params[:size])
     else
-       app.fetch(Refinery::Image.find(params[:id]).image_uid)
+      app.fetch(Refinery::Image.find(params[:id]).image_uid)
     end
   }, :as => 'thumbnail'
   
