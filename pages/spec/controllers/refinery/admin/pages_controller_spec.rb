6d850c976cc38c964843de157e117cef04ef238f
Philip Arndt <parndt@gmail.com>
2012-06-11 21:39:06 +1200

    All my one tests were passing.

--- pages/spec/controllers/refinery/admin/pages_controller_spec.rb
+++ pages/spec/controllers/refinery/admin/pages_controller_spec.rb
@@ -2,13 +2,13 @@ require "spec_helper"
 
 module Refinery
   module Admin
-    describe PagesController, :focus => true do
+    describe PagesController do
       login_refinery_superuser
 
       describe "#update_positions" do
         it "calls .rebuild! on Refinery::Page" do
           Refinery::Page.should_receive(:rebuild!).once
-        
+
           post :update_positions, :ul => []
         end
       end
