2b94065980b669a0f8e52a4e9df4efc3da9909ac
Philip Arndt <parndt@gmail.com>
2012-07-30 01:25:47 -0700

    Refactored code from Dialogs#show into utility methods.

--- core/app/controllers/refinery/admin/dialogs_controller.rb
+++ core/app/controllers/refinery/admin/dialogs_controller.rb
@@ -3,23 +3,13 @@ module ::Refinery
     class DialogsController < ::Refinery::AdminController
       TYPES = %w[image link]
 
-      before_filter :find_dialog_type, :only => [:show]
+      before_filter :find_dialog_type, :find_iframe_src, :only => [:show]
 
       def index
         redirect_to refinery.admin_root_path
       end
 
       def show
-        url_params = params.reject {|key, value|
-          /(action)|(controller)/ === key
-        }.merge(:id => nil)
-
-        @iframe_src = if @dialog_type == 'image'
-          refinery.insert_admin_images_path url_params.merge(:modal => true)
-        elsif @dialog_type == 'link'
-          refinery.link_to_admin_pages_dialogs_path url_params
-        end
-
         render :layout => false
       end
 
@@ -32,6 +22,21 @@ module ::Refinery
         error_404 if TYPES.exclude? params[:id].downcase
 
         @dialog_type = params[:id].downcase
+      end
+
+      def find_iframe_src
+        if @dialog_type == 'image'
+          @iframe_src = refinery.insert_admin_images_path(
+            url_params.merge(:modal => true)
+          )
+        elsif @dialog_type == 'link'
+          @iframe_src = refinery.link_to_admin_pages_dialogs_path url_params
+        end
+      end
+
+      def url_params
+        params.reject {|key, value| /(action)|(controller)/ === key}.
+               merge :id => nil
       end
 
     end
