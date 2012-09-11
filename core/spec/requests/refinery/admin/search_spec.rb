7121c045c76a8830d7eb29726a77f152af378582
Uģis Ozols <ugis.ozolss@gmail.com>
2012-09-11 08:33:56 +0300

    Add users extension to search specs. Make search specs shorter by introducing shared_examples.

--- core/spec/requests/refinery/admin/search_spec.rb
+++ core/spec/requests/refinery/admin/search_spec.rb
@@ -4,71 +4,75 @@ module Refinery
   describe "search" do
     refinery_login_with :refinery_user
 
-    context "when searched item exists" do
-      describe "image extension" do
-        let!(:image) { FactoryGirl.create(:image) }
-
-        it "returns found image" do
-          visit refinery.admin_images_path
-          fill_in "search", :with => "beach"
-          click_button "Search"
-
-          within ".actions" do
-            page.should have_selector("a[href$='#{image.image_name}']")
-          end
-        end
-      end
-
-      describe "resource extension" do
-        before { FactoryGirl.create(:resource) }
-
-        it "returns found resource" do
-          visit refinery.admin_resources_path
-          fill_in "search", :with => "refinery"
-          click_button "Search"
-          page.should have_content("Refinery Is Awesome.txt")
-        end
-      end
-
-      describe "page extension" do
-        before { FactoryGirl.create(:page, :title => "Ugis Ozols") }
-
-        it "returns found page" do
-          visit refinery.admin_pages_path
-          fill_in "search", :with => "ugis"
-          click_button "Search"
-          page.should have_content("Ugis Ozols")
-        end
-      end
-    end
-
-    context "when searched item don't exist" do
-      def shared_stuff
+    shared_examples "no result search" do
+      it "returns no results" do
         fill_in "search", :with => "yada yada"
         click_button "Search"
         page.should have_content("Sorry, no results found")
       end
+    end
 
-      describe "image extension" do
-        it "returns no results" do
-          visit refinery.admin_images_path
-          shared_stuff
+    describe "images extension" do
+      let!(:image) { FactoryGirl.create(:image) }
+      before do
+        visit refinery.admin_images_path
+      end
+
+      it "returns found image" do
+        fill_in "search", :with => "beach"
+        click_button "Search"
+
+        within ".actions" do
+          page.should have_selector("a[href$='#{image.image_name}']")
         end
       end
 
-      describe "resource extension" do
-        it "returns no results" do
-          visit refinery.admin_resources_path
-          shared_stuff
-        end
+      it_behaves_like "no result search"
+    end
+
+    describe "resources extension" do
+      before do
+        FactoryGirl.create(:resource)
+        visit refinery.admin_resources_path
       end
 
-      describe "page extension" do
-        it "returns no results" do
-          visit refinery.admin_pages_path
-          shared_stuff
-        end
+      it "returns found resource" do
+        fill_in "search", :with => "refinery"
+        click_button "Search"
+        page.should have_content("Refinery Is Awesome.txt")
       end
+
+      it_behaves_like "no result search"
+    end
+
+    describe "pages extension" do
+      before do
+        FactoryGirl.create(:page, :title => "Ugis Ozols")
+        visit refinery.admin_pages_path
+      end
+
+      it "returns found page" do
+        fill_in "search", :with => "ugis"
+        click_button "Search"
+        page.should have_content("Ugis Ozols")
+      end
+
+      it_behaves_like "no result search"
+    end
+
+    describe "users extension" do
+      before do
+        FactoryGirl.create(:user, :username => "ugis")
+        visit refinery.admin_users_path
+      end
+
+      it "returns found user" do
+        fill_in "search", :with => "ugis"
+        click_button "Search"
+        page.should have_content("ugis")
+      end
+
+      it_behaves_like "no result search"
     end
   end
 end
