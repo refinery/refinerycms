2889b7cddd1edb3ca2ac16b9feb83b678cb9fec3
Uģis Ozols <ugis.ozolss@gmail.com>
2012-09-27 22:01:05 +0300

    Fix recent inquiries not showing up in the dashboard.

--- dashboard/app/controllers/refinery/admin/dashboard_controller.rb
+++ dashboard/app/controllers/refinery/admin/dashboard_controller.rb
@@ -22,8 +22,8 @@ module Refinery
         @recent_activity = @recent_activity.sort_by(&:updated_at).reverse.
                            first(Refinery::Dashboard.activity_show_limit)
 
-        @recent_inquiries = if Refinery::Plugins.active.find_by_name("refinery_inquiries")
-          Inquiry.latest(Refinery::Dashboard.activity_show_limit)
+        @recent_inquiries = if Refinery::Plugins.active.find_by_name("refinerycms_inquiries")
+          Refinery::Inquiries::Inquiry.latest(Refinery::Dashboard.activity_show_limit)
         else
           []
         end
