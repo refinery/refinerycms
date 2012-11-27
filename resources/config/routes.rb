a31e5897926624712f23fd58c0867ab392f40eb9
Josef Šimánek <retro@ballgag.cz>
2012-11-27 15:25:58 +0100

    Trivial custom backend route config.
    Fixes #2050

--- resources/config/routes.rb
+++ resources/config/routes.rb
@@ -1,7 +1,7 @@
 Refinery::Core::Engine.routes.draw do
   get '/system/resources/*dragonfly', :to => Dragonfly[:refinery_resources]
 
-  namespace :admin, :path => 'refinery' do
+  namespace :admin, :path => Refinery::Core.backend_route do
     resources :resources, :except => :show do
       get :insert, :on => :collection
     end
