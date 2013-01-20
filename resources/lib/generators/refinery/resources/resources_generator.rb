454eaacb9b0c1bd840d537e9efbfaaf8c0682743
Philip Arndt <parndt@gmail.com>
2013-01-21 09:23:01 +1300

    Skip migrations when testing generators.
    Closes #2154

--- resources/lib/generators/refinery/resources/resources_generator.rb
+++ resources/lib/generators/refinery/resources/resources_generator.rb
@@ -1,9 +1,12 @@
 module Refinery
   class ResourcesGenerator < Rails::Generators::Base
+    class_option :skip_migrations, :type => :boolean, :default => false, :aliases => nil, :group => :runtime,
+                           :desc => "Skip over installing or running migrations."
+
     source_root File.expand_path('../templates', __FILE__)
 
     def rake_db
-      rake "refinery_resources:install:migrations"
+      rake "refinery_resources:install:migrations" unless self.options[:skip_migrations]
     end
 
     def generate_resources_initializer
