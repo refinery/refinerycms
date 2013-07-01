e69a310d0bb2ee2600ce323b881ce1133224ef9e
David Chen <darkbaby123@gmail.com>
2013-07-01 17:50:34 +0800

    Let 404 page can use "render_with_templates?" method

--- pages/lib/refinery/pages/instance_methods.rb
+++ pages/lib/refinery/pages/instance_methods.rb
@@ -10,7 +10,7 @@ module Refinery
       def error_404(exception=nil)
         if (@page = ::Refinery::Page.where(:menu_match => "^/404$").includes(:parts).first).present?
           # render the application's custom 404 page with layout and meta.
-          if self.respond_to? :render_with_templates?
+          if self.respond_to? :render_with_templates?, true
             render_with_templates? @page, :status => 404
           else
             render :template => '/refinery/pages/show', :formats => [:html], :status => 404
