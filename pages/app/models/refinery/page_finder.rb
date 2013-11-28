82309fb22dd97a977db26f899c3e7169a7c72f93
Amanda Wagener <amanda@awagener.com>
2013-11-28 01:34:40 +0100

    Remove comments duplicated from Page.

--- pages/app/models/refinery/page_finder.rb
+++ pages/app/models/refinery/page_finder.rb
@@ -1,34 +1,21 @@
 module Refinery
   class PageFinder
-    # Find page by path, checking for scoping rules
     def self.find_by_path(path)
       PageFinderByPath.new(path).find
     end
-    
-    # Helps to resolve the situation where you have a path and an id
-    # and if the path is unfriendly then a different finder method is required
-    # than find_by_path.
+
     def self.find_by_path_or_id(path, id)
       PageFinderByPathOrId.new(path, id).find
     end
 
-    # Finds pages by their title.  This method is necessary because pages
-    # are translated which means the title attribute does not exist on the
-    # pages table thus requiring us to find the attribute on the translations table
-    # and then join to the pages table again to return the associated record.
     def self.by_title(title)
       PageFinderByTitle.new(title).find
     end
 
-    # Finds pages by their slug.  This method is necessary because pages
-    # are translated which means the slug attribute does not exist on the
-    # pages table thus requiring us to find the attribute on the translations table
-    # and then join to the pages table again to return the associated record.
     def self.by_slug(slug, conditions={})
       PageFinderBySlug.new(slug, conditions).find
     end
-    
-    # Wrap up the logic of finding the pages based on the translations table.
+
     def self.with_globalize(conditions = {})
       PageFinder.new.with_globalize(conditions)
     end
