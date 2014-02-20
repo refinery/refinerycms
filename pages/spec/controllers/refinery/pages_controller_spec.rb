e6ee29e3aadea45466f65dbac9eaa50829efef44
John Iacona <john.iacona@worldone.com>
2014-02-20 14:12:51 -0500

    Added specs for multisite functionality

--- pages/spec/controllers/refinery/pages_controller_spec.rb
+++ pages/spec/controllers/refinery/pages_controller_spec.rb
@@ -2,13 +2,10 @@ require "spec_helper"
 
 module Refinery
   describe PagesController do
-    before do
-      FactoryGirl.create(:page, :link_url => "/")
-      FactoryGirl.create(:page, :title => "test")
-    end
-
     describe "#home" do
       it "renders home template" do
+        create(:page, :link_url => "/")
+
         get :home
         expect(response).to render_template("home")
       end
@@ -16,8 +13,32 @@ module Refinery
 
     describe "#show" do
       it "renders show template" do
+        create(:page, :title => 'test')
+
         get :show, :path => "test"
         expect(response).to render_template("show")
+      end
+    end
+
+    context "multisite page selection" do
+      let(:pages) { create_list(:page, 2, :site_specific, :link_url => "/") }
+
+      describe "#home" do
+        it "populates site specific @page variable" do
+          request.host = pages.last.site.hostname
+
+          get :home
+          expect(assigns(:page)).to eq pages.last
+        end
+      end
+
+      describe "#show" do
+        it "populates site specific @page variable" do
+          request.host = pages.last.site.hostname
+
+          get :show, :path => pages.last.slug
+          expect(assigns(:page)).to eq pages.last
+        end
       end
     end
   end
