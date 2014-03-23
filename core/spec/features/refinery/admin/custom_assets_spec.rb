460522e30ef148e83aa88425e487c2d5cc07d7e0
Philip Arndt <p@arndt.io>
2014-03-23 09:18:54 -0700

    Tidy up asset usage/registration in specs to reveal a hidden pagination failure in xhr_paging_spec.rb

--- core/spec/features/refinery/admin/custom_assets_spec.rb
+++ core/spec/features/refinery/admin/custom_assets_spec.rb
@@ -3,12 +3,12 @@ require "spec_helper"
 module Refinery
   describe "custom" do
     refinery_login_with :refinery_user
+    after do
+      Refinery::Core.javascripts.reject! {|j| %w[custom_js].include?(j) }
+      Refinery::Core.stylesheets.reject! {|s| %w[custom_css].include?(s.path) }
+    end
 
     context "javascripts" do
-      before do
-        ::Refinery::Core.clear_javascripts!
-      end
-
       it "should be rendered when specified" do
         ::Refinery::Core.config.register_javascript('custom_js')
         visit refinery.admin_dashboard_path
@@ -17,10 +17,6 @@ module Refinery
     end
 
     context "stylesheets" do
-      before do
-        ::Refinery::Core.clear_stylesheets!
-      end
-
       it "should be rendered when specified" do
         ::Refinery::Core.config.register_stylesheet('custom_css')
         visit refinery.admin_dashboard_path
