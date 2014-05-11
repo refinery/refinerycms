6a59f4b772012339b6c911405b3e4e013fa54137
Amanda Wagener <amanda@awagener.com>
2014-05-11 23:20:20 +1200

    Introduce ThumbnailDimensions model to simply Image.

--- images/app/models/refinery/image.rb
+++ images/app/models/refinery/image.rb
@@ -44,51 +44,8 @@ module Refinery
 
     # Intelligently works out dimensions for a thumbnail of this image based on the Dragonfly geometry string.
     def thumbnail_dimensions(geometry)
-      geometry = if geometry.is_a?(Symbol) && Refinery::Images.user_image_sizes.keys.include?(geometry)
-        Refinery::Images.user_image_sizes[geometry]
-      else
-        geometry.to_s
-      end
-
-      width = original_width = self.image.width.to_f
-      height = original_height = self.image.height.to_f
-      geometry_width, geometry_height = geometry.split(%r{\#{1,2}|\+|>|!|x}im)[0..1].map(&:to_f)
-      thumb_geometry = Regexp.union(::Dragonfly::ImageMagick::Processors::Thumb::RESIZE_GEOMETRY,
-                                    ::Dragonfly::ImageMagick::Processors::Thumb::CROPPED_RESIZE_GEOMETRY,
-                                    ::Dragonfly::ImageMagick::Processors::Thumb::CROP_GEOMETRY)
-      if (original_width * original_height > 0) && thumb_geometry === geometry
-        if ::Dragonfly::ImageMagick::Processors::Thumb::RESIZE_GEOMETRY === geometry
-          if geometry !~ %r{\d+x\d+>} || (%r{\d+x\d+>} === geometry && (width > geometry_width.to_f || height > geometry_height.to_f))
-            # Try scaling with width factor first. (wf = width factor)
-            wf_width = (original_width * geometry_width / width).round
-            wf_height = (original_height * geometry_width / width).round
-
-            # Scale with height factor (hf = height factor)
-            hf_width = (original_width * geometry_height / height).round
-            hf_height = (original_height * geometry_height / height).round
-
-            # Take the highest value that doesn't exceed either axis limit.
-            use_wf = wf_width <= geometry_width && wf_height <= geometry_height
-            if use_wf && hf_width <= geometry_width && hf_height <= geometry_height
-              use_wf = wf_width * wf_height > hf_width * hf_height
-            end
-
-            if use_wf
-              width = wf_width
-              height = wf_height
-            else
-              width = hf_width
-              height = hf_height
-            end
-          end
-        else
-          # cropping
-          width = geometry_width
-          height = geometry_height
-        end
-      end
-
-      { :width => width.to_i, :height => height.to_i }
+      dimensions = ThumbnailDimensions.new(geometry, image.width, image.height)
+      { :width => dimensions.width, :height => dimensions.height }
     end
 
     # Returns a titleized version of the filename
