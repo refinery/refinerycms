4e545612e4700db3846a420b196acf48d7c355ff
Anita Graham <anita@joli.com.au>
2014-06-06 11:55:28 +0800

    Allow page parts to use customised presenters
    A page part will look for a named presenter and named data.
    If found it will use the presenter to present the data.
    
    Collection_presenter is a new presenter which will present any
    collection as an unordered list.

--- pages/app/presenters/refinery/pages/content_page_presenter.rb
+++ pages/app/presenters/refinery/pages/content_page_presenter.rb
@@ -8,7 +8,7 @@ module Refinery
       def initialize(page, page_title)
         super()
         add_default_title_section(page_title) if page_title.present? && Pages.show_title_in_body
-        add_page_parts(page.parts) if page
+        add_page_parts(page) if page
         add_default_post_page_sections
       end
 
@@ -23,10 +23,33 @@ module Refinery
         add_section_if_missing(:id => :side_body)
       end
 
-      def add_page_parts(parts)
-        parts.each do |part|
-          add_section PagePartSectionPresenter.new(part)
+      def add_page_parts(page)
+        page.parts.each do |part|
+
+          # find an instance variable or attribute named for this page part
+          section_data = find_data(page, part.title.gsub(/\s+/,""))
+
+          # find a presenter for this attribute/collection?
+          section_presenter = find_presenter("Refinery::Pages::#{part.title.capitalize.gsub(/\s+/, "")}SectionPresenter")
+
+          if section_presenter.nil? || section_data.nil?
+            # if either presenter/special data is absent default to generic presenter and original part
+            section_presenter = PagePartSectionPresenter
+            section_data = part
+          else
+            # augment the data to make it more like a page part
+            section_data  = {id: part.title, data:section_data } unless section_data.nil?
+          end
+          add_section section_presenter.new(section_data) unless section_data.nil?
         end
+      end
+
+      def find_presenter(presenter_name)
+        presenter_name.constantize if Object.const_defined?(presenter_name)
+      end
+
+      def find_data(page, part_name)
+        data = page.respond_to?(part_name) ? page.send(part_name) : instance_variable_get('@'+part_name)
       end
     end
   end
