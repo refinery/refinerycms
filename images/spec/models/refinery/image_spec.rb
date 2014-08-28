179f803c534fe03ddf687b01cac3d2d666c76a60
Philip Arndt <p@arndt.io>
2014-08-28 17:44:10 +0200

    Handle change in Dragonfly where verify_urls is on by default.
    This provides better protection from DOS attacks and is probably a Good Thing.

--- images/spec/models/refinery/image_spec.rb
+++ images/spec/models/refinery/image_spec.rb
@@ -70,7 +70,7 @@ module Refinery
       end
 
       it "contains its filename at the end" do
-        expect(created_image.url.split('/').last).to eq(created_image.image_name)
+        expect(created_image.url.split('/').last).to match(/\A#{created_image.image_name}/)
       end
 
       it "becomes different when supplying geometry" do
