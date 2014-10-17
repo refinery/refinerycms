6be31330932e86030b1913b3c731bb0b7382fd44
Anita Graham <anita@joli.com.au>
2014-10-17 20:29:44 +0800

    Add named paths for pages to workaround rails #16058

--- pages/config/routes.rb
+++ pages/config/routes.rb
@@ -1,32 +1,32 @@
 Refinery::Core::Engine.routes.draw do
-  root :to => 'pages#home', :via => :get
-  get '/pages/:id', :to => 'pages#show', :as => :page
+  root to: 'pages#home', via: :get
+  get '/pages/:id', to: 'pages#show', as: :page
 
-  namespace :pages, :path => '' do
-    namespace :admin, :path => Refinery::Core.backend_route do
-      scope :path => :pages do
-        post 'preview', :to => 'preview#show', :as => :preview_pages
-        patch 'preview/*path', :to => 'preview#show', :as => :preview_page
+  namespace :pages, path: '' do
+    namespace :admin, path: Refinery::Core.backend_route do
+      scope path: :pages do
+        post 'preview', to: 'preview#show', as: :preview_pages
+        patch 'preview/*path', to: 'preview#show', as: :preview_page
       end
     end
   end
 
-  namespace :admin, :path => Refinery::Core.backend_route do
-    get 'pages/*path/edit', :to => 'pages#edit'
-    get 'pages/*path/children', :to => 'pages#children', :as => 'children_pages'
-    patch 'pages/*path', :to => 'pages#update'
-    delete 'pages/*path', :to => 'pages#destroy'
+  namespace :admin, path: Refinery::Core.backend_route do
+    get 'pages/*path/edit', to: 'pages#edit', as: 'edit_page'
+    get 'pages/*path/children', to: 'pages#children', as: 'children_pages'
+    patch 'pages/*path', to: 'pages#update', as: 'update_page'
+    delete 'pages/*path', to: 'pages#destroy', as: 'delete_page'
 
-    resources :pages, :except => :show do
-      post :update_positions, :on => :collection
+    resources :pages, except: :show do
+      post :update_positions, on: :collection
     end
 
-    resources :pages_dialogs, :only => [] do
+    resources :pages_dialogs, only: [] do
       collection do
         get :link_to
       end
     end
 
-    resources :page_parts, :only => [:new, :create, :destroy]
+    resources :page_parts, only: [:new, :create, :destroy]
   end
 end
