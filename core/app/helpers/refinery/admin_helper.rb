f0198d3e35fb0632bfdd7a62fb4a919e71e71640
Arthur CHAN <arthur@talkgoal.com>
2014-12-10 23:14:40 +0800

    dialect overflow e.g. zh-TW

--- core/app/helpers/refinery/admin_helper.rb
+++ core/app/helpers/refinery/admin_helper.rb
@@ -5,6 +5,11 @@ module Refinery
       # TODO: move current_admin_locale to Refinery::I18n
       ::I18n.locale
     end
+    
+    def truncate_locale(locale)
+      locale =~ /\-/ ? locale.upcase.to_s.split('-')[1] : locale.upcase
+    
+    end
 
   end
 end
