065c5f4ecc9daeb1e7e51cf9b6b008b7eb14bc6b
Philip Arndt <git@p.arndt.io>
2014-12-13 09:57:24 +1300

    Refactored image_fu helper to only use geometry when not nil.
    Conflicts:
    	core/app/helpers/refinery/image_helper.rb

--- core/app/helpers/refinery/image_helper.rb
+++ core/app/helpers/refinery/image_helper.rb
@@ -20,15 +20,16 @@ module Refinery
     # Say for example that we had a @model.image (@model having a belongs_to :image relationship)
     # and we wanted to display a thumbnail cropped to 200x200 then we can use image_fu like this:
     # <%= image_fu @model.image, '200x200' %> or with no thumbnail: <%= image_fu @model.image %>
-    def image_fu(image, geometry = nil, options={})
-      if image.present?
-        dimensions = (image.thumbnail_dimensions(geometry) rescue {})
+    def image_fu(image, geometry = nil, options = {})
+      return nil if image.blank?
 
-        image_tag(image.thumbnail(:geometry => geometry,
-                                  :strip => options[:strip]).url, {
-          :alt => image.respond_to?(:title) ? image.title : image.image_name,
-        }.merge(dimensions).merge(options))
-      end
+      thumbnail_args = options.slice(:strip)
+      thumbnail_args[:geometry] = geometry if geometry
+
+      image_tag_args = (image.thumbnail_dimensions(geometry) rescue {})
+      image_tag_args[:alt] = image.respond_to?(:title) ? image.title : image.image_name
+
+      image_tag(image.thumbnail(thumbnail_args).url, image_tag_args.merge(options))
     end
   end
 end
