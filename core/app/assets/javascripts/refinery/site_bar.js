f0bfab3d00dc920db97197051139ffadeac5bf46
Brice Sanchez <contact@brice-sanchez.com>
2015-01-20 01:08:41 -0500

    Improve site bar CSS to be responsive without CSS framework

--- core/app/assets/javascripts/refinery/site_bar.js
+++ core/app/assets/javascripts/refinery/site_bar.js
@@ -1,8 +1,3 @@
 $(function() {
-  $logo = $('#site_bar_content #site_bar_refinery_cms_logo');
-  $logo.css('left', ($('#site_bar_content').width() / 2) - ($logo.width() / 2));
-
   $switch_anchor = $('#editor_switch a').not('.ie7 #editor_switch a, .ie6 #editor_switch a');
-
-  $('#editor_switch a').appendTo($('<span></span>').prependTo('#editor_switch'));
 });
