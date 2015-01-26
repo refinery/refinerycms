ee60a927ed24fbd3be282201931b09dca0daa65c
Philip Arndt <git@p.arndt.io>
2015-01-27 10:29:50 +1300

    Tidied up the edge installation template.

--- templates/refinery/edge.rb
+++ templates/refinery/edge.rb
@@ -1,8 +1,11 @@
-require 'rbconfig'
-
 # We want to ensure that you have an ExecJS runtime available!
 begin
   require 'execjs'
+  begin
+    ::ExecJS::Runtimes.autodetect
+  rescue
+    gsub_file 'Gemfile', "# gem 'therubyracer'", "gem 'therubyracer'"
+  end
 rescue LoadError
   abort "ExecJS is not installed. Please re-start the installer after running:\ngem install execjs"
 end
@@ -13,27 +16,16 @@ end
 
 append_file 'Gemfile' do
 "
-gem 'refinerycms', git: 'https://github.com/refinery/refinerycms', branch: 'master'
+gem 'refinerycms', git: 'https://github.com/refinery/refinerycms', branch: 'template_test'
 
 gem 'quiet_assets', :group => :development
 
-# Add support for refinerycms-acts-as-indexed
+# Add support for searching inside Refinery's admin interface.
 gem 'refinerycms-acts-as-indexed', ['~> 2.0', '>= 2.0.0']
 
-# Add support for refinerycms-wymeditor
+# Add support for Refinery's custom fork of the visual editor WYMeditor.
 gem 'refinerycms-wymeditor', ['~> 1.0', '>= 1.0.6']
-
-gem 'seo_meta', git: 'https://github.com/parndt/seo_meta', branch: 'master'
-
-gem 'paper_trail', git: 'https://github.com/airblade/paper_trail', branch: 'master'
 "
-end
-
-begin
-  require 'execjs'
-  ::ExecJS::Runtimes.autodetect
-rescue
-  gsub_file 'Gemfile', "# gem 'therubyracer'", "gem 'therubyracer'"
 end
 
 run 'bundle install'
