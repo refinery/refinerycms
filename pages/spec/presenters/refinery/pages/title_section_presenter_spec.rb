8422040fea7821da2b506865fe6fc18c39be4c9e
Josef Šimánek <josef.simanek@gmail.com>
2015-02-15 15:21:40 +0100

    Assert html as xml in page presenter specs.

--- pages/spec/presenters/refinery/pages/title_section_presenter_spec.rb
+++ pages/spec/presenters/refinery/pages/title_section_presenter_spec.rb
@@ -7,13 +7,13 @@ module Refinery
         it "wraps a title section in a title element" do
           section = TitleSectionPresenter.new(:fallback_html => 'foobar')
           expect(section.has_content?(true)).to be_truthy
-          expect(section.wrapped_html(true)).to eq('<h1>foobar</h1>')
+          expect(section.wrapped_html(true)).to xml_eq('<h1>foobar</h1>')
         end
 
         it "will use the specified id" do
           section = TitleSectionPresenter.new(:fallback_html => 'foobar', :id => 'mynode')
           expect(section.has_content?(true)).to be_truthy
-          expect(section.wrapped_html(true)).to eq('<h1 id="mynode">foobar</h1>')
+          expect(section.wrapped_html(true)).to xml_eq('<h1 id="mynode">foobar</h1>')
         end
       end
     end
