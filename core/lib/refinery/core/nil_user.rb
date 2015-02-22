a6a41ce46944aa049b296bdf8795a3608ce17280
Philip Arndt <git@p.arndt.io>
2015-02-22 18:38:54 +1300

    Introduced API: user.has_plugin?(name) to 'replace' Refinery::Plugins.active singleton.

--- core/lib/refinery/core/nil_user.rb
+++ core/lib/refinery/core/nil_user.rb
@@ -4,20 +4,28 @@ require 'refinery/plugins'
 module Refinery
   module Core
     class NilUser < Zilch::Authorisation::NilUser
-      # The default NilUser has all roles and can access all plugins. Should be
-      # overridden in authentication solutions.
+      # The default NilUser has all roles and can access all plugins.
+      # Should be overridden in authentication solutions.
       def has_role?(role)
         true
       end
 
-      # Returns all plugins in system. Should be overridden in
-      # authentication solutions.
+      # Returns all registered plugins.
+      # Should be overridden in authentication solutions.
       def plugins
         Refinery::Plugins.registered
       end
 
+      # Returns true.
+      # Should be overridden in authentication solutions.
+      # A real solution might be: `Refinery::Plugins.active.names.include?(name)`
+      def has_plugin?(name)
+        true
+      end
+
       # Returns a URL to the first plugin with a URL in the menu. Used for
       # admin users' root admin url.
+      # Should be overridden in authentication solutions.
       def landing_url
         plugins.first_url_in_menu
       end
