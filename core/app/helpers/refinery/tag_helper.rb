9daa0b30ed64e59028cecaac8705a81e15f15724
Anita Graham <anita@joli.com.au>
2015-02-26 15:29:20 +0800

    Stage one of using icon font font-awesome
    Basic CSS changes to use icons.
    Icon colours and sizes not changed (except for accept and close)
    Replaced icons in views
    Add colours. Class-ify page actions
    Change link_to to action_icon or action_label
    Add fonts to vendor
    Use SCSS partials and @import them.
    Use action_label for view switching

--- core/app/helpers/refinery/tag_helper.rb
+++ core/app/helpers/refinery/tag_helper.rb
@@ -1,12 +1,9 @@
 module Refinery
   module TagHelper
 
-    # Returns <img class='help' tooltip='Your Input' src='refinery/icons/information.png' />
     # Remember to wrap your block with <span class='label_with_help'></span> if you're using a label next to the help tag.
-    def refinery_help_tag(title = '')
-      title = h(title) unless title.html_safe?
-
-      refinery_icon_tag('information', :class => 'help', :tooltip => title)
+    def refinery_help_tag(title='Tip')
+      action_icon(:info, '#', title.html_safe? ? title : h(title), {tooltip: title})
     end
 
     # This is just a quick wrapper to render an image tag that lives inside refinery/icons.
@@ -14,6 +11,38 @@ module Refinery
     def refinery_icon_tag(filename, options = {})
       filename = "#{filename}.png" unless filename.split('.').many?
       image_tag "refinery/icons/#{filename}", {:width => 16, :height => 16}.merge(options)
+    end
+
+    def action_icon(action, url, title, options={})
+      action_icon_label(action, url, title, options, false)
+    end
+
+    def action_label(action, url, title, options={})
+      action_icon_label(action, url, title, options, true)
+    end
+
+    def action_icon_label(action, url, title, options={}, label = true)
+#   See icons.scss for defined icons/classes
+      options[:title] = title
+      options[:class].presence ? options[:class] << " #{action}_icon" : options[:class] = " #{action}_icon"
+
+      case action
+      when :preview
+        options[:target] = '_blank'
+      when :delete
+        options[:method] = :delete
+        options[:class] << ' cancel confirm-delete'
+      when :reorder_done
+        options[:class] << ' hidden'
+      end
+      link_to(label && title || '', url, options)
+    end
+
+    # this stacks the text onto the locale icon (actually a comment balloon)
+    def locale_text_icon(text)
+      content_tag(:span, class: 'fa-stack') do
+        content_tag(:i, '', class: 'fa fa-comment') << content_tag(:strong, text)
+      end
     end
 
   end
