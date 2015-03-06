36964d2b4438a449df6a26a372031eca537c5b49
Chris Yate <chrisyate@gmail.com>
2015-03-06 22:14:53 +0000

    Modify the generator to have include_spam option
    - Add option to include spam checking
    - slight changes to the generator options in .erbs
    - option switched by "--include-spam"
    - Currently not properly functional
    
    Conflicts:
    	core/lib/generators/refinery/form/templates/spec/features/refinery/namespace/admin/plural_name_spec.rb.erb

--- core/lib/generators/refinery/form/form_generator.rb
+++ core/lib/generators/refinery/form/form_generator.rb
@@ -7,6 +7,16 @@ module Refinery
 
     include Refinery::ExtensionGeneration
 
+    class_option :include_spam,
+      :desc => 'Generate extension with spam filtering',
+      :type => :boolean,
+      :default => false,
+      :required => false
+
+    def include_spam?
+      options[:include_spam]
+    end
+
     def description
       "Generates an extension which is set up for frontend form submissions like a contact page."
     end
