648190ddddcf3dbb4220b999456070441cbf54e8
Anita Graham <anita@joli.com.au>
2015-03-11 19:11:06 +0800

    Ensure fonts served from assets pipeline
    1. add fonts to precompiled assets
    2. put fonts in correct directories
    3. set font-path

---
+++ core/config/initializers/assets.rb
@@ -0,0 +1,1 @@
+Rails.application.config.assets.precompile += %w( font-awesome/fontawesome-webfont.eot font-awesome/fontawesome-webfont.woff font-awesome/fontawesome-webfont.ttf font-awesome/fontawesome-webfont.svg)
