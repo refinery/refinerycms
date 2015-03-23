b05e012d12a7b5ddddf83c515517253044689c0c
Anita Graham <anita@joli.com.au>
2015-03-23 12:05:29 +0800

    Use full iconset
    1. Use full iconset
    2. Hover effect on icons changed to size+

--- core/config/initializers/assets.rb
+++ core/config/initializers/assets.rb
@@ -1,1 +1,6 @@
-Rails.application.config.assets.precompile += %w( font-awesome/fontawesome-webfont.eot font-awesome/fontawesome-webfont.woff font-awesome/fontawesome-webfont.ttf font-awesome/fontawesome-webfont.svg)
+Rails.application.config.assets.precompile += %w(
+  font-awesome/fontawesome-webfont.eot
+  font-awesome/fontawesome-webfont.woff2
+  font-awesome/fontawesome-webfont.woff
+  font-awesome/fontawesome-webfont.ttf
+  font-awesome/fontawesome-webfont.svg)
