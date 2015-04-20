fcd4411cd6bd693d29cfb62ca0dc0703481cb136
Johan Bruning <johan@taquito.nl>
2015-04-20 15:04:31 +0200

    Make Refinery's FormHelper extension required_label behave the same as label for i18n

--- core/lib/refinery/ext/action_view/helpers/form_helper.rb
+++ core/lib/refinery/ext/action_view/helpers/form_helper.rb
@@ -3,11 +3,19 @@ ActionView::Helpers::FormHelper.module_eval do
   def required_label(object_name, method, options = {})
     options = {:class => "required"}.merge!(options)
 
-    label(object_name, method, "#{label_humanize_text(method, options)} *", options)
+    label(object_name, method, "#{label_humanize_text(object_name, method, options)} *", options)
   end
 
-  def label_humanize_text(method, options = {})
+  def label_humanize_text(object_name, method, options = {})
     object = options[:object]
+
+    if object.respond_to?(:to_model)
+      key = object.class.model_name.i18n_key
+      i18n_default = ["#{key}.#{method}".to_sym, ""]
+    end
+
+    i18n_default ||= ""
+    content = I18n.t("#{object_name}.#{method}", :default => i18n_default, :scope => "helpers.label").presence
 
     content ||= if object && object.class.respond_to?(:human_attribute_name)
       object.class.human_attribute_name(method)
