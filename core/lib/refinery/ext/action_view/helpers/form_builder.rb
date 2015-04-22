b49e86b84542762dcc90b800ce85345d12baa4a2
Johan Bruning <johan@taquito.nl>
2015-04-22 09:23:02 +0200

    Remove action_view form extensions that provide trivial functionality and don't adhere to Rails default i18n behavior.
    Discussed here: https://github.com/refinery/refinerycms/pull/2954#issuecomment-94977561

--- core/lib/refinery/ext/action_view/helpers/form_builder.rb
+++
@@ -1,9 +0,0 @@
-require 'action_view/helpers'
-
-ActionView::Helpers::FormBuilder.module_eval do
-
-  def required_label(method, options = {})
-    @template.required_label(@object_name, method, objectify_options(options))
-  end
-
-end
