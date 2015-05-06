9b0ecf4f270c40d2abaa8381e02f6f9990ad4930
Philip Arndt <git@p.arndt.io>
2015-05-06 18:36:40 +1200

    Reduced line length and made logic a little easier to follow.

--- pages/app/controllers/refinery/pages_controller.rb
+++ pages/app/controllers/refinery/pages_controller.rb
@@ -53,7 +53,10 @@ module Refinery
     end
 
     def should_redirect_to_friendly_url?
-      requested_friendly_id != page.friendly_id || ::Refinery::Pages.scope_slug_by_parent && params[:path].present? && params[:path].match(page.root.slug).nil?
+      requested_friendly_id != page.friendly_id || (
+        ::Refinery::Pages.scope_slug_by_parent &&
+        params[:path].present? && params[:path].match(page.root.slug).nil?
+      )
     end
 
     def current_user_can_view_page?
