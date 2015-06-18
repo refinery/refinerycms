9447576e2feebbcfd9b6606003dff8deb358523a
Stefan <stefan@glasscanvascreative.com>
2015-06-18 12:38:52 -0700

    CSS fixes & updates

--- core/app/assets/javascripts/glass/modules/forms.js
+++ core/app/assets/javascripts/glass/modules/forms.js
@@ -509,7 +509,7 @@ var CanvasForms = (function ($) {
       // Add a semantic modal to the body of the page.
       $('body').append([
         '<div id="delete-confirm-modal" class="ui basic modal">',
-        '<i class="close icon"></i>',
+        '<i class="gcicon gcicon-close close"></i>',
         '<div class="header">',
         'Confirm Deletion',
         '</div>',
