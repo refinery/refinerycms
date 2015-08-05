a08908e26d03d8005668ce5f2adbf94dde26effe
Joseph Krump <krump.joseph@gmail.com>
2015-08-05 15:25:32 -0700

    icons set for datetimepickers

--- core/app/assets/javascripts/glass/modules/datetime.js
+++ core/app/assets/javascripts/glass/modules/datetime.js
@@ -4,24 +4,40 @@
 
 var GlassDateTime = (function ($) {
   $(document).on('content-ready', function (e, element) {
+    var icons = {
+      time: 'icon icon-clock',
+      date: 'icon icon-calendar',
+      up: 'icon icon-up',
+      down: 'icon icon-down',
+      previous: 'icon icon-angle-left',
+      next: 'icon icon-angle-right',
+      today: 'icon icon-crosshair',
+      clear: 'icon icon-trash',
+      close: 'icon icon-cancel'
+    };
+
     $('.datetimepicker.default').datetimepicker({
       useCurrent: true,
-      defaultDate: new Date()
+      defaultDate: new Date(),
+      icons: icons
     });
     $('.datetimepicker.side-by-side').datetimepicker({
       sideBySide: true,
       useCurrent: true,
-      defaultDate: new Date()
+      defaultDate: new Date(),
+      icons: icons
     });
     $('.datetimepicker.time-only').datetimepicker({
       useCurrent: true,
       format: 'LT',
-      defaultDate: new Date()
+      defaultDate: new Date(),
+      icons: icons
     });
     $('.datetimepicker.date-only').datetimepicker({
       useCurrent: true,
       format: 'MM/DD/YYYY',
-      defaultDate: new Date()
+      defaultDate: new Date(),
+      icons: icons
     });
   });
 
