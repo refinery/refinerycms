daeb1829023b23676bf056d4a4940a7039b3b921
Brice Sanchez <brice@bisscomm.com>
2016-02-22 15:54:07 -0500

    Remove deprecated rspec config treat_symbols_as_metadata_keys_with_true_values

--- core/lib/generators/refinery/engine/templates/spec/spec_helper.rb
+++ core/lib/generators/refinery/engine/templates/spec/spec_helper.rb
@@ -17,7 +17,6 @@ Rails.backtrace_cleaner.remove_silencers!
 
 RSpec.configure do |config|
   config.mock_with :rspec
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
 end
