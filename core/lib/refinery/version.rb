82257fe38149db703e5ea47b5327c0c13550c82f
Brice Sanchez <brice@bisscomm.com>
2016-06-07 09:31:19 -0400

    Set version to 4.0.0.dev

--- core/lib/refinery/version.rb
+++ core/lib/refinery/version.rb
@@ -1,9 +1,9 @@
 module Refinery
   class Version
-    @major = 3
+    @major = 4
     @minor = 0
-    @tiny  = 3
-    @build = nil
+    @tiny  = 0
+    @build = 'dev'
 
     class << self
       attr_reader :major, :minor, :tiny, :build
