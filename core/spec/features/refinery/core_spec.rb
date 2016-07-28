8646fb2ceabeb1ba89d44b72c3c859e235ca798b
Philip Arndt <git@p.arndt.io>
2016-07-28 14:18:30 +1200

    Allow usage of bin/rspec ./*/spec --only-failures

--- core/spec/features/refinery/core_spec.rb
+++ core/spec/features/refinery/core_spec.rb
@@ -1,12 +1,14 @@
+require "spec_helper"
+
 module Refinery
   module Core
     describe Engine, :type => :feature do
       describe "#refinery_inclusion!" do
         it "should be idempotent" do
-          expect { visit(refinery.root_path) }.to_not raise_error
+          expect { visit(refinery.root_path) }.not_to raise_error
           Engine.refinery_inclusion!
           Engine.refinery_inclusion!
-          expect { visit(refinery.root_path) }.to_not raise_error
+          expect { visit(refinery.root_path) }.not_to raise_error
         end
       end
     end
