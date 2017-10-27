83b28fbd224fb0a9d24161a0e322411e9821b99f
Brice Sanchez <contact@brice-sanchez.com>
2017-10-26 22:47:37 -0400

    Improve Page and PagePart ObjectType

--- api/app/graph/refinery/api/types/page_type.rb
+++ api/app/graph/refinery/api/types/page_type.rb
@@ -9,7 +9,30 @@ module Refinery
 
         interfaces [Types::ActiveRecordInterface]
 
+        field :parent_id, types.Int
+        field :path, types.String
+        field :show_in_menu, types.Boolean
+        field :link_url, types.String
+        field :menu_match, types.String
+        field :deletable, types.Boolean
+        field :draft, types.Boolean
+        field :skip_to_first_child, types.Boolean
+        field :lft, types.Int
+        field :rgt, types.Int
+        field :depth, types.Int
+        field :view_template, types.String
+        field :layout_template, types.String
+
+        field :locale, types.String
         field :title, types.String
+        field :custom_slug, types.String
+        field :menu_title, types.String
+        field :slug, types.String
+
+        field :meta_description, types.String
+        field :browser_title, types.String
+
+        field :parts, types[Types::PagePartType]
       end
     end
   end
