242f1f7efb1554b74ee5463f4917c2985bc3329b
Josh McArthur <joshua.mcarthur@gmail.com>
2018-03-02 14:25:16 +1300

    Update Refinery::Pages::Type#parts= writer to rewrite arrays of string part titles and print a deprecation notice
    This implementation is taken from
    https://github.com/refinery/refinerycms/commit/3535b906fa. Note that
    the version marker will need updating before release.

--- pages/lib/refinery/pages/type.rb
+++ pages/lib/refinery/pages/type.rb
@@ -4,6 +4,21 @@ module Refinery
 
       attr_accessor :name, :parts, :template
 
+      def parts=(new_parts)
+        @parts = if new_parts.all? { |v| v.is_a?(String) }
+          new_syntax = new_parts.map do |part|
+            { title: part, slug: part.downcase.gsub(" ", "_") }
+          end
+          Refinery.deprecate(
+            "Change Refinery::Pages.default_parts= from #{@parts} to #{new_syntax}",
+            when: "4.1.0"
+          )
+          new_syntax
+        else
+          new_parts
+        end
+      end
+
       def parts
         @parts ||= []
       end
