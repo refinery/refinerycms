bcad92db7c088acb9632ec0fd8848c3fb17424c7
Josh McArthur <joshua.mcarthur@gmail.com>
2018-03-02 14:26:03 +1300

    Refactor test to simply assert that default_parts_for delegates to the type record

--- pages/spec/lib/pages_spec.rb
+++ pages/spec/lib/pages_spec.rb
@@ -32,26 +32,13 @@ module Refinery
 
       context "with registered type" do
         let(:type_name) { "custom_type" }
-        before { Pages::Types.registered.register(type_name) { |type| type.parts = type_parts } }
+        before { Pages::Types.registered.register(type_name) }
         after { Pages::Types.registered.delete(Pages::Types.registered.find_by_name(type_name)) }
 
-        context "page parts specified as array of hashes (part attributes)" do
-          let(:type_parts) { [{slug: "body", title: "Body"}] }
-          it "returns the parts for the type" do
-            type_parts = Pages::Types.registered.find_by_name(type_name).parts
-            page = Page.new(view_template: type_name)
-            expect(subject.default_parts_for(page)).to eq type_parts
-          end
-        end
-
-        context "page parts specified as array of strings" do
-          let(:type_parts) { %w[Body] }
-          it "returns the parts for the type as a hash" do
-            page = Page.new(view_template: type_name)
-            expect(subject.default_parts_for(page)).to eq [{slug: "body", title: "Body"}]
-          end
-
-          xit "warns that this configuration format is deprecated"
+        it "delegates to the type's parts" do
+          part = Pages::Types.registered.find_by_name(type_name)
+          expect(part).to receive(:parts).and_return []
+          subject.default_parts_for(Page.new(view_template: type_name))
         end
       end
     end
