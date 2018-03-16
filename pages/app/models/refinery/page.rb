6f76d4a9aa220244227dfe8a85003f45ef426d6c
Brice Sanchez <bricesanchez@users.noreply.github.com>
2018-03-15 23:34:21 -0400

    Add children_count to acts_as_nested_set of Page

--- pages/app/models/refinery/page.rb
+++ pages/app/models/refinery/page.rb
@@ -55,7 +55,7 @@ module Refinery
 
     # Docs for acts_as_nested_set https://github.com/collectiveidea/awesome_nested_set
     # rather than :delete_all we want :destroy
-    acts_as_nested_set :dependent => :destroy
+    acts_as_nested_set counter_cache: :children_count, dependent: :destroy
 
     friendly_id :custom_slug_or_title, FriendlyIdOptions.options
 
