06e9a73de8a47b992977ff6e2d944fddc665cee8
Brice Sanchez <bricesanchez@users.noreply.github.com>
2018-04-05 20:07:55 -0400

    Improve Refiner::Page#slug_locale for mobility
    See: https://github.com/refinery/refinerycms/pull/3360#discussion_r179470780

--- pages/app/models/refinery/page.rb
+++ pages/app/models/refinery/page.rb
@@ -365,9 +365,9 @@ module Refinery
     end
 
     def slug_locale
-      return Mobility.locale if slug_backend.read(Mobility.locale).present?
+      return Mobility.locale if slug
 
-      if translations.empty? || slug_backend.read(Refinery::I18n.default_frontend_locale).try(:slug).present?
+      if translations.empty? || slug(locale: Refinery::I18n.default_frontend_locale)
         Refinery::I18n.default_frontend_locale
       else
         translations.first.locale
