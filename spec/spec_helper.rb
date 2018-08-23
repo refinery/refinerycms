8064b24fdb79705eba5281e6339d275adfa96ce1
Brice Sanchez <bricesanchez@users.noreply.github.com>
2018-08-23 00:30:13 -0400

    Add no-sandbox config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -67,6 +67,7 @@ require "selenium/webdriver"
 Capybara.register_driver :selenium_chrome_headless do |app|
   browser_options = ::Selenium::WebDriver::Chrome::Options.new
   browser_options.args << '--headless'
+  browser_options.args << '--no-sandbox'
   browser_options.args << '--disable-gpu'
   browser_options.args << '--window-size=1440,1080'
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
