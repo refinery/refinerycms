c3879931612d8cdceb2a635d5b9f8fcfd56e09b6
Joe <6955350+joebutler2@users.noreply.github.com>
2019-11-26 22:09:43 -0500

    Fix the configuration for Dragonfly so you can use the before_s… (#3451)
    This fixes an issue with setting the `Dragonfly` configuration for `before_serve`. It looked like before it was a simple attr_accessor, but since `before_serve` is more like a DSL it was expecting to be called with a block. This PR corrects that, and I'm able to confirm it works on my project.
    
    The good news is this also solves another use case, serving private content. If you look at this Google Groups post from a few years ago, it appears my client isn't alone in wanting this feature. https://groups.google.com/forum/#!searchin/refinery-cms/dragonfly$20before_serve%7Csort:date/refinery-cms/lRBOsddSzHM/1tXNjzaHcBUJ

--- resources/spec/lib/refinery/resources/engine_spec.rb
+++ resources/spec/lib/refinery/resources/engine_spec.rb
@@ -4,5 +4,17 @@ module Refinery
   describe Resources do
     it_has_behaviour 'Creates a dragonfly App:'
     it_has_behaviour 'adds the dragonfly app to the middleware stack'
+
+    it 'calls dragonfly#before_serve to set configuration' do
+      dummy_proc = -> (_job, _env) {}
+      expect_any_instance_of(::Dragonfly::Server).to(
+        receive(:before_serve) { |&block| expect(block).to be(dummy_proc) }
+      )
+      ::Refinery::Resources.configure do |config|
+        config.dragonfly_before_serve = dummy_proc
+      end
+
+      ::Refinery::Dragonfly.configure!(::Refinery::Resources)
+    end
   end
 end
