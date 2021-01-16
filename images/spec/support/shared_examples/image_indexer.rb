db0f9d542c68d23b56417eea8fe46c4e081f4fde
Anita Graham <anita@joli.com.au>
2021-01-16 17:16:52 +0800

    Basic changes for image presenter . ensure markup is identical for each view as far as possible . Use css-grid to prevent image title/file name overwriting icons . Add locale markers to images in grid view

--- images/spec/support/shared_examples/image_indexer.rb
+++ images/spec/support/shared_examples/image_indexer.rb
@@ -1,14 +1,14 @@
 shared_examples_for 'indexes images' do
-
-  let(:image_count) {[Refinery::Image.count, Refinery::Images.pages_per_admin_index].min}
+  let(:image_count) { [Refinery::Image.count, Refinery::Images.pages_per_admin_index].min }
 
   before do
-    raise "please set let(:initial_path)" if initial_path.blank?
+    raise 'please set let(:initial_path)' if initial_path.blank?
+
     ensure_on(initial_path)
     initialize_context
   end
 
-  it 'shows all the images', js: true do
+  it 'shows images', js: true do
     if index_in_frame
       page.within_frame(dialog_frame_id) do
         expect(page).to have_selector(index_item_selector, count: image_count)
@@ -17,79 +17,62 @@ shared_examples_for 'indexes images' do
       expect(page).to have_selector(index_item_selector, count: image_count)
     end
   end
-
 end # image index
 
-shared_examples_for 'shows list and grid views' do
-
-  let(:image_count) {[Refinery::Image.count, Refinery::Images.pages_per_admin_index].min}
+shared_examples_for 'different views' do
+  let(:image_count) { [Refinery::Image.count, Refinery::Images.pages_per_admin_index].min }
 
   before do
-    raise "please set let(:initial_path)" if initial_path.blank?
+    raise 'please set let(:initial_path)' if initial_path.blank?
+
     ensure_on(initial_path)
   end
 
-  context "when in grid view" do
+  context 'grid view' do
+    before { ensure_on(current_path + '?view=grid') }
 
-    before { ensure_on(current_path + "?view=grid") }
-
-    it 'shows the images with thumbnails', js: true do
+    it 'shows thumbnails', js: true do
       expect(page).to have_selector(index_item_selector << gridview_img_selector, count: image_count)
     end
 
-    it 'makes the title attribute of each image available', js: true do
-      expect(page).to have_selector(index_item_selector << gridview_img_selector << gridview_title_selector, count: image_count)
-    end
-
-    it 'makes the alt attribute of each image available', js: true do
-      expect(page).to have_selector(index_item_selector << gridview_img_selector << gridview_alt_selector, count: image_count)
-    end
-
-    it 'has an option to switch to list view' do
+    it 'can switch to list view' do
       expect(page).to have_content(::I18n.t('switch_to', view_name: 'list', scope: 'refinery.admin.images.index.view'))
     end
+  end # grid view
 
-  end  # grid view
-
-  context "when in list view" do
-
-    before  do
-      ensure_on(current_path + "?view=list")
-    end
-
-    it 'makes the title attribute of each image available', js: true do
-      expect(page).to have_selector(index_item_selector <<  listview_title_selector, count: image_count)
-    end
-
-    it 'makes the filename of each image available' do
-      expect(page).to have_selector(index_item_selector << listview_filename_selector, count: image_count)
-    end
-
-    it 'has an option to switch to grid view' do
+  context 'list view' do
+    before do
       ensure_on(current_path + '?view=list')
+    end
 
+    it 'shows image titles', js: true do
+      expect(page).to have_selector(index_item_selector << listview_title_selector, count: image_count)
+    end
+
+    it 'can switch to grid view' do
+      ensure_on(current_path + '?view=list')
       expect(page).to have_content(::I18n.t('switch_to', view_name: 'grid', scope: 'refinery.admin.images.index.view'))
     end
-
-  end # list view
+  end
 end
 
 shared_examples_for 'paginates the list of images' do
-
   let(:image_count) {[Refinery::Image.count, Refinery::Images.pages_per_admin_index].min}
 
   before do
-    raise "please set let(:initial_path)" if initial_path.blank?
+    raise 'please set let(:initial_path)' if initial_path.blank?
+
     ensure_on(initial_path)
   end
 
   describe 'pagination', unless: Refinery::Image.count <= 2 do
-    before {
+    before do
       Refinery::Images.pages_per_admin_index = 2
-      Refinery::Images.pages_per_dialog_that_have_size_options = 2 }
+      Refinery::Images.pages_per_dialog_that_have_size_options = 2
+    end
 
     it 'divides the index into pages' do
-      expect(page).to have_selector("div.pagination em.current")
+      expect(page).to have_selector('div.pagination em.current')
     end
 
     context 'when on the first page' do
@@ -112,7 +95,6 @@ shared_examples_for 'paginates the list of images' do
       it 'has disabled the next link' do
         expect(page).to have_selector('.next_page.disabled')
       end
-
     end
-  end  # pagination
+  end # pagination
 end # image index
