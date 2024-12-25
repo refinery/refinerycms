fe6899356317805fee92fd2207dcaf69dee5ca84
Anita <anita@joli.com.au>
2024-12-25 10:24:11 +0800

    refinery_help_tag will always show the 'tooltip' message

--- core/app/helpers/refinery/tag_helper.rb
+++ core/app/helpers/refinery/tag_helper.rb
@@ -6,7 +6,7 @@ module Refinery
     # Remember to wrap your block with <span class='label_with_help'></span> if you're using a label next to the help tag.
     def refinery_help_tag(title='Tip')
       title = title.html_safe? ? title : h(title)
-      action_icon(:info, '#', title, {tooltip: title})
+      tag.span title, class: :help
     end
 
     # This is just a quick wrapper to render an image tag that lives inside refinery/icons.
