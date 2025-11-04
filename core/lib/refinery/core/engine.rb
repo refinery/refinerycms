716e44cb2c8c8a4db3a0294a28bdcf3309361967
Philip Arndt <git@p.arndt.io>
2025-11-04 16:45:43 +1300

    Refactor Mobility configuration to use plugins

--- core/lib/refinery/core/engine.rb
+++ core/lib/refinery/core/engine.rb
@@ -45,11 +45,16 @@ module Refinery
       end
 
       initializer "refinery.mobility" do
-        Mobility.configure do |config|
-          config.default_backend = :table
-          config.accessor_method = :translates
-          config.query_method    = :i18n
-          config.default_options[:dirty] = true
+        Mobility.configure do
+          plugins do
+            backend :table
+            active_record
+            reader
+            writer
+            query
+            dirty
+            presence
+          end
         end
       end
 
