b0a22acdc7228204e58adcf2d700a5817c62f7dc
Philip Arndt <git@p.arndt.io>
2026-01-02 10:47:20 +1300

    bring back image grid

--- core/app/helpers/refinery/action_helper.rb
+++ core/app/helpers/refinery/action_helper.rb
@@ -34,33 +34,39 @@ module Refinery
 
 
     def edit_in_current_locale(url:, title:, **options)
-      action_icon(:edit, url, title, class: :edit,  **options  )
+      action_icon(:edit, url, title, class: :edit,  **options)
     end
 
     def locale_language(locale)
       Refinery::I18n.locales[locale]
     end
 
-    def edit_in_locale(locale, url:, title:, **options)
+    def edit_in_locale(locale, url:, title: nil, **options)
       if options.delete(:label)
-        title = locale.to_s.upcase
-        action_label(:locale, "#{url}?switch_locale=#{locale}", title, **options,
-                    id: locale, class: :edit, **options
+        action_label(
+          :locale, "#{url}?switch_locale=#{locale}",
+          title || locale.to_s.upcase,
+          **options,
+          id: locale,
+          class: :edit, **options
         )
       else
-        title = ::I18n.t('.edit_in_language', language: locale_language(locale), scope: 'refinery.admin.locale_picker')
-        action_icon(:locale, "#{url}?switch_locale=#{locale}", title,
-                    id: locale, class: :edit, **options
+        action_icon(
+          :locale, "#{url}?switch_locale=#{locale}",
+          title || ::I18n.t('.edit_in_language', language: locale_language(locale), scope: 'refinery.admin.locale_picker'),
+          **options,
+          id: locale,
+          class: :edit
         )
       end
     end
 
 
-    def edit_in_locales(edit_url, locales=[])
-      return if locales.empty?
+    def edit_in_locales(edit_url, locales = [])
+      return if locales.empty? || locales.one?
 
       edit_links = locales.map do |locale|
-        edit_in_locale(locale, url: edit_url, title: t('.edit_in_locale', locale: locale) )
+        edit_in_locale(locale, url: edit_url, title: t('.edit_in_locale', locale: locale))
       end
       tag.span edit_links.compact.join(' ').html_safe, class: :locales
     end
