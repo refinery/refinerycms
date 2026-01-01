c3c3cbca2c562158932f4ab53be4b275911897e2
Philip Arndt <git@p.arndt.io>
2026-01-01 19:02:10 +1300

    Add locales_with_translated_field helper for locale picker views
    The refactored pages and resources views use locales_with_translated_field
    helper to display locale indicators. This helper queries translations to
    find which locales have content for a given field.
    
    Also revert xhr_paging spec to use image_grid selector instead of
    image_index, which was part of the UI refactoring that should stay in
    the feature branch.

--- core/app/helpers/refinery/translation_helper.rb
+++ core/app/helpers/refinery/translation_helper.rb
@@ -14,5 +14,16 @@ module Refinery
     def translated_field(record, field)
       Refinery::TranslatedFieldPresenter.new(record).call(field)
     end
+
+    def locales_with_translated_field(record, field_name, include_current: true)
+      field_name = field_name.to_sym
+      translations = record.translations.where.not(field_name => [nil, ""])
+      translations = translations.where.not(locale: current_locale) unless include_current
+      translations.map { |t| t.locale.to_sym }.sort_by { |locale| Refinery::I18n.frontend_locales.index(locale) }
+    end
+
+    private def current_locale
+      Refinery::I18n.current_locale.to_s
+    end
   end
 end
